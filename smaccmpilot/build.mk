0ca7d88a0a1c7001dba098be0748eba9a83a36b0
James Bielman <jamesjb@galois.com>
2013-01-22 10:34:13 -0800

    smaccmpilot: Remove gcs_transmit.o from build.mk.

--- smaccmpilot/build.mk
+++ smaccmpilot/build.mk
@@ -32,7 +32,6 @@ SMACCMPILOT_OBJECTS :=       \
   src/userinput.o            \
   src/stabilize.o            \
   src/gcs_receive.o          \
-  src/gcs_transmit.o         \
   src/pid_stabilize.o
 
 $(eval $(call library,SMACCMPILOT))
