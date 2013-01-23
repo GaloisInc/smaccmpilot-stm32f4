e378feb379bc06ac7100b715aa1e2fb7783d9fdf
Pat Hickey <pat@galois.com>
2013-01-23 10:29:38 -0800

    smaccmpilot: Stabilize helpers factored into IvoryHelpers

--- smaccmpilot/ivory/IvoryHelpers.hs
+++ smaccmpilot/ivory/IvoryHelpers.hs
@@ -7,15 +7,23 @@ module IvoryHelpers where
 
 import Ivory.Language
 
+----------------------------------------------------------------------
+-- Ivory Utilities
+--
+-- If these are generally useful, they should probably be moved into a
+-- "user-space" Ivory utility module.
+
+-- | Infix structure field access and dereference.
+-- This is a shorthand for 'deref $ s~>x'.
+--
 -- I would like to annotate this with a type signature but Label is not
 -- exported from Ivory.Language
 -- (~>*) :: (IvoryType r, IvoryExpr a) =>
 --     Ref (Struct sym) -> Label sym (Stored a) -> Ivory r a
--- This is a shorthand for 'deref $ s~>x'.
 struct ~>* label = deref $ struct~>label
 infixl 8 ~>*
 
--- Another handy shorthand for transfering members
+-- | handy shorthand for transfering members
 resultInto :: (IvoryType r, IvoryExpr a) =>
      Ivory r a -> Ref (Stored a) -> Ivory r ()
 resultInto a b = do
@@ -28,3 +36,21 @@ into a b = do
   v <- deref a
   store b v
 
+-- | Modify the value stored at a reference by a function.
+(%=) :: (IvoryType r, IvoryExpr a) => Ref (Stored a) -> (a -> a) -> Ivory r ()
+ref %= f = do
+  val <- deref ref
+  store ref (f val)
+
+-- | Modify the value stored at a reference by a function that returns
+-- a value in the Ivory monad.
+(%=!) :: (IvoryType r, IvoryExpr a) =>
+         Ref (Stored a) -> (a -> Ivory r a) -> Ivory r ()
+ref %=! mf = do
+  val  <- deref ref
+  val' <- mf val
+  store ref val'
+
+-- | Increment the value stored at a reference.
+(+=) :: (Num a, IvoryType r, IvoryExpr a) => Ref (Stored a) -> a -> Ivory r ()
+ref += x = ref %= (+ x)
--- smaccmpilot/ivory/Stabilize.hs
+++ smaccmpilot/ivory/Stabilize.hs
@@ -14,6 +14,8 @@
 
 module Stabilize where
 
+import IvoryHelpers
+
 import Control.Applicative
 
 import Ivory.Language
@@ -28,39 +30,6 @@ stabilizeModule = package "pid_stabilize" $ do
   incl pid_update
   incl stabilize_from_angle
   incl stabilize_from_rate
-
-----------------------------------------------------------------------
--- Ivory Utilities
---
--- If these are generally useful, they should probably be moved into a
--- "user-space" Ivory utility module.
-
--- | Infix structure field access and dereference.
---
--- This is a shorthand for 'deref $ s~>x'.
-(~>*) :: (IvoryStruct sym, IvoryType r, IvoryExpr a) =>
-         Ref (Struct sym) -> Label sym (Stored a) -> Ivory r a
-struct ~>* label = deref $ struct~>label
-infixl 8 ~>*
-
--- | Modify the value stored at a reference by a function.
-(%=) :: (IvoryType r, IvoryExpr a) => Ref (Stored a) -> (a -> a) -> Ivory r ()
-ref %= f = do
-  val <- deref ref
-  store ref (f val)
-
--- | Modify the value stored at a reference by a function that returns
--- a value in the Ivory monad.
-(%=!) :: (IvoryType r, IvoryExpr a) =>
-         Ref (Stored a) -> (a -> Ivory r a) -> Ivory r ()
-ref %=! mf = do
-  val  <- deref ref
-  val' <- mf val
-  store ref val'
-
--- | Increment the value stored at a reference.
-(+=) :: (Num a, IvoryType r, IvoryExpr a) => Ref (Stored a) -> a -> Ivory r ()
-ref += x = ref %= (+ x)
 
 ----------------------------------------------------------------------
 -- Math Utilities (move into a Math.hs?)
