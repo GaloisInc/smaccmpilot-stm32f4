d86218a48de6f1189c862bdda848354d79d5b30d
Lee Pike <leepike@gmail.com>
2013-03-01 09:44:10 -0800

    smaccmpilot build: fix formatting.

--- smaccmpilot/ivory/build.mk
+++ smaccmpilot/ivory/build.mk
@@ -12,29 +12,29 @@ EXEC=$(SMACCMDIR)/ivory/dist/build/$(GEN)/$(GEN)
 IVORY-OPTS=--const-fold --overflow --div-zero
 # A little too noisy: --fp-check
 
-HDRS=																										 \
- $(INCDIR)/position_type.h															 \
- $(INCDIR)/pid_stabilize.h															 \
- $(INCDIR)/servo_type.h																	 \
- $(INCDIR)/sensors_type.h																 \
- $(INCDIR)/motorsoutput_type.h													 \
- $(INCDIR)/userinput_type.h                              \
- $(INCDIR)/gcs_transmit_type.h													 \
- $(INCDIR)/userinput_decode.h                            \
- $(INCDIR)/optflow_type.h																 \
- $(INCDIR)/position_estimate_type.h											 \
- $(INCDIR)/ivory_string.h																 \
- $(INCDIR)/console.h																		 \
- $(INCDIR)/driver_i2c.h																	 \
- $(INCDIR)/storage_eeprom.h															 \
- $(INCDIR)/storage_partition.h													 \
+HDRS=	\
+ $(INCDIR)/position_type.h \
+ $(INCDIR)/pid_stabilize.h \
+ $(INCDIR)/servo_type.h \
+ $(INCDIR)/sensors_type.h \
+ $(INCDIR)/motorsoutput_type.h \
+ $(INCDIR)/userinput_type.h \
+ $(INCDIR)/gcs_transmit_type.h \
+ $(INCDIR)/userinput_decode.h \
+ $(INCDIR)/optflow_type.h \
+ $(INCDIR)/position_estimate_type.h	\
+ $(INCDIR)/ivory_string.h \
+ $(INCDIR)/console.h \
+ $(INCDIR)/driver_i2c.h \
+ $(INCDIR)/storage_eeprom.h	\
+ $(INCDIR)/storage_partition.h \
  $(INCDIR)/param.h
 
-SRCS=																										 \
-  $(SRCDIR)/pid_stabilize.c															 \
-  $(SRCDIR)/gcs_transmit_driver.c												 \
-  $(SRCDIR)/userinput_decode.c													 \
-  $(SRCDIR)/storage_partition.c													 \
+SRCS= \
+  $(SRCDIR)/pid_stabilize.c \
+  $(SRCDIR)/gcs_transmit_driver.c	\
+  $(SRCDIR)/userinput_decode.c \
+  $(SRCDIR)/storage_partition.c	\
   $(SRCDIR)/param.c
 
 # ------------------------------------------------------------------------------
@@ -48,30 +48,39 @@ ivory-build: $(EXEC) $(HDRS) $(SRCS)
 # them.  (It's not even clear if that's the right thing to do, since even if
 # they don't change, if the compiler changes, you should rebuild.)
 $(HDRS) $(SRCS):
-	$(SANDBOX)/bin/$(GEN) --src-dir=$(SRCDIR) --include-dir=$(INCDIR) $(IVORY-OPTS)
+	$(SANDBOX)/bin/$(GEN) \
+    --src-dir=$(SRCDIR) \
+    --include-dir=$(INCDIR) $(IVORY-OPTS)
+    #  \
+    # --cfg=true \
+    # --cfg-proc=$(SRCDIR)/pid_stabilize.c
 
 # Build the binary to generate the code.
 .PRECIOUS: $(EXEC)
 $(EXEC):
-	cabal-dev -s $(SANDBOX) install --builddir=$(SMACCMDIR)/ivory $(SMACCMDIR)/ivory
+	cabal-dev -s $(SANDBOX) install \
+    --builddir=$(SMACCMDIR)/ivory $(SMACCMDIR)/ivory
 
 CLEAN     += $(HDRS) $(SRCS)
-VERYCLEAN += $(SMACCMDIR)/ivory/dist
+VERYCLEAN += $(SMACCMDIR)/ivory/dist $(SMACCMDIR)/ivory/cabal-dev
 
 # ------------------------------------------------------------------------------
 # CBMC stuff
 # ------------------------------------------------------------------------------
 
-INCS=                                                    \
-  -I./smaccmpilot/include/smaccmpilot										 \
-  -I./smaccmpilot/include																 \
-  -I./hwf4/include																			 \
-  -I./include																						 \
-  -I./smavlink/include																	 \
-  -I./smavlink/include/smavlink/messages								 \
+INCS= \
+  -I./smaccmpilot/include/smaccmpilot \
+  -I./smaccmpilot/include \
+  -I./hwf4/include \
+  -I./include	\
+  -I./smavlink/include \
+  -I./smavlink/include/smavlink/messages \
   -I./include
 
-STARTS := $(shell $(SANDBOX)/bin/$(GEN) --src-dir=$(SRCDIR) --include-dir=$(INCDIR) --out-proc-syms)
+STARTS := $(shell $(SANDBOX)/bin/$(GEN)\
+  --src-dir=$(SRCDIR) \
+  --include-dir=$(INCDIR) \
+  --out-proc-syms)
 
 # STARTS = stabilize_from_rate stabilize_from_angle
 
