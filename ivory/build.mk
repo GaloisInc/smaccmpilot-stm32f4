67e39d1ea86cecc4a638292bff061c3d5eab2ab4
Pat Hickey <pat@galois.com>
2013-03-01 15:52:05 -0800

    ivory/build.mk: fixed messed up rebase

--- ivory/build.mk
+++ ivory/build.mk
@@ -1,93 +1,49 @@
-SMACCMDIR=smaccmpilot
+GENERATEDDIR=flight-generated
 
 # Relative to current dir
 SANDBOX=../dsl/cabal-dev
 
-SRCDIR=$(SMACCMDIR)/src
-INCDIR=$(SMACCMDIR)/include/smaccmpilot
+SRCDIR=$(GENERATEDDIR)/src
+INCDIR=$(GENERATEDDIR)/include/smaccmpilot
 
 GEN=smaccmpilot-gen
-EXEC=$(SMACCMDIR)/ivory/dist/build/$(GEN)/$(GEN)
+EXEC=ivory/dist/build/$(GEN)/$(GEN)
 
-IVORY-OPTS=--const-fold --overflow --div-zero
+IVORY_OPTS=--const-fold --overflow --div-zero
 # A little too noisy: --fp-check
 
-HDRS=	\
- $(INCDIR)/position_type.h \
- $(INCDIR)/pid_stabilize.h \
- $(INCDIR)/servo_type.h \
- $(INCDIR)/sensors_type.h \
- $(INCDIR)/motorsoutput_type.h \
- $(INCDIR)/userinput_type.h \
- $(INCDIR)/gcs_transmit_type.h \
- $(INCDIR)/userinput_decode.h \
- $(INCDIR)/optflow_type.h \
- $(INCDIR)/position_estimate_type.h	\
- $(INCDIR)/ivory_string.h \
- $(INCDIR)/console.h \
- $(INCDIR)/driver_i2c.h \
- $(INCDIR)/storage_eeprom.h	\
- $(INCDIR)/storage_partition.h \
- $(INCDIR)/param.h
+GENERATED_DEP=$(GENERATEDDIR)/dep.mk
 
-SRCS= \
-  $(SRCDIR)/pid_stabilize.c \
-  $(SRCDIR)/gcs_transmit_driver.c	\
-  $(SRCDIR)/userinput_decode.c \
-  $(SRCDIR)/storage_partition.c	\
-  $(SRCDIR)/param.c
+include $(GENERATED_DEP)
 
 # ------------------------------------------------------------------------------
 
 # Generate the srcs and headers.
 IVORY += ivory-build
 .PHONY: ivory-build
-ivory-build: $(EXEC) $(HDRS) $(SRCS)
+ivory-build: $(EXEC) $(FLIGHT_GENERATED_HEADERS) $(FLIGHT_GENERATED_SOURCES)
+
+$(GENERATED_DEP): 
+	$(SANDBOX)/bin/$(GEN) --src-dir=$(SRCDIR) --include-dir=$(INCDIR) \
+		--deps=$(GENERATED_DEP) --dep-prefix=FLIGHT_GENERATED
 
 # We don't add the .hs files as dependencies.  It's up to the user to clean
 # them.  (It's not even clear if that's the right thing to do, since even if
 # they don't change, if the compiler changes, you should rebuild.)
-$(HDRS) $(SRCS):
-	$(SANDBOX)/bin/$(GEN) \
-    --src-dir=$(SRCDIR) \
-    --include-dir=$(INCDIR) $(IVORY-OPTS)
-    #  \
-    # --cfg=true \
-    # --cfg-proc=$(SRCDIR)/pid_stabilize.c
+$(FLIGHT_GENERATED_HEADERS) $(FLIGHT_GENERATED_SOURCES): $(GENERATED_DEP)
+	$(SANDBOX)/bin/$(GEN) --src-dir=$(SRCDIR) --include-dir=$(INCDIR) \
+		$(IVORY_OPTS)
 
 # Build the binary to generate the code.
 .PRECIOUS: $(EXEC)
 $(EXEC):
-	cabal-dev -s $(SANDBOX) install \
-    --builddir=$(SMACCMDIR)/ivory $(SMACCMDIR)/ivory
+	cabal-dev -s $(SANDBOX) install --builddir=$(SMACCMDIR)/ivory \
+		$(SMACCMDIR)/ivory
 
-CLEAN     += $(HDRS) $(SRCS)
-VERYCLEAN += $(SMACCMDIR)/ivory/dist $(SMACCMDIR)/ivory/cabal-dev
+CLEAN     += $(GENERATED_DEP)
+# use wildcard, not the dep file, to clean subdirs, because if dep file
+# doesn't exist we won't get a proper clean.
+CLEAN     += $(wildcard $(SRCDIR)/*.c)
+CLEAN     += $(wildcard $(INCDIR)/*.h)
+VERYCLEAN += $(SMACCMDIR)/ivory/dist
 
-# ------------------------------------------------------------------------------
-# CBMC stuff
-# ------------------------------------------------------------------------------
-
-INCS= \
-  -I./smaccmpilot/include/smaccmpilot \
-  -I./smaccmpilot/include \
-  -I./hwf4/include \
-  -I./include	\
-  -I./smavlink/include \
-  -I./smavlink/include/smavlink/messages \
-  -I./include
-
-STARTS := $(shell $(SANDBOX)/bin/$(GEN)\
-  --src-dir=$(SRCDIR) \
-  --include-dir=$(INCDIR) \
-  --out-proc-syms)
-
-# STARTS = stabilize_from_rate stabilize_from_angle
-
-CBMC_EXEC := $(addprefix $(CONFIG_CBMC_PREFIX)/, cbmc)
-
-# >&2 redirects cbmc output from stderr so you can see it.
-.PHONY: verify
-verify: $(HDRS) $(SRCS)
-	$(foreach func, $(STARTS), \
-    $(shell $(CBMC_EXEC) -D IVORY_CBMC $(INCS) --function $(func) $(SRCS) >&2 ))
