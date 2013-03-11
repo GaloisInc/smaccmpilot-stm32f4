d5bb822729d008a3617235fb26073dd6e9a80dba
Pat Hickey <pat@galois.com>
2013-03-11 16:34:20 -0700

    fixup apmotors wrapper header

--- flight-support/include/smaccmpilot/apmotors_wrapper.h
+++ flight-support/include/smaccmpilot/apmotors_wrapper.h
@@ -12,7 +12,8 @@ extern "C" {
 #include "smaccmpilot/flightmode_type.h"
 
 void apmotors_output_init(void);
-void apmotors_output_set(const struct controloutput *);
+void apmotors_output_set(const struct controloutput *,
+                         const struct flightmode *);
 void apmotors_servo_get(struct servos *);
 
 #ifdef __cplusplus
