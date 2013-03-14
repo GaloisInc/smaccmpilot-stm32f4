63624f73063dfa0874ffc2f08710b9d8eba224f0
Pat Hickey <pat@galois.com>
2013-03-14 16:06:33 -0700

    ivory-bsp-hwf4wrapper: add usart driver

--- bsp/ivory/ivory-bsp-hwf4wrapper/src/Ivory/BSP/HWF4.hs
+++ bsp/ivory/ivory-bsp-hwf4wrapper/src/Ivory/BSP/HWF4.hs
@@ -6,13 +6,15 @@ module Ivory.BSP.HWF4
 import Ivory.Language
 
 import Ivory.BSP.HWF4.EEPROM
-import Ivory.BSP.HWF4.GPIO
 import Ivory.BSP.HWF4.I2C
+import Ivory.BSP.HWF4.GPIO
+import Ivory.BSP.HWF4.USART
 
 hwf4Modules :: [Module]
 hwf4Modules =
   [ eepromModule
-  , gpioModule
   , i2cModule
+  , gpioModule
+  , usartModule
   ]
 
