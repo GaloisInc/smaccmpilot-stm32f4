1c66d6e69b06466c8ad936a786503f53804341d7
Pat Hickey <pat@galois.com>
2013-03-21 18:49:36 -0700

    gcs recieve: basic ivory implementation which runs mavlinkRecieveByte machine once

--- ivory/SMACCMPilot/Flight/GCS/Receive/Task.hs
+++ ivory/SMACCMPilot/Flight/GCS/Receive/Task.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module SMACCMPilot.Flight.GCS.Receive.Task
   ( gcsReceiveTask
@@ -11,8 +12,13 @@ import Prelude hiding (last)
 
 import Ivory.Language
 import Ivory.Tower
+
 import qualified Ivory.OS.FreeRTOS as OS
 import SMACCMPilot.Util.Periodic
+
+import Ivory.BSP.HWF4.USART
+
+import qualified SMACCMPilot.Mavlink.Receive as R
 
 import qualified SMACCMPilot.Flight.Types.GCSStreamTiming as S
 import SMACCMPilot.Flight.GCS.Stream (defaultPeriods)
@@ -20,15 +26,31 @@ import SMACCMPilot.Flight.GCS.Stream (defaultPeriods)
 gcsReceiveTask :: MemArea (Struct "usart")
                -> Source (Struct "gcsstream_timing")
                -> String -> Task
-gcsReceiveTask usart s_src uniquename =
+gcsReceiveTask usart_area s_src uniquename =
   withSource "streamperiods" s_src $ \streamPeriodSource ->
   let tDef = proc ("gcsReceiveTaskDef" ++ uniquename) $ body $ do
         s_periods <- local defaultPeriods
         source streamPeriodSource (constRef s_periods)
+
+        usart <- addrOf usart_area
+        buf <- local (iarray []) ::
+          Ivory (Top s) () (Ref (Stack s) (Array 1 (Stored Uint8)))
+        state <- local (istruct [ R.status .= ival R.status_IDLE ])
         forever $ do
-          call_ OS.delay 10000
+          n <- call usartRead usart (toCArray buf) 1
+          ifte (n ==? 0)
+            (return ())
+            (do b <- deref (buf ! 0)
+                R.mavlinkReceiveByte state b
+                s <- deref (state ~> R.status)
+                ifte (s ==? R.status_GOTMSG)
+                  (forever (call_ OS.delay 1000)) -- trap for debugger.
+                  (return ())
+            )
       mDefs = do
         depend OS.taskModule
+        depend usartModule
+        defStruct (Proxy :: Proxy "mavlink_receive_state")
         incl tDef
   in task tDef mDefs
 
