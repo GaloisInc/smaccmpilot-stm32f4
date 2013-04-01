bc043449954d0e00fb0c86d469de39e0b3b36435
James Bielman <jamesjb@galois.com>
2013-04-01 15:06:43 -0700

    ivory-bsp-stm32f4: Update bit data definitions to MSB-first order.

--- bsp/ivory/ivory-bsp-stm32f4/src/Ivory/BSP/STM32F4/SPI.hs
+++ bsp/ivory/ivory-bsp-stm32f4/src/Ivory/BSP/STM32F4/SPI.hs
@@ -22,42 +22,44 @@ import Ivory.BSP.STM32F4.GPIO
 
 [bitdata|
  bitdata SPI_CR1 :: Bits 16 = spi_cr1
-   { spi_cr1_cpha     :: Bit
-   , spi_cr1_cpol     :: Bit
-   , spi_cr1_mstr     :: Bit
-   , spi_cr1_br       :: SPIBaud
-   , spi_cr1_spe      :: Bit
-   , spi_cr1_lsbfirst :: Bit
-   , spi_cr1_ssi      :: Bit
-   , spi_cr1_ssm      :: Bit
-   , spi_cr1_rxonly   :: Bit
-   , spi_cr1_dff      :: Bit
-   , spi_cr1_crcnext  :: Bit
-   , spi_cr1_crcen    :: Bit
+   { spi_cr1_bidimode :: Bit
    , spi_cr1_bidioe   :: Bit
-   , spi_cr1_bidimode :: Bit
+   , spi_cr1_crcen    :: Bit
+   , spi_cr1_crcnext  :: Bit
+   , spi_cr1_dff      :: Bit
+   , spi_cr1_rxonly   :: Bit
+   , spi_cr1_ssm      :: Bit
+   , spi_cr1_ssi      :: Bit
+   , spi_cr1_lsbfirst :: Bit
+   , spi_cr1_spe      :: Bit
+   , spi_cr1_br       :: SPIBaud
+   , spi_cr1_mstr     :: Bit
+   , spi_cr1_cpol     :: Bit
+   , spi_cr1_cpha     :: Bit
    }
 
  bitdata SPI_CR2 :: Bits 16 = spi_cr2
-   { spi_cr2_rxdmaen  :: Bit
-   , spi_cr2_txdmaen  :: Bit
-   , spi_cr2_ssoe     :: Bit
-   , _                :: Bits 2
-   , spi_cr2_errie    :: Bit
-   , spi_cr2_rxneie   :: Bit
+   { _                :: Bits 8
    , spi_cr2_txeie    :: Bit
-   , _                :: Bits 8
+   , spi_cr2_rxneie   :: Bit
+   , spi_cr2_errie    :: Bit
+   , _                :: Bits 2
+   , spi_cr2_ssoe     :: Bit
+   , spi_cr2_txdmaen  :: Bit
+   , spi_cr2_rxdmaen  :: Bit
    }
 
   bitdata SPI_SR :: Bits 16 = spi_sr
-   { spi_sr_rxne      :: Bit
-   , spi_sr_txe       :: Bit
-   , spi_sr_chside    :: Bit
-   , spi_sr_udr       :: Bit
-   , spi_sr_crcerr    :: Bit
-   , spi_sr_modf      :: Bit
-   , spi_sr_ovr       :: Bit
+   { _                :: Bits 7
+   , spi_sr_fre       :: Bit
    , spi_sr_bsy       :: Bit
+   , spi_sr_ovr       :: Bit
+   , spi_sr_modf      :: Bit
+   , spi_sr_crcerr    :: Bit
+   , spi_sr_udr       :: Bit
+   , spi_sr_chside    :: Bit
+   , spi_sr_txe       :: Bit
+   , spi_sr_rxne      :: Bit
    }
 |]
 
