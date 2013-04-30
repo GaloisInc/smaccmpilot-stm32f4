85b275c220db2831c16fe6b532b46c5f64cc3a73
Lee Pike <leepike@vapidus.galois.com>
2013-04-30 16:12:08 -0700

    sample-rtv-task: wibble to builid.

--- sample-rtv-task/build.mk
+++ sample-rtv-task/build.mk
@@ -1,4 +1,5 @@
 # -*- Mode: makefile-gmake; indent-tabs-mode: t; tab-width: 2 -*-
+# vim: set ft=make noet ts=2:
 
 # Should be the sample name as the project directory.
 PRJ := sample-rtv-task
@@ -21,10 +22,12 @@ RTV_CHECKER_GEN_EXE := \
 APP_RTV_INCLUDES     += $(FREERTOS_INCLUDES)
 APP_RTV_INCLUDES     += -I$(TOP)/ivory-freertos-wrapper/include
 APP_RTV_INCLUDES     += -I$(TOP)/ivory-runtime/
+APP_RTV_INCLUDES     += -I$(TOP)/$(OUTDIR)
 APP_RTV_INCLUDES     += -I$(TOP)/bsp/hwf4/include
 
 APP_RTV_CFLAGS       += $(APP_RTV_INCLUDES)
-APP_RTV_CFLAGS       += -fplugin=$(TOP)/../../ARM-analysis/GCC_plugin/instrument_plugin.so
+APP_RTV_CFLAGS       += \
+  -fplugin=$(TOP)/../../ARM-analysis/GCC_plugin/instrument_plugin.so
 
 APP_RTV_LIBRARIES    += libhwf4.a
 APP_RTV_LIBRARIES    += libstm32_usb.a
@@ -42,13 +45,6 @@ $(RTV_GEN_HEADERS) $(RTV_GEN_SOURCES): $(RTV_CHECKER_GEN_EXE)
 	echo "Generating RTV sources..."
 	cd $(PRJ) && ../$(RTV_CHECKER_GEN_EXE)
 
-# Made from DSL file.
-# .PRECIOUS: $(RTV_CHECKER_GEN_EXE)
-# $(RTV_CHECKER_GEN_EXE):
-# 	cabal-dev -s $(IVORY_RTV_SANDBOX) install --builddir=$(TOP)/$(PRJ) \
-# 	          $(TOP)/$(PRJ)
-
-# vim: set ft=make noet ts=2:
-
 CLEAN += $(OUTDIR)
 CLEAN += $(addprefix $(OBJ_DIR)/, $(PRJ))
+
