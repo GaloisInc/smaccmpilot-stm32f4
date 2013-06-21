1b735a7c2f462565b738d4980327b42658365075
Pat Hickey <pat@galois.com>
2013-06-21 15:19:39 -0700

    ivory-bsp-stm32f4: spi umbrella

---
+++ src/bsp/ivory/ivory-bsp-stm32f4/src/Ivory/BSP/STM32F4/SPI.hs
@@ -0,0 +1,11 @@
+
+module Ivory.BSP.STM32F4.SPI
+  -- Regs definitions:
+  ( SPIRegs()
+  , spi1Regs, spi2Regs, spi3Regs
+  -- Peripheral definitions:
+  , SPIDevice()
+  ) where
+
+import Ivory.BSP.STM32F4.SPI.Regs
+import Ivory.BSP.STM32F4.SPI.Peripheral
