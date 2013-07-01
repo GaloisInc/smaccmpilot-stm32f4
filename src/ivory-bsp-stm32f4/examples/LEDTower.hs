d3de257b30231d4a553a4a383dd893f633496089
Pat Hickey <pat@galois.com>
2013-07-01 16:37:14 -0700

    ivory-bsp-stm32f4: well-commented LEDTower to use as example in tech talk

--- src/ivory-bsp-stm32f4/examples/LEDTower.hs
+++ src/ivory-bsp-stm32f4/examples/LEDTower.hs
@@ -27,28 +27,43 @@ ledOff pin = pinSetMode pin gpio_mode_analog
 
 ------------------------------
 
-blink :: (SingI n) => Integer -> ChannelSource n (Stored IBool) -> Task ()
-blink per outputSource = do
-  outputEmitter <- withChannelEmitter outputSource "output"
-  p <- withPeriod per
-  taskBody $ \sch -> do
-    out <- local (ival false)
-    eventLoop sch $ onTimer p $ \time -> do
-      store out ((time .% (fromIntegral (2*per))) <? (fromIntegral per))
-      emit_ sch outputEmitter (constRef out)
-
+-- | LED Controller: Given a set of GPIO pins and a control channel of booleans,
+--   setup the pin hardware, and turn the pins on when the control channel is
+--   true.
 ledController :: (SingI n) => [GPIOPin] -> ChannelSink n (Stored IBool) -> Task ()
 ledController pins outputSink = do
+  -- Bring the channel into scope for this Task
   rxer <- withChannelReceiver outputSink "outputSink"
-  p <- withPeriod 250
+  -- Bookkeeping: this task uses Ivory.HW.Module.hw_moduledef
   taskModuleDef $ const hw_moduledef
-  taskBody $ \sch -> do
+  -- Task Body is parameterized on receiver
+  taskBody (ledTaskBody rxer)
+  where
+  -- Task body takes receiver and schedule
+  ledTaskBody rxer sch = do
+    -- First, setup hardware
     mapM_ ledSetupPin pins
-    eventLoop sch $ onChannel rxer $ \stateref -> do
-      out <- deref stateref
+    -- Then, make an event loop with one handler.
+    eventLoop sch $ onChannelV rxer $ \out-> do
+      -- Turn pins on or off according to event value
       ifte_ out
         (mapM_ ledOn pins)
         (mapM_ ledOff pins)
+
+-- | Blink task: Given a period and a channel source, output an alternating
+--   stream of true / false on each period.
+blink :: (SingI n) => Integer -> ChannelSource n (Stored IBool) -> Task ()
+blink per outSource = do
+  -- Bring the emitter into scope for this Task
+  outEmitter <- withChannelEmitter outSource "output"
+  -- Declare a period for this Task
+  t <- withPeriod per
+  taskBody $ \sch -> do
+    -- Make an event loop with a handler for the period declared above
+    eventLoop sch $ onTimer t $ \time ->
+      -- Emit boolean value which will alternate each period.
+      emitV_ sch outEmitter ((time .% 2*p) <? p)
+  where p = fromIntegral per
 
 blinkApp :: Integer -> [GPIOPin] -> Tower ()
 blinkApp period pins = do
