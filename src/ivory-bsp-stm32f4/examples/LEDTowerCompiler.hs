750890936248bd8f4bd83e5e812b006e677f9c9c
Pat Hickey <pat@galois.com>
2013-08-06 19:55:37 -0700

    ivory-bsp-stm32f4: led tower compiler tests new platform polymorphism in tower

--- src/ivory-bsp-stm32f4/examples/LEDTowerCompiler.hs
+++ src/ivory-bsp-stm32f4/examples/LEDTowerCompiler.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 
 -- Compiler imports:
 import Ivory.Tower.Frontend
@@ -9,15 +10,34 @@ import Ivory.Tower
 import Ivory.BSP.STM32F4.GPIO
 import LEDTower (blinkApp)
 
-app :: Tower p ()
+app :: forall p . (ColoredLEDs p) => Tower p ()
 app = blinkApp period leds
   where
   period = 250
   -- On PX4FMU 1.x, these are the blue and red leds:
-  leds = [pinB14, pinB15]
+  leds = [redLED p, blueLED p]
+  p = (undefined :: p) -- ugly, is there a better way?
 
-main = compile conf app
+data PX4FMUv17 = PX4FMUv17
+data OpenF407 = OpenF407
+
+class ColoredLEDs p where
+  redLED  :: p -> GPIOPin
+  blueLED :: p -> GPIOPin
+
+instance ColoredLEDs PX4FMUv17 where
+  redLED _  = pinB14
+  blueLED _ = pinB15
+
+instance ColoredLEDs OpenF407 where -- this is just made up
+  redLED _  = pinA14
+  blueLED _ = pinA15
+
+main = compilePlatforms conf platforms
   where
   conf = searchPathConf [HW.searchDir, BSP.searchDir]
+  platforms =
+    [("px4fmu17", Twr (app :: Tower PX4FMUv17 ()))
+    ,("open407",  Twr (app :: Tower OpenF407 ()))]
 
 
