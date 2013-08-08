b0527a459f3e93c535f000cdd7cc753b6a7e7c38
Thomas M. DuBuisson <tommd@galois.com>
2013-08-07 19:22:11 -0700

    Some additional comments on the commsec example.c

--- apps/tom-aes-test/example.c
+++ apps/tom-aes-test/example.c
@@ -1,22 +1,47 @@
-#include "aeslib/commsec.h"
 #include <string.h>
 #include <stdio.h>
+#include "aeslib/commsec.h"
 
 int main()
 {
+    // The commsec_ctx structure is a structure that is *mutated*
+    // as data is sent (counter value is incremented) and received
+    // (one of an array of counter values can be updated).
     commsec_ctx uav, base0, base1;
+
+    // Separate keys allow a clean division of ownership of particular
+    // fields in our commsec_ctx structure.  Additionally, separate keys
+    // is just standard practice, which we bend pretty far.
     uint8_t uav_to_base_key[16] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
     uint8_t base_to_uav_key[16] = {15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0};
+
+    // The uavID can be thought of as completely arbitrary.
+    // I was tempted to define a new format that did not require the
+    // UAV to have an ID, but keeping symmetry proves more useful.
     uint32_t uavID = 0;
+
+    // The base station IDs must be unique - this is critical!
     uint32_t base0ID = 0;
     uint32_t base1ID = 1;
     uint32_t b2uSalt = 9219834;
     uint32_t u2bSalt = 284920;
+
+    // Our API was built with the assumption that messages, "msg" here,
+    // would be build in-place in a larger buffer we call the "package".
+    // Thus, this example with a static "msg" variable whose memory must be
+    // copied is not representative of the intended use case.
     unsigned char *msg = "This is a message"; // Message to encrypt
     unsigned char msgLen = strlen(msg);
-    uint8_t *package_from_uav;                // Storage for the ciphertexts
+
+    // package_* variables are used to store the ciphertext from one
+    // entity to another.
+    uint8_t *package_from_uav;
     uint8_t *package_from_base0;
     uint8_t *package_from_base1;
+
+    // We can use the size_of_package function to determine how large our
+    // buffer needs to be in order to accommodate a plaintext message of
+    // a given length.
     uint32_t packageLen = securePkg_size_of_package(msgLen);
     uint32_t res=0;
     int i;
@@ -27,11 +52,19 @@ int main()
     package_from_base1 = malloc(packageLen);
     // FIXME error check malloc and all other function calls
 
-    // Populate packages, leaving the room the the header at the front
+    // Populate packages, leaving the room the the header at the front.
+    // The room required for the header is a static amount 'HEADER_LEN'.
+    //
+    // Potentially, future changes could alter the cipher or authentication
+    // used and this is why we used securePkg_size_of_package(), instead
+    // of adding a static value, to obtain the overall package size.
     memcpy(package_from_uav   + HEADER_LEN, msg, msgLen);
     memcpy(package_from_base0 + HEADER_LEN, msg, msgLen);
     memcpy(package_from_base1 + HEADER_LEN, msg, msgLen);
 
+    // Initialization includes setting up tables for the GCM routine,
+    // expanding AES keys, initializing counters to zero (incoming) and
+    // one (outgoing).
     securePkg_init(&uav, uavID, b2uSalt, base_to_uav_key
                               , u2bSalt, uav_to_base_key);
     securePkg_init(&base0, base0ID, u2bSalt, uav_to_base_key
@@ -39,7 +72,7 @@ int main()
     securePkg_init(&base1, base1ID, u2bSalt, uav_to_base_key
                                   , b2uSalt, base_to_uav_key);
     // NB securePkg_init does not zero the raw key (*_key)
-    // for security reasons, this memory should be zeroed
+    // for security reasons, that memory should be zeroed
 
 #if 1
     fprintf(stderr, "Plaintext: ");
@@ -54,7 +87,10 @@ int main()
     fprintf(stderr, "\n");
 #endif
 
-    // Perform some encryptions
+    // Perform some encryptions.  Alternatively, securePkg_enc() could have
+    // been used and passed the right pointers to the memory area for the
+    // header (package) the message (package+HEADER_LEN) and tail
+    // (package+HEADER_LEN+msgLen).
     securePkg_enc_in_place(&uav,    package_from_uav , HEADER_LEN, msgLen);
     securePkg_enc_in_place(&base0, package_from_base0, HEADER_LEN, msgLen);
     securePkg_enc_in_place(&base1, package_from_base1, HEADER_LEN, msgLen);
@@ -85,12 +121,14 @@ int main()
     // Bases will not decrypt messages from other bases.
     // No entity will decrypt a message more than once (replays)
     res |= securePkg_dec(&uav, package_from_uav, packageLen);
+    fprintf(stderr, "Result of invalid decryptions (should be nonzero): %d\n", res);
     res |= securePkg_dec(&base0, package_from_base0, packageLen);
+    fprintf(stderr, "Result of invalid decryptions (should be nonzero): %d\n", res);
     res |= securePkg_dec(&base0, package_from_base1, packageLen);
     fprintf(stderr, "Result of invalid decryptions (should be nonzero): %d\n", res);
 
-    // Due to _dec mutating memory, reencrypt (just for purpose of this example)
-    // Perform some encryptions
+    // Due to _dec mutating memory, we reencrypt
+    // (just for purpose of this example). Then perform some encryptions.
     memcpy(package_from_uav   + HEADER_LEN, msg, msgLen);
     memcpy(package_from_base0 + HEADER_LEN, msg, msgLen);
     memcpy(package_from_base1 + HEADER_LEN, msg, msgLen);
