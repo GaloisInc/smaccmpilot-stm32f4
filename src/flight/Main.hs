63d6fa634e2bf298e3fce7649c69cf26e5aa15b4
Pat Hickey <pat@galois.com>
2013-08-19 17:16:19 -0700

    SMACCMPilot main: fix the build

--- src/flight/Main.hs
+++ src/flight/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import Ivory.Language
 
 import Ivory.Stdlib.String (stdlibStringModule)
+import qualified Ivory.Stdlib.SearchDir as Stdlib
 
 import Ivory.Tower
 import Ivory.Tower.Frontend
@@ -20,11 +21,16 @@ import SMACCMPilot.Flight.UserInput.Task
 import SMACCMPilot.Flight.BlinkTask
 import SMACCMPilot.Flight.GCS.Tower
 
+import SMACCMPilot.Param (paramModule)
+import SMACCMPilot.Console (consoleModule)
+import SMACCMPilot.Storage.Partition (partitionModule)
+
 import SMACCMPilot.Mavlink.Messages (mavlinkMessageModules)
 import SMACCMPilot.Mavlink.Pack (packModule)
 import SMACCMPilot.Mavlink.CRC (mavlinkCRCModule)
 
 import qualified Ivory.BSP.HWF4.EEPROM as HWF4
+import qualified Ivory.BSP.HWF4.I2C as HWF4
 
 import qualified Ivory.BSP.STM32F4.SearchDir as BSP
 import qualified Ivory.BSP.STM32F4.GPIO as GPIO
@@ -35,7 +41,7 @@ import Arm32SizeMap (sizeMap)
 main :: IO ()
 main = compile conf app
   where
-  sp   = searchPathConf [HW.searchDir, BSP.searchDir]
+  sp   = searchPathConf [Stdlib.searchDir, HW.searchDir, BSP.searchDir]
   conf = sp { bc_sizemap = Just sizeMap }
 
 app :: Tower p ()
@@ -68,16 +74,24 @@ app = do
   redledpin = GPIO.pinB14
 
 otherms :: [Module]
-otherms =
+otherms = concat
   -- flight types
-  typeModules ++
+  [ typeModules
   -- control subsystem
-  controlModules ++
+  , controlModules
   -- mavlink system
-  mavlinkMessageModules ++ [packModule, mavlinkCRCModule] ++
-  -- hwf4 bsp
-  [ HWF4.eepromModule ] ++
+  , mavlinkMessageModules
+  ] ++
+  [ packModule
+  , mavlinkCRCModule
+  -- Param and related helpers:
+  , paramModule
+  , consoleModule
+  , partitionModule
+  -- hwf4 bsp is used for I2C, EEPROM
+  , HWF4.eepromModule
+  , HWF4.i2cModule
   -- the rest:
-  [ userInputDecodeModule
+  , userInputDecodeModule
   , stdlibStringModule
   ]
