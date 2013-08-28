50004099c00410a185cbb1f31d4fadd0a5234600
Pat Hickey <pat@galois.com>
2013-08-28 10:41:49 -0700

    open407toy: started implementing pin debouncer
    * need to change up some statemachine semantics to make it work

--- src/ivory-bsp-stm32f4/examples/Open407Toy.hs
+++ src/ivory-bsp-stm32f4/examples/Open407Toy.hs
@@ -1,14 +1,16 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE RecursiveDo #-}
 
 import Data.Char (ord)
 
 import Ivory.Language
 import Ivory.Stdlib
-import Ivory.Tower
 import Ivory.HW
 import Ivory.BitData
+import Ivory.Tower
+import Ivory.Tower.StateMachine
 
 import Ivory.Tower.Frontend
 import qualified Ivory.HW.SearchDir as HW
@@ -34,3 +36,51 @@ main = compilePlatforms conf [("open407vc", Twr app)]
   where
   conf = searchPathConf [HW.searchDir, BSP.searchDir]
 
+
+button :: GPIOPin -> Tower p (ChannelSink 1 (Stored IBool))
+button pin = do
+  c <- channelWithSize
+  task "btn" $ do
+    e <- withChannelEmitter (src c) "btnstate"
+    ctr <- taskLocalInit "changectr" (ival (0 :: Uint32))
+    debouncer <- stateMachine "debouncer" $ mdo
+                         -- XXX periodically:
+      down    <- state $ onTimeout 1 $ liftIvory $ do
+                   v <- pinRead pin
+                   return $ branch v rising
+      rising  <- state $ do
+                   -- XXX really, on entering state should have its own
+                   -- handler...
+                   onTimeout 0 $ liftIvory_ (store ctr 0)
+                   -- XXX periodically:
+                   onTimeout 1 $ liftIvory $ do
+                     v <- pinRead pin
+                     c <- deref ctr
+                     return $ do
+                       branch (iNot v)          down
+                       branch (c >=? threshold) risen
+      risen   <- state $ onTimeout 0 $ do
+                   liftIvory_ (emitV_ e true)
+                   goto up
+      up      <- state $ onTimeout 1 $ liftIvory $ do
+                   v <- pinRead pin
+                   return $ branch (iNot v) falling
+      falling <- state $ do
+                   onTimeout 0 $ liftIvory_ (store ctr 0)
+                   onTimeout 1 $ liftIvory $ do
+                     v <- pinRead pin
+                     c <- deref ctr
+                     return $ do
+                       branch v                 up
+                       branch (c >=? threshold) fallen
+      fallen  <- state $ onTimeout 0 $ do
+                   liftIvory_ (emitV_ e false)
+                   goto down
+      return down
+    taskInit $ do
+      pinEnable pin
+      pinSetMode pin gpio_mode_input
+      begin debouncer
+
+  return (snk c)
+  where threshold = 50
