493c015edede768ec1c6246a22bb8afa1cac06cf
Pat Hickey <pat@galois.com>
2013-09-03 19:12:46 -0700

    ivory-px4-hw: by all accounts the ioar driver works.
    * Test code in next commit

--- src/ivory-px4-hw/SMACCMPilot/Hardware/PX4IOAR.hs
+++ src/ivory-px4-hw/SMACCMPilot/Hardware/PX4IOAR.hs
@@ -36,10 +36,9 @@ pins_init = do
 
 select_set :: Uint8 -> IBool -> Ivory eff ()
 select_set pnum v = foldl sel (return ()) (zip [(0::Int)..] select_pins)
-  where
-  sel acc (ind,pin) = ifte_ ((fromIntegral ind) ==? pnum) (act pin) acc
-  -- Active Low:
+  where -- Active Low:
   act pin = ifte_ v (pinClear pin) (pinSet pin)
+  sel acc (ind,pin) = ifte_ ((fromIntegral ind) ==? pnum) (act pin) acc
 
 select_set_all :: IBool -> Ivory eff ()
 select_set_all v = mapM_ act select_pins
@@ -57,20 +56,19 @@ px4ioarTower motorChan = do
     istream <- withChannelEvent   motorChan  "motor_istream"
     motorInit <- taskLocal "motorInit"
     bootAttempts <- taskLocal "bootAttempts"
+    throttle <- taskLocal "throttle"
     let put :: (Scope cs ~ GetAlloc eff) => Uint8 -> Ivory eff ()
         put = emitV_ ostream
         putbyte = liftIvory_ . put
+        putPacket :: (SingI n)
+                  => ConstRef s (Array n (Stored Uint8))
+                  -> Ivory (AllocEffects cs) ()
+        putPacket p = arrayMap $ \i ->
+          noBreak $ deref (p ! i) >>= (emitV_ ostream)
         initBytes = [0xE0, 0x91, 0xA1, 0x40]
         sendMultiPacket :: (Scope cs ~ GetAlloc eff) => Ivory eff ()
         sendMultiPacket = replicateM_ 6 (put 0xA0)
 
-        turnM1OnLow :: (Scope cs ~ GetAlloc eff) => Ivory eff ()
-        turnM1OnLow = do
-          put 0x20
-          put 0xA0
-          put 0x00
-          put 0x00
-          put 0x00
     sm <- stateMachine "ioar" $ mdo
       bootBegin <- stateNamed "bootBegin" $ do
         timeout 100 $ liftIvory $ do
@@ -107,6 +105,7 @@ px4ioarTower motorChan = do
       initMulti <- stateNamed "initMulti" $ entry $ do
         liftIvory_ $ do
           select_set_all true
+          -- Send it twice for good luck.
           sendMultiPacket
           sendMultiPacket
         goto bootCheckComplete
@@ -114,12 +113,25 @@ px4ioarTower motorChan = do
         timeout 2 $ liftIvory $ do
           t <- deref bootAttempts
           store bootAttempts (t+1)
+          -- Need to try booting thrice for it to stick.  First boot tends to
+          -- make motor controller leds blink, and nothing else. Second boot
+          -- tends to boot motors 2,3,4. Third boot will generally boot motor 1.
+          -- This comment brought to you by makers of High Assurance Software (tm)
           return $ do
             branch (t <? 2) bootBegin
             goto loop
-      loop <- stateNamed "loop" $ timeout 5 $ do
-        liftIvory_ turnM1OnLow
-        goto loop
+      loop <- stateNamed "loop" $ do
+        entry $ liftIvory_ $ do
+          arrayMap $ \i ->
+            store (throttle ! i) 0.0
+        period 5 $ liftIvory_ $ do
+          scaled   <- scale_motors (constRef throttle)
+          packet   <- motor_packet scaled
+          putPacket packet
+        on istream $ \inputs -> liftIvory_ $
+          arrayMap $ \i -> do
+            input <- deref (inputs ! i)
+            store (throttle ! i) input
       return bootBegin
 
     taskInit $ do
@@ -128,17 +140,42 @@ px4ioarTower motorChan = do
       store bootAttempts (0::Uint32)
       begin sm
 
- -- change motor mixer [0.0f..1.0f] range to to [0..500]
+-- change motor mixer [0.0f..1.0f] range to to [0..500]
 scale_period :: IFloat -> Uint16
 scale_period f = castWith 0 (500*f)
 
-scale_motors :: ConstRef s (Array 4 (Stored IFloat)) -> Ivory eff (ConstRef s' (Array 4 (Stored Uint16)))
-scale_motors ms = undefined
+scale_motors :: (GetAlloc eff ~ Scope cs)
+             => ConstRef s (Array 4 (Stored IFloat))
+             -> Ivory eff (ConstRef (Stack cs) (Array 4 (Stored Uint16)))
+scale_motors ms = do
+  out <- local (iarray [ival 0, ival 0, ival 0, ival 0])
+  arrayMap $ \i -> do
+    m <- deref (ms ! i)
+    store (out ! i) (scale_period m)
+  return (constRef out)
 
-ardrone_motor_set :: ConstRef s (Array 4 (Stored Uint16)) -> Ivory eff ()
-ardrone_motor_set periods = return () -- XXX
+motor_packet :: (GetAlloc eff ~ Scope cs)
+             => ConstRef s (Array 4 (Stored Uint16))
+             -> Ivory eff (ConstRef (Stack cs) (Array 5 (Stored Uint8)))
+motor_packet ms = do
+  m1 <- deref (ms ! 0)
+  m2 <- deref (ms ! 1)
+  m3 <- deref (ms ! 2)
+  m4 <- deref (ms ! 3)
+  let (m1h, m1l) = hiLo $ shiftUp m1 4
+      (m2h, m2l) = hiLo $ shiftUp m2 3
+      (m3h, m3l) = hiLo $ shiftUp m3 2
+      (m4h, m4l) = hiLo $ shiftUp m4 1
+      b0 = 0x20 .| m1h
+      b1 = m1l  .| m2h
+      b2 = m2l  .| m3h
+      b3 = m3l  .| m4h
+      b4 = m4l
+  out <- local (iarray [ival b0, ival b1, ival b2, ival b3, ival b4])
+  return (constRef out)
+  where
+  hiLo :: Uint16 -> (Uint8, Uint8)
+  hiLo a = (ubits a, lbits a)
+  shiftUp :: Uint16 -> Uint16 -> Uint16
+  shiftUp m s = (m .& 0x01FF) `iShiftL` s
 
-ardrone_pins_init :: Ivory eff ()
-ardrone_pins_init = return () -- XXX
-
--- motor_init: state machine that requires timed waiting
