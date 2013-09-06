6f2f6d334c9020ec046ddb285bdad3c3baf8cbb2
Lee Pike <leepike@gmail.com>
2013-09-05 23:14:24 -0700

    hxstream (hs): wibbles.

--- src/hx-stream/hs/src/Data/HXStream/Test.hs
+++ src/hx-stream/hs/src/Data/HXStream/Test.hs
@@ -1,14 +1,14 @@
 
 module Data.HXStream.Test where
 
-import qualified Data.ByteString.Char8 as B
+import qualified Data.ByteString as B
 
 import Test.QuickCheck
 
 import Data.HXStream
 
 decodeEncoded :: [Frame] -> Bool
-decodeEncoded fs = fs' == fs
+decodeEncoded fs = fs' == map B.pack fs
   where
   encoded = foldl (\acc f -> B.append acc (encode f)) B.empty fs
   (fs',_) = decode encoded emptyStreamState
