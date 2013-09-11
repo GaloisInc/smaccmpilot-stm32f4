c72097de76dedef039e887a66c73f5826d643813
Pat Hickey <pat@galois.com>
2013-09-10 17:57:50 -0700

    flight: correct mixing of motors to px4fmu pwm channels

--- src/flight/SMACCMPilot/Flight/Motors/Platforms.hs
+++ src/flight/SMACCMPilot/Flight/Motors/Platforms.hs
@@ -40,11 +40,13 @@ fmuPwmMotorDecoder :: ConstRef s (Struct "motors")
                    -> Ivory (AllocEffects cs)
                          (ConstRef (Stack cs) (Array 4 (Stored IFloat)))
 fmuPwmMotorDecoder ms = do
-  m1 <- deref (ms ~> M.frontleft)
-  m2 <- deref (ms ~> M.frontright)
-  m3 <- deref (ms ~> M.backright)
-  m4 <- deref (ms ~> M.backleft)
-  l <- local (iarray [ival m1, ival m2, ival m3, ival m4]) -- XXX ORDER MIGHT BE WRONG!
+  fl <- deref (ms ~> M.frontleft) -- m3
+  fr <- deref (ms ~> M.frontright) -- m2
+  br <- deref (ms ~> M.backright) -- m4
+  bl <- deref (ms ~> M.backleft) -- m2
+  -- Canonical ArduCopter motor ordering, see:
+  -- http://copter.ardupilot.com/wiki/connecting-your-rc-input-and-motors/
+  l <- local (iarray [ival fr, ival bl, ival fl, ival br])
   return (constRef l)
 
 
