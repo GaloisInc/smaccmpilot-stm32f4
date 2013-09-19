f2b6f7a8bd1bb3f8d26236933f21b221a6333bf2
Pat Hickey <pat@galois.com>
2013-09-19 14:06:32 -0700

    flight sensors task uses new sensors capture interface

--- src/flight/SMACCMPilot/Flight/Sensors/Task.hs
+++ src/flight/SMACCMPilot/Flight/Sensors/Task.hs
@@ -19,6 +19,7 @@ sensorsTask :: forall n p
             => ChannelSource n (Struct "sensors_result")
             -> Task p ()
 sensorsTask s = do
+  m <- withGetTimeMillis
   sensorsEmitter <- withChannelEmitter s "sensors"
   withStackSize 1024
 
@@ -29,10 +30,39 @@ sensorsTask s = do
       -- time consuming: boots up and calibrates sensors
       call_ sensors_begin (sensorOrientation (Proxy :: Proxy p))
       return $ goto loop
+
     loop <- stateNamed "captureloop" $ period 10 $ liftIvory_ $ do
       call_ sensors_update
-      res <- local (istruct [])
-      call_ sensors_getstate res
+      time <- getTimeMillis m
+
+      (rpy :: Ref (Stack cs) (Array 3 (Stored IFloat))) <- local (iarray [])
+      call_ sensors_get_rpy (toCArray rpy)
+      roll  <- deref (rpy ! 0)
+      pitch <- deref (rpy ! 1)
+      yaw   <- deref (rpy ! 2)
+
+      (omega :: Ref (Stack cs) (Array 3 (Stored IFloat))) <- local (iarray [])
+      call_ sensors_get_omega (toCArray omega)
+      omega_x <- deref (omega ! 0)
+      omega_y <- deref (omega ! 1)
+      omega_z <- deref (omega ! 2)
+
+      baro_alt <- call sensors_get_baro_alt
+
+      res <- local $ istruct
+        [ S.valid     .= ival true
+        , S.roll      .= ival roll
+        , S.pitch     .= ival pitch
+        , S.yaw       .= ival yaw
+        , S.omega_x   .= ival omega_x
+        , S.omega_y   .= ival omega_y
+        , S.omega_z   .= ival omega_z
+        , S.baro_alt  .= ival baro_alt
+        , S.xacc      .= ival 0
+        , S.yacc      .= ival 0
+        , S.zacc      .= ival 0
+        , S.time      .= ival time
+        ]
       emit_ sensorsEmitter (constRef res)
     return init
   taskInit $ begin sm
