1bb41aab300c35c56ed3775db6c5183a24f32b7f
Lee Pike <leepike@gmail.com>
2013-10-03 14:29:16 -0700

    gcs: update script.

--- src/gcs/gcs.sh
+++ src/gcs/gcs.sh
@@ -2,12 +2,23 @@
 
 HOST=127.0.0.1
 PORT=6000
-COMMSEC_SERVER=../../../cabal-dev/bin/commsec-gcs
+COMMSEC_SERVER=commsec-gcs
+DIR=../../../cabal-dev/bin
 
 EXEC_MAVPROXY="python mavlink/mavproxy.py --master=tcp:$HOST:$PORT --baud=57600"
 
 echo $COMMSEC_SERVER
-$COMMSEC_SERVER&
+$DIR/$COMMSEC_SERVER&
 
 echo $EXEC_MAVPROXY
 gnome-terminal -x $EXEC_MAVPROXY
+
+function kill() {
+    read -p "$*"
+}
+
+# Wait for input.
+kill "Press [Enter] to quit ..."
+
+# Kill the script.
+killall $COMMSEC_SERVER
