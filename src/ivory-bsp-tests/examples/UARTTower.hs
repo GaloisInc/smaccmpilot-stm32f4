29bce55e762eb840f921554c7803ae3a7c819c7f
Lee Pike <leepike@gmail.com>
2013-12-02 20:02:04 -0800

    uart-tower-test: cleanups, comments.

--- src/ivory-bsp-tests/examples/UARTTower.hs
+++ src/ivory-bsp-tests/examples/UARTTower.hs
@@ -10,43 +10,44 @@ import Data.Char (ord)
 import Ivory.Language
 import Ivory.Stdlib
 import Ivory.Tower
-import Ivory.HW
-import Ivory.BitData
 
 import Platforms
-import qualified LEDTower
+import LEDTower
 
-import Ivory.BSP.STM32F4.Interrupt
-import Ivory.BSP.STM32F4.GPIO
 import Ivory.BSP.STM32F4.UART
-import Ivory.BSP.STM32F4.UART.Regs
 import Ivory.BSP.STM32F4.RCC
+
+--------------------------------------------------------------------------------
 
 app :: forall p . (ColoredLEDs p, BoardHSE p) => Tower p ()
 app = do
-  LEDTower.blinkApp period [blue]
-
+  -- Starts two tasks: a blink task and a controller task.  Periodically blink
+  -- the blue LED.
+  blinkApp period [blue]
+  -- A new queue
   redledctl <- channel
-
+  -- Starts a UART (serial) task
   (istream, ostream) <- uartTower uart5 115200
-
+  -- Start the task defined below
   echoPrompt "hello world" ostream istream (src redledctl)
-
-  task "settableLED" $ LEDTower.ledController [red] (snk redledctl)
-
+  -- A task that takes control input (Boolean) from the echo prompt and controls
+  -- the red LED based on it.
+  task "settableLED" $ ledController [red] (snk redledctl)
   where
   period = 333
-  -- red and blue leds provided through ColoredLEDs typeclass:
-  red = redLED (Proxy :: Proxy p)
+  -- red and blue LEDs provided through ColoredLEDs typeclass:
+  red  = redLED  (Proxy :: Proxy p)
   blue = blueLED (Proxy :: Proxy p)
+
+--------------------------------------------------------------------------------
 
 echoPrompt :: (SingI n)
            => String
            -> ChannelSource 128 (Stored Uint8)
            -> ChannelSink   128 (Stored Uint8)
-           -> ChannelSource n (Stored IBool)
+           -> ChannelSource n   (Stored IBool)
            -> Tower p ()
-echoPrompt greet ostream istream ledctlstream = task "echoprompt" $ do
+echoPrompt greeting ostream istream ledctlstream = task "echoprompt" $ do
   o      <- withChannelEmitter ostream      "ostream"
   ledctl <- withChannelEmitter ledctlstream "ledctl"
 
@@ -57,19 +58,19 @@ echoPrompt greet ostream istream ledctlstream = task "echoprompt" $ do
       putc = emitV_ o
 
   taskInit $ do
-    puts (greet ++ "\n")
-    puts "tower> "
+    puts (greeting ++ "\n")
+    puts prompt
   onChannelV istream "istream" $ \input -> do
     putc input -- echo to terminal
+    let testChar = (input `isChar`)
+    return ()
     cond_
-      [ input `isChar` '1' ==>
-          emitV_ ledctl true
-      , input `isChar` '2' ==>
-          emitV_ ledctl false
-      , input `isChar` '\n' ==>
-          puts "tower> "
+      [ testChar '1'  ==> emitV_ ledctl true
+      , testChar '2'  ==> emitV_ ledctl false
+      , testChar '\n' ==> puts prompt
       ]
+  where prompt = "tower> "
 
 isChar :: Uint8 -> Char -> IBool
-isChar b c = b ==? (fromIntegral (ord c))
+isChar b c = b ==? (fromIntegral $ ord c)
 
