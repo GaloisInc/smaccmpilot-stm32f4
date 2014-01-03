557385dad097e1a3e474e356f1bd5e405b386ff0
Pat Hickey <pat@galois.com>
2014-01-02 21:24:06 -0800

    expand poscontroldebug type

--- src/flight/SMACCMPilot/Flight/Types/PosControlDebug.hs
+++ src/flight/SMACCMPilot/Flight/Types/PosControlDebug.hs
@@ -22,9 +22,11 @@ struct pos_control_dbg
   ; lon_setpt   :: Stored Sint32
   ; x_deviation :: Stored IFloat
   ; y_deviation :: Stored IFloat
+  ; x_vel_est   :: Stored IFloat
   ; x_vel_p     :: Stored IFloat
   ; x_vel_i     :: Stored IFloat
   ; x_vel_d     :: Stored IFloat
+  ; y_vel_est   :: Stored IFloat
   ; y_vel_p     :: Stored IFloat
   ; y_vel_i     :: Stored IFloat
   ; y_vel_d     :: Stored IFloat
