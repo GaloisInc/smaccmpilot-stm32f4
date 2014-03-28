fdb63815737b9ec0ce675132a1e88dde319083cb
Pat Hickey <pat@galois.com>
2014-03-27 17:06:04 -0700

    ivory-bsp-stm32f4: correct implementation of UART.Tower

--- src/ivory-bsp-stm32f4/src/Ivory/BSP/STM32F4/UART/Tower.hs
+++ src/ivory-bsp-stm32f4/src/Ivory/BSP/STM32F4/UART/Tower.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module Ivory.BSP.STM32F4.UART.Tower where
 
@@ -10,55 +11,88 @@ import GHC.TypeLits
 import Ivory.Language
 import Ivory.Stdlib
 import Ivory.Tower
+import Ivory.Tower.Signal (withUnsafeSignalEvent)
 import Ivory.HW
 import Ivory.HW.Module
 import Ivory.BitData
 
-import Ivory.BSP.STM32F4.Interrupt
+import Ivory.BSP.STM32F4.Signalable
 import Ivory.BSP.STM32F4.UART.Regs
 import Ivory.BSP.STM32F4.UART.Peripheral
 import Ivory.BSP.STM32F4.RCC
+import Ivory.BSP.STM32F4.Interrupt
 
-uartTower :: forall n m p
-           . (SingI n, SingI m, BoardHSE p)
+uartTower :: forall n p
+           . (SingI n, BoardHSE p, STM32F4Signal p)
           => UART
           -> Integer
-          -> Tower p ( ChannelSink   n (Stored Uint8)
-                     , ChannelSource m (Stored Uint8))
-uartTower uart baud = do
-  -- XXX MAGIC NUMBER: freertos syscalls must be lower (numerically greater than) level 11
+          -> Proxy (n :: Nat)
+          -> Tower p ( ChannelSink   (Stored Uint8)
+                     , ChannelSource (Stored Uint8))
+uartTower uart baud sizeproxy = do
+  -- MAGIC NUMBER: freertos syscalls must be lower (numerically greater
+  -- than) level 11
   let max_syscall_priority = (12::Uint8)
 
-  (src_ostream, snk_ostream) <- channelWithSize
-  (src_istream, snk_istream) <- channelWithSize
+  (src_ostream, snk_ostream) <- channelWithSize sizeproxy
+  (src_istream, snk_istream) <- channelWithSize sizeproxy
 
-  let user_src_ostream = channelSourceCallback
-                            (const (setTXEIE uart true))
-                             hw_moduledef
-                             src_ostream
-  -- Signal:
-  -- runs the UART Interrupt Service Routine
-  signal (uartName uart ++ "ISR") $ do
-    signalInit $ do
-      uartInit    uart (Proxy :: Proxy p) (fromIntegral baud)
-      uartInitISR uart max_syscall_priority
+  task (uartName uart ++ "_driver") $ do
+
     o <- withChannelReceiver snk_ostream "ostream"
     i <- withChannelEmitter  src_istream "istream"
-    signalName (handlerName (uartInterrupt uart))
-    signalModuleDef $ hw_moduledef
-    signalBody $ do
+
+    taskModuleDef $ hw_moduledef
+
+    txpending     <- taskLocal "txpending"
+    txpendingbyte <- taskLocal "txpendingbyte"
+
+    txcheck <- timerEvent txcheck_period
+    interrupt <- withUnsafeSignalEvent
+      (stm32f4Interrupt (uartInterrupt uart))
+      (uartName uart ++ "_isr")
+      (interrupt_disable (uartInterrupt uart))
+
+    taskInit $ do
+      store txpending false
+      uartInit    uart (Proxy :: Proxy p) (fromIntegral baud)
+      uartInitISR uart max_syscall_priority
+
+    handle interrupt "interrupt" $ \_msg -> do
       sr <- getReg (uartRegSR uart)
       cond_
        [ bitToBool (sr #. uart_sr_rxne) ==> do
            byte <- readDR uart
            bref <- local (ival byte)
            emit_ i (constRef bref)
-       , bitToBool (sr #. uart_sr_txe)  ==> do
-           byte <- local (ival 0)
-           rv   <- receive o byte
-           ifte_ rv
-             (setDR uart =<< deref byte)
-             (setTXEIE uart false)
+       , bitToBool (sr #. uart_sr_txe) ==> do
+           pending <- deref txpending
+           ifte_ pending
+             (do store txpending false
+                 setDR uart =<< deref txpendingbyte)
+             (do byte <- local (ival 0)
+                 rv   <- receive o byte
+                 ifte_ rv
+                   (setDR uart =<< deref byte)
+                   (setTXEIE uart false))
        ]
+      interrupt_enable (uartInterrupt uart)
 
-  return (snk_istream, user_src_ostream)
+    handle txcheck "txcheck" $ \_ -> do
+      txeie <- getTXEIE uart
+      pending <- deref txpending
+      unless (txeie .&& iNot pending) $ do
+        byte <- local (ival 0)
+        txready <- receive o byte
+        when txready $ do
+          store txpending true
+          store txpendingbyte =<< deref byte
+          setTXEIE uart true
+
+
+
+  return (snk_istream, src_ostream)
+
+  where
+  txcheck_period = Milliseconds 1
+
