7898fc25619bf429b7a8529bd76b4b2f875b5d0f
Pat Hickey <pat@galois.com>
2014-04-08 17:26:14 -0700

    ivory-bsp-tests: using uart stress test to debug timing with logic analyzer

--- src/ivory-bsp-tests/examples/UARTStressTest.hs
+++ src/ivory-bsp-tests/examples/UARTStressTest.hs
@@ -39,14 +39,14 @@ garbage_vals :: Uint8
 garbage_vals = 4
 
 
-loopbackMonitor :: ChannelSource (Stored Uint8)
+pollingLoopback :: ChannelSource (Stored Uint8)
                 -> ChannelSink (Stored Uint8)
                 -> GPIOPin
                 -> String
                 -> Uint8
                 -> Tower p ()
-loopbackMonitor src snk p n correctval = task (named "loopbackMonitor") $ do
-  timer <- timerEvent (Milliseconds 2)
+pollingLoopback src snk handlepin n correctval = task (named "pollingLoopback") $ do
+  timer <- timerEvent (Milliseconds 1)
   out <- withChannelEmitter  src "out"
   inp <- withChannelReceiver snk "in"
 
@@ -55,8 +55,7 @@ loopbackMonitor src snk p n correctval = task (named "loopbackMonitor") $ do
   time    <- taskLocal     (named "time")
 
   taskInit $ do
-    ledSetup
-    ledOff
+    gpioSetup handlepin
 
   handleV timer "periodic" $ \t -> do
     s <- deref state
@@ -68,70 +67,92 @@ loopbackMonitor src snk p n correctval = task (named "loopbackMonitor") $ do
             return ()
           -- Send one byte:
           emitV_ out correctval
+          gpioOn handlepin
           store time t
           store state sent
 
       , s ==? sent ==> do
           sendtime <- deref time
-          when (t >? (sendtime + (fromIMilliseconds (2 :: Sint64)))) $ do
-            (rxd, val) <- receiveV inp
-            when rxd $ do
+          (rxd, val) <- receiveV inp
+          ifte_ rxd
+            (do store time t
+                gpioOff handlepin
+                ifte_ (val ==? correctval)
+                  (store state got)
+                  (store state failed_badval))
+            (when (t >? (sendtime + (fromIMilliseconds (2 :: Sint64)))) $ do
               store time t
-              ifte_ (val ==? correctval)
-                (store state got)
-                (store state failed_badval)
-          when (t >? (sendtime + (fromIMilliseconds (4 :: Sint64)))) $ do
-            store time t
-            (store state failed_timeout)
+              (store state failed_timeout))
 
       , s ==? got  ==> do
-          gottime <- deref time
-          when (t >? (gottime + (fromIMilliseconds (4 :: Sint64)))) $ do
-            a <- local (ival false)
-            arrayMap $ \(_v :: Ix 99) -> do
-              (rxd, _) <- receiveV inp
-              when rxd $ store a true
-            anythingelse <- deref a
-            ifte_ anythingelse
-              (store state garbage_vals)
-              (ledOn >> success %= (+1) >> store state ready)
-      , s ==? failed_badval ==> do
-          ledOff
-      , s ==? failed_timeout ==> do
-          ledOff
-      , s ==? garbage_vals ==> do
-          ledOff
+          a <- local (ival false)
+          arrayMap $ \(_v :: Ix 99) -> do
+            (rxd, _) <- receiveV inp
+            when rxd $ store a true
+          anythingelse <- deref a
+          ifte_ anythingelse
+            (store state garbage_vals)
+            (do success %= (+1)
+                store state ready)
+
+      , true ==> gpioOff handlepin
       ]
   where
   named s = n ++ "_" ++ s
-  ledSetup :: Ivory eff ()
-  ledSetup = do
-    pinEnable        p
-    pinSetOutputType p gpio_outputtype_pushpull
-    pinSetSpeed      p gpio_speed_50mhz
-    pinSetPUPD       p gpio_pupd_none
-  ledOff :: Ivory eff ()
-  ledOff = do
-    -- pinSetMode       p gpio_mode_analog
-    pinClear         p
-    pinSetMode       p gpio_mode_output
-  ledOn :: Ivory eff ()
-  ledOn = do
-    pinSet           p
-    pinSetMode       p gpio_mode_output
+
+gpioSetup :: GPIOPin -> Ivory eff ()
+gpioSetup p = do
+  pinEnable        p
+  pinSetOutputType p gpio_outputtype_pushpull
+  pinSetSpeed      p gpio_speed_50mhz
+  pinSetPUPD       p gpio_pupd_none
+  gpioOff          p
+gpioOff :: GPIOPin -> Ivory eff ()
+gpioOff p = do
+  pinClear         p
+  pinSetMode       p gpio_mode_output
+gpioOn :: GPIOPin -> Ivory eff ()
+gpioOn p = do
+  pinSet           p
+  pinSetMode       p gpio_mode_output
 
 app :: forall p . (ColoredLEDs p, BoardHSE p, STM32F4Signal p) => Tower p ()
 app = do
   -- XXX doesn't uartTower return source, sink in the wrong pairing?
 
-  uart3streams <- uartTower uart3 57600 (Proxy :: Proxy 256)
-  uart4streams <- uartTower uart4 57600 (Proxy :: Proxy 256)
+--  uart3streams <- uartTower uart3 57600 (Proxy :: Proxy 256)
+--  uart4streams <- uartTower uart4 57600 (Proxy :: Proxy 256)
 
-  (u1snk, u1src) <- uartTower uart1 57600 (Proxy :: Proxy 256)
-  loopbackMonitor u1src u1snk pinD12 "uart1" 0x66
+  (u1snk, u1src) <- uartTowerDebuggable uart1 57600 (Proxy :: Proxy 256) UARTTowerDebugger
+                      { debug_init = do
+                          gpioSetup pinD3
+                          gpioSetup pinD4
+                          gpioSetup pinD5
+                          gpioSetup pinD6
+                          gpioSetup pinD7
+                      , debug_isr = do
+                          gpioOn  pinD3
+                          gpioOff pinD3
+                      , debug_evthandler_start = do
+                          gpioOn  pinD4
+                      , debug_evthandler_end = do
+                          gpioOff pinD4
+                      , debug_txcheck = do
+                          gpioOn  pinD5
+                          gpioOff pinD5
+                      , debug_txcheck_pend = do
+                          gpioOn  pinD6
+                          gpioOff pinD6
+                      , debug_txeie = \en -> do
+                          ifte_ en
+                            (gpioOn  pinD7)
+                            (gpioOff pinD7)
+                      }
 
-  (u2snk, u2src) <- uartTower uart2 57600 (Proxy :: Proxy 256)
-  loopbackMonitor u2src u2snk pinD13 "uart2" 0x77
+  pollingLoopback u1src u1snk pinD12 "uart1" 0x66
+
+  --(u2snk, u2src) <- uartTower uart2 57600 (Proxy :: Proxy 256)
+  --pollingLoopback u2src u2snk pinD13 "uart2" 0x77
 
 
 
