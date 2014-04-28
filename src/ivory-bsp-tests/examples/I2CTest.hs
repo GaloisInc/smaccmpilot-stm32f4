5dd0955fc893573e479a3ac286a35dd6ed755d3a
Pat Hickey <pat@galois.com>
2014-04-28 11:26:34 -0700

    ivory-bsp-tests i2ctest: eeprom read/write looks good

--- src/ivory-bsp-tests/examples/I2CTest.hs
+++ src/ivory-bsp-tests/examples/I2CTest.hs
@@ -28,17 +28,16 @@ app = do
     handleV periodic "periodic" $ \p -> do
       ifte_ ((p .% 500000) >=? 250000)
         (do r <- local $ istruct
-                   -- Write values 0xF2, 0xF3 to page 0
+                   -- Write values 0xF2, 0xF3, 0xEE to page 0
                    [ tx_addr   .= ival 0x50
-                   , tx_buf    .= iarray [ival 0x00, ival 0xF2, ival 0xF3]
-                   , tx_len    .= ival 3
+                   , tx_buf    .= iarray [ival 0x00, ival 0xF2, ival 0xF3, ival 0xEE]
+                   , tx_len    .= ival 4
                    , rx_len    .= ival 0
                    ]
             emit_ req_emitter (constRef r))
         (do r <- local $ istruct
                    [ tx_addr   .= ival 0x50
-                   -- Read 3 values out of page 0 (should be 0xF2, 0xF3, and
-                   -- anything)
+                   -- Read 3 values out of page 0 (should be 0xF2, 0xF3, 0xEE)
                    , tx_buf    .= iarray [ival 0x00]
                    , tx_len    .= ival 1
                    , rx_len    .= ival 3
