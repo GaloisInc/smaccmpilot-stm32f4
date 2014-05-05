42e3d2ae7c0e56ea44f39fe7e6d7beb47394718c
Pat Hickey <pat@galois.com>
2014-05-05 15:02:46 -0700

    ivory-px4-hw: create test for ms5611 driver

---
+++ src/ivory-px4-hw/px4-ms5611-test/Main.hs
@@ -0,0 +1,74 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE RecursiveDo #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module Main where
+
+import Ivory.Language
+import Ivory.Stdlib
+
+import Ivory.Tower
+import Ivory.Tower.StateMachine
+import Ivory.Tower.Frontend
+
+import Ivory.BSP.STM32F4.RCC (BoardHSE)
+import qualified Ivory.HW.SearchDir          as HW
+import qualified Ivory.BSP.STM32F4.SearchDir as BSP
+
+import Ivory.BSP.STM32F4.UART.Tower
+import Ivory.BSP.STM32F4.GPIO
+import Ivory.BSP.STM32F4.I2C
+import Ivory.BSP.STM32F4.Signalable
+
+import SMACCMPilot.Hardware.MS5611.I2C
+
+import Platform
+
+main :: IO ()
+main = compilePlatforms conf (gpsPlatforms app)
+  where
+  conf = searchPathConf [ HW.searchDir, BSP.searchDir ]
+
+app :: forall p . (MPU6kPlatform p, BoardHSE p, STM32F4Signal p) => Tower p ()
+app = do
+  --towerModule  rawSensorTypeModule
+  --towerDepends rawSensorTypeModule
+
+  --raw_sensor <- channel
+
+  (_consIn,_consOut) <- uartTower (consoleUart (Proxy :: Proxy p))
+                                115200 (Proxy :: Proxy 128)
+
+  (req, res) <- i2cTower i2c2 pinB10 pinB11
+
+  -- 0b01110110 = 0x76
+  ms5611ctl req res (I2CDeviceAddr 0x76)
+
+  -- XXX hook snk raw_sensor up to console.
+
+ms5611ctl :: forall p
+        . (BoardHSE p, STM32F4Signal p)
+       => ChannelSource (Struct "i2c_transaction_request")
+       -> ChannelSink   (Struct "i2c_transaction_result")
+       -> I2CDeviceAddr
+       -> Tower p ()
+ms5611ctl toDriver fromDriver addr = task "ms5611ctl" $ do
+  i2cRequest <- withChannelEmitter toDriver "toDriver"
+  i2cResult <- withChannelEvent fromDriver "fromDriver"
+  --sensorEmitter <- withChannelEmitter sensorSource "sensorOutput"
+
+  (initMachine, deviceprops) <- initializerMachine addr i2cRequest i2cResult
+
+  taskStackSize 2048
+
+  taskInit $ do
+    begin initMachine
+
+  p <- withPeriodicEvent (Milliseconds 5) -- 200hz
+  handle p "period" $ \_ -> do
+    initializing <- active initMachine
+    unless initializing $ do
+      e <- deref (init_failure deviceprops)
+      comment "placeholder"
