4a821eb13fc13ce8ca1bd0381844f7034fd60775
Pat Hickey <pat@galois.com>
2014-05-05 16:38:10 -0700

    ivory-bsp-stm32f4: fix typo bug in i2c2 definition!

--- src/ivory-bsp-stm32f4/src/Ivory/BSP/STM32F4/I2C/Peripheral.hs
+++ src/ivory-bsp-stm32f4/src/Ivory/BSP/STM32F4/I2C/Peripheral.hs
@@ -73,7 +73,7 @@ i2c1 :: I2CPeriph
 i2c1 = mkI2CPeriph i2c1_periph_base rcc_apb1en_i2c1 I2C1_EV I2C1_ER "i2c1"
 
 i2c2 :: I2CPeriph
-i2c2 = mkI2CPeriph i2c3_periph_base rcc_apb1en_i2c2 I2C2_EV I2C2_ER "i2c2"
+i2c2 = mkI2CPeriph i2c2_periph_base rcc_apb1en_i2c2 I2C2_EV I2C2_ER "i2c2"
 
 i2c3 :: I2CPeriph
 i2c3 = mkI2CPeriph i2c3_periph_base rcc_apb1en_i2c3 I2C3_EV I2C3_ER "i2c3"
