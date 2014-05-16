d179abd000f405d4ab7760af2414572a18d6cd1b
Pat Hickey <pat@galois.com>
2014-05-16 09:11:47 -0700

    ivory-bsp-stm32: fixes to generated vector table

--- src/ivory-bsp-stm32/src/Ivory/BSP/STM32F4/VectorTable.hs
+++ src/ivory-bsp-stm32/src/Ivory/BSP/STM32F4/VectorTable.hs
@@ -3,7 +3,11 @@ module Ivory.BSP.STM32F4.VectorTable
   ( vector_table
   ) where
 
+import Ivory.BSP.ARMv7M.Exception
 import Ivory.BSP.STM32F4.Interrupt
+
+reset_handler :: String
+reset_handler = exceptionHandlerName Reset
 
 vector_table :: String
 vector_table = unlines $
@@ -13,21 +17,23 @@ vector_table = unlines $
   , "\t.thumb"
   , "\t.cpu cortex-m3"
   , "\t.fpu softvfp"
-
-  , ".global Reset_Handler"
+  , ""
+  , ""
+  , ".global " ++ reset_handler
   , ".global g_vectors"
   , ".word _sidata"
   , ".word _sdata"
   , ".word _edata"
   , ".word _sbss"
   , ".word _ebss"
-
+  , ""
+  , ""
   , "\t.section .text.defaultExceptionHandler, \"ax\",%progbits"
   , "defaultExceptionHandler:"
   , "\tb defaultExceptionHandler"
   , "\t.size defaultExceptionHandler, .-defaultExceptionHandler"
   , ""
-  , "\t.section .isr_vectors,\"ax\""
+  , "\t.section .isr_vector,\"ax\""
   , "\t.code    16"
   , "\t.align   2"
   , "\t.globl   g_vectors"
@@ -35,21 +41,22 @@ vector_table = unlines $
   , ""
   , "g_vectors:"
   , ""
-  , entry "(_estack+0x400)"
-  , entry "Reset_Handler"
+  , "\t.word _estack"
   ] ++
-  map (entry . exceptionHandlerName) (enumFrom NonMaskable) ++
-  map (entry . interruptHandlerName) (enumFrom WWDG) ++
+  map (entry . (fmap exceptionHandlerName)) exceptionTable ++ -- XXX freertos names needed for some of these.
+  map (entry . (fmap interruptHandlerName)) interruptTable ++
   [ ""
   , "\t.size g_vectors, .-g_vectors"
   , ""
   ] ++
-  map (weakdef . exceptionHandlerName) (enumFrom NonMaskable) ++
-  map (weakdef . interruptHandlerName) (enumFrom WWDG)
+  map (weakdef . (fmap exceptionHandlerName)) (drop 1 exceptionTable) ++
+  map (weakdef . (fmap interruptHandlerName)) interruptTable
 
   where
-  entry e = "\t.word " ++ e
-  weakdef handler = "\t.weak " ++ handler ++
+  entry (Just e) = "\t.word " ++ e
+  entry Nothing  = "\t.word 0"
+  weakdef (Just handler) = "\t.weak " ++ handler ++
     "\n\t.thumb_set " ++ handler ++ ",defaultExceptionHandler\n"
+  weakdef Nothing = ""
 
 
