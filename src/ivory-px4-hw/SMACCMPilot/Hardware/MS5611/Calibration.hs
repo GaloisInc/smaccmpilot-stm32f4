a3abc33b7f299265b243345522fcd43732c5636d
Pat Hickey <pat@galois.com>
2014-06-27 18:19:02 -0700

    ivory-px4-hw: add MS5611 calibration code
    untested

---
+++ src/ivory-px4-hw/SMACCMPilot/Hardware/MS5611/Calibration.hs
@@ -0,0 +1,73 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module SMACCMPilot.Hardware.MS5611.Calibration where
+
+import Ivory.Language
+import SMACCMPilot.Hardware.MS5611.Types
+
+measurement :: ConstRef s1 (Struct "ms5611_calibration")
+            -> ConstRef s2 (Struct "ms5611_sample")
+            -> Ref s3 (Struct "ms5611_measurement")
+            -> Ivory eff ()
+measurement cal sample meas = do
+  c1 <- deref (cal ~> coeff1)
+  c2 <- deref (cal ~> coeff2)
+  c3 <- deref (cal ~> coeff3)
+  c4 <- deref (cal ~> coeff4)
+  c5 <- deref (cal ~> coeff5)
+  c6 <- deref (cal ~> coeff6)
+
+  d1 <- deref (sample ~> sample_pressure)
+  d2 <- deref (sample ~> sample_temperature)
+
+  -- Step 1: calculate temperature
+  let -- Its safe to promote d2 to a positive signed number, because
+      -- it only uses the low 24 bits of the uint32.
+      d2_signed :: Sint32
+      d2_signed = signCast d2
+      -- It is always safe to promote a Uint16 to a Sint32.
+      c5_sint32 :: Sint32
+      c5_sint32 = safeCast c5
+
+  (dT :: Sint32) <- assign (d2_signed - (c5_sint32 * (2^(8::Integer))))
+
+  let twentyC :: Sint32
+      twentyC = 2000
+      -- intermediate value has 41 sig bits, will shift down to fit into 32
+      c6_sint64 :: Sint64
+      c6_sint64 = safeCast c6
+      ts64 :: Sint32 -> Sint64
+      ts64 t = (safeCast t * c6_sint64) `iDiv` (2^(23::Integer))
+      tempsense :: Sint32 -> Sint32
+      tempsense t = castWith 0 (ts64 t)
+
+  (temp :: Sint32) <- assign (twentyC + tempsense dT)
+
+  let c2_sint64 :: Sint64
+      c2_sint64 = safeCast c2
+      c4_sint64 :: Sint64
+      c4_sint64 = safeCast c4
+      dT_sint64 :: Sint64
+      dT_sint64 = safeCast dT
+  (off :: Sint64) <- assign ( (c2_sint64 * 2^(16::Integer))
+                            + ((c4_sint64 * dT_sint64) `iDiv` (2^(7::Integer))))
+  let c1_sint64 :: Sint64
+      c1_sint64 = safeCast c1
+      c3_sint64 :: Sint64
+      c3_sint64 = safeCast c3
+  (sens :: Sint64) <- assign ( (c1_sint64 * 2^(15::Integer))
+                             + ((c3_sint64 * dT_sint64) `iDiv` (2^(8::Integer))))
+
+  let d1_sint64 :: Sint64
+      d1_sint64 = safeCast d1
+      pres_64 = ((((d1_sint64 * sens) `iDiv` (2^(21::Integer)))
+                      - off) `iDiv` (2^(15::Integer)))
+  (pres :: Sint32) <- assign (castWith 0 pres_64)
+
+  t <- deref (sample ~> sample_time)
+
+  store (meas ~> pressure)    ((safeCast pres) / 100)
+  store (meas ~> temperature) ((safeCast temp) / 100)
+  store (meas ~> time)        t
