3919be24ad985d5e18195e290feda93d639a1c50
Pat Hickey <pat@galois.com>
2014-07-02 18:45:45 -0700

    ivory-px4-tests: mpu6k test outputs serialized struct
    - but crashes after outputting a dozen or so of them.
      need to investigate!

--- src/ivory-px4-tests/PX4/Tests/MPU6000.hs
+++ src/ivory-px4-tests/PX4/Tests/MPU6000.hs
@@ -9,11 +9,14 @@ module PX4.Tests.MPU6000 (mpu6000SensorManager, testApp) where
 
 import Ivory.Language
 import Ivory.Stdlib
+import Ivory.Serialize
+import qualified Ivory.HXStream as HX
 
 import Ivory.Tower
 import Ivory.Tower.StateMachine
 
 import Ivory.BSP.STM32.Driver.SPI
+import Ivory.BSP.STM32.Driver.UART
 
 import SMACCMPilot.Hardware.MPU6000
 
@@ -27,10 +30,40 @@ testApp = do
   (req, res) <- spiTower [mpu6000Device platform]
   mpu6000SensorManager req res (src sample) (SPIDeviceHandle 0)
 
+  (_uarti, uarto) <- uartTower (consoleUart platform) 115200 (Proxy :: Proxy 256)
+  task "mpu6000sender" $ do
+    uartout <- withChannelEmitter uarto "uartout"
+    mpu6000Sender (snk sample) uartout
+
+  towerDepends serializeModule
+  towerModule  serializeModule
   towerModule  mpu6000TypesModule
   towerDepends mpu6000TypesModule
   where
   platform = Proxy :: Proxy p
+
+mpu6000Sender :: ChannelSink (Struct "mpu6000_sample")
+              -> ChannelEmitter (Stored Uint8)
+              -> Task p ()
+mpu6000Sender meassink out = do
+  meas <- withChannelEvent meassink "measurement"
+  (buf :: Ref Global (Array 23 (Stored Uint8))) <- taskLocal "mpu6000_ser_buf"
+  handle meas "measurement" $ \s -> noReturn $ do
+    sfail <- deref (s ~> valid)
+    stime <- deref (s ~> time)
+    packInto_ buf 0 $ do
+      mpackV (sfail ? ((1 :: Uint8), 0))
+      mpack  (s ~> gyro_x)
+      mpack  (s ~> gyro_y)
+      mpack  (s ~> gyro_z)
+      mpack  (s ~> accel_x)
+      mpack  (s ~> accel_y)
+      mpack  (s ~> accel_z)
+      mpack  (s ~> temp)
+      mpackV (toIMicroseconds stime)
+    HX.encode tag (constRef buf) (emitV_ out)
+  where
+  tag = 103 -- 'g' for gyro
 
 mpu6000SensorManager :: ChannelSource (Struct "spi_transaction_request")
                      -> ChannelSink   (Struct "spi_transaction_result")
