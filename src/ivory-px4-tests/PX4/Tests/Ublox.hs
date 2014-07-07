1928242d3177b4b96be7e8be944236a0743cdcb7
Pat Hickey <pat@galois.com>
2014-07-07 16:40:21 -0700

    ivory-px4-tests: ublox test changed to use serialization

--- src/ivory-px4-tests/PX4/Tests/Ublox.hs
+++ src/ivory-px4-tests/PX4/Tests/Ublox.hs
@@ -7,13 +7,11 @@
 
 module PX4.Tests.Ublox (app) where
 
-import Data.Char (ord)
-
 import Ivory.Language
-import Ivory.Stdlib
+import Ivory.Serialize
+import qualified Ivory.HXStream as HX
 
 import Ivory.Tower
-import Ivory.Tower.StateMachine
 
 import Ivory.BSP.STM32.Driver.UART
 
@@ -26,108 +24,42 @@ import PX4.Tests.Platforms
 app :: forall p . (TestPlatform p) => Tower p ()
 app = do
   boardInitializer
-  (shelli,shello ) <- uartTower (consoleUart (Proxy :: Proxy p))
+  (_uarti,uarto) <- uartTower (consoleUart (Proxy :: Proxy p))
                                 115200 (Proxy :: Proxy 128)
   (gpsi, _gpso) <- uartTower (gpsUart (Proxy :: Proxy p))
                                 38400 (Proxy :: Proxy 128)
   position <- channel
-  shell "gps test shell, console." shello shelli (snk position)
   ubloxGPSTower gpsi (src position)
+  task "positionSender" $ do
+    uartout <- withChannelEmitter uarto "uartout"
+    positionSender (snk position) uartout
 
-forward :: (IvoryArea a, IvoryZero a)
-        => ChannelSource a
-        -> ChannelSink   a
-        -> Tower p ()
-forward osrc isnk = task "forward" $ do
-  o <- withChannelEmitter  osrc "ostream"
-  i <- withChannelEvent    isnk "istream"
-  handle i "forwarding" (emit_ o)
+  towerDepends serializeModule
+  towerModule  serializeModule
 
-
-shell :: String
-      -> ChannelSource (Stored Uint8)
-      -> ChannelSink   (Stored Uint8)
-      -> ChannelSink   (Struct "position")
-      -> Tower p ()
-shell greet ostream istream ipos = task "shell" $ do
-  out <- withChannelEmitter  ostream "ostream"
-  inp <- withChannelEvent    istream "istream"
-  posin <- withChannelEvent  ipos    "positionin"
-  -- withStackSize 1024 -- XXX
-  let puts :: (GetAlloc eff ~ Scope s) => [Char] -> Ivory eff ()
-      puts str = mapM_ (\c -> putc (fromIntegral (ord c))) str
-      putc :: (GetAlloc eff ~ Scope s) => Uint8 -> Ivory eff ()
-      putc c = emitV_ out c
-
-  sm <- stateMachine "motor_shell" $ mdo
-    init <- stateNamed "init" $ entry $ do
-      liftIvory_ $ puts (greet ++ "\n")
-      goto prompt
-
-    prompt <- stateNamed "prompt" $ do
-      entry $ liftIvory_ $ puts "> "
-      on inp $ \inref -> liftIvory $ do
-        i <- deref inref
-        putc i -- Echo
-        return $ do
-          branch (i `isChar` '?')   help
-          branch (i `isChar` 'f')   showfix
-          branch (i `isChar` 's')   showsats
-          branch (i `isChar` '\n')  prompt
-          goto unknown
-
-    showsats <- stateNamed "sats" $ do
-      entry $ liftIvory_ $
-        puts "\nshowing number of gps satellites, any key to exit\n"
-      on inp $ \_ -> goto prompt
-      on posin $ \p -> liftIvory_ $ do
-        numsv <- deref (p ~> P.num_sv)
-        puts "# "
-        ifte_ (numsv <? 10)
-              (putc (itoa numsv))
-              (puts "9+")
-        puts "\n"
-
-    showfix <- stateNamed "fix" $ do
-      entry $ liftIvory_ $
-        puts "\nshowing gps fix, any key to exit\n"
-      on inp $ \_ -> goto prompt
-      on posin $ \p -> liftIvory_ $ do
-        f <- deref (p ~> P.fix)
-        cond_ [ f ==? fix_none ==> puts "no fix\n"
-              , f ==? fix_2d   ==> puts "2d fix\n"
-              , f ==? fix_3d   ==> puts "3d fix\n"
-              ]
-
-    unknown <- stateNamed "unknown" $ do
-      entry $ do
-        liftIvory_ $ puts "\n\nunknown command"
-        goto help
-
-    help <- stateNamed "help" $ do
-      entry $ do
-        liftIvory_ $ puts helpmsg
-        goto prompt
-
-    return init
-
-  taskInit $ begin sm
-
-helpmsg :: String
-helpmsg = unlines $
-  [ ""
-  , "Help:"
-  , "f: show gps fix status"
-  , "s: show number of satellites visible"
-  , "? for help"
-  ]
-
-isChar :: Uint8 -> Char -> IBool
-isChar b c = b ==? (fromIntegral (ord c))
-
--- Hacky and unsafe...
-itoa :: Uint8 -> Uint8
-itoa i = i + (fromIntegral (ord '0'))
-atoi :: Uint8 -> Uint8
-atoi a = a - (fromIntegral (ord '0'))
+positionSender :: ChannelSink (Struct "position")
+               -> ChannelEmitter (Stored Uint8)
+               -> Task p ()
+positionSender possink out = do
+  pos <- withChannelEvent possink "position"
+  (buf :: Ref Global (Array 46 (Stored Uint8))) <- taskLocal "pos_ser_buf"
+  handle pos "position_serialize" $ \p -> noReturn $ do
+    f     <- deref (p ~> fix)
+    t     <- deref (p ~> time)
+    packInto_ buf 0 $ do
+      mpackV ((f ==? fix_none) ? (0, (f ==? fix_2d) ? (2, 3 :: Uint8)))
+      mpack  (p ~> num_sv)
+      mpack  (p ~> dop)
+      mpack  (p ~> lat)
+      mpack  (p ~> lon)
+      mpack  (p ~> alt)
+      mpack  (p ~> vnorth)
+      mpack  (p ~> veast)
+      mpack  (p ~> vdown)
+      mpack  (p ~> vground)
+      mpack  (p ~> heading)
+      mpackV (toIMicroseconds t)
+    HX.encode tag (constRef buf) (emitV_ out)
+  where
+  tag = 112 -- 'p' for position
 
