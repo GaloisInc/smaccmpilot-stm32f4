d3cb1b3f855082b1de9a2a0c2d9ebd8799f3f20b
Jamey Sharp <jamey@galois.com>
2014-09-27 21:18:44 -0700

    Rewrite matInvert: Model vectors as [a] and scalars as (a).

--- Matrix.hs
+++ Matrix.hs
@@ -17,21 +17,23 @@ matMult a b = [ [ dotp l r | r <- transpose b ] | l <- a ]
 matVecMult :: Num a => [[a]] -> [a] -> [a]
 matVecMult m v = [ dotp r v | r <- m ]
 
+vecMatMult :: Num a => [a] -> [[a]] -> [a]
+vecMatMult v m = [ dotp v c | c <- transpose m ]
+
 diagMat :: Num a => [a] -> [[a]]
 diagMat = foldr push []
     where
     push v [] = [[v]]
     push v rs@(r:_) = (v : map (const 0) r) : map (0 :) rs
 
-msplit :: [[a]] -> (a, [[a]], [[a]], [[a]])
-msplit (row:rows) = (first, [top], left, rest)
+msplit :: [[a]] -> (a, [a], [a], [[a]])
+msplit (row:rows) = (first, top, left, rest)
     where
     first : top = row
-    (left, rest) = unzip $ map (\(x:xs)-> ([x], xs)) rows
+    (left, rest) = unzip $ map (\(x:xs)-> (x, xs)) rows
 
-mjoin :: (a, [[a]], [[a]], [[a]]) -> [[a]]
-mjoin (first, top, left, rest) = ([[first]] `hcat` top) ++ (left `hcat` rest)
-    where hcat = zipWith (++)
+mjoin :: (a, [a], [a], [[a]]) -> [[a]]
+mjoin (first, top, left, rest) = (first : top) : (zipWith (:) left rest)
 
 matInvert :: Fractional a => [[a]] -> [[a]]
 matInvert [] = []
@@ -40,11 +42,9 @@ matInvert m = mjoin (a', b', c', d')
     where
     (a, b, c, d) = msplit m
     aInv = recip a
-    caInv = matUnOp (* aInv) c
-    aInvb = matUnOp (aInv *) b
-    d' = matInvert $ matBinOp (-) d $ matMult c aInvb
-    c' = matUnOp negate $ matMult d' caInv
-    b' = matUnOp negate $ matMult aInvb d'
-    a' = aInv + case matMult aInvb $ matMult d' caInv of
-        [[]] -> 0
-        [[v]] -> v
+    caInv = fmap (* aInv) c
+    aInvb = fmap (aInv *) b
+    d' = matInvert $ matBinOp (-) d $ fmap (\ v -> fmap (v *) aInvb) c
+    c' = fmap negate $ matVecMult d' caInv
+    b' = fmap negate $ vecMatMult aInvb d'
+    a' = aInv + dotp aInvb (matVecMult d' caInv)
