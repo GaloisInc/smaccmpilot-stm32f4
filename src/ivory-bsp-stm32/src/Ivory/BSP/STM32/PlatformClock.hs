d2fe1b1152c1998abaae1a93452997380e96125f
Pat Hickey <pat@galois.com>
2014-11-06 14:45:05 -0800

    remove ivory-bsp-stm32 and ivory-bsp-tests
    these two packages have been moved to the ivory-tower-stm32 repo

--- src/ivory-bsp-stm32/src/Ivory/BSP/STM32/PlatformClock.hs
+++
@@ -1,8 +0,0 @@
-
-module Ivory.BSP.STM32.PlatformClock where
-
-import Ivory.Language
-import Ivory.BSP.STM32.ClockConfig
-
-class PlatformClock p where
-  platformClockConfig :: Proxy p -> ClockConfig
