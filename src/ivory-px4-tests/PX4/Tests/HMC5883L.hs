0687cbd01f62431dfee9c15a1e7950b08fd5ae75
Pat Hickey <pat@galois.com>
2014-12-04 15:55:48 -0800

    ivory-px4-tests: hmc5883l test typechecks

--- src/ivory-px4-tests/PX4/Tests/HMC5883L.hs
+++ src/ivory-px4-tests/PX4/Tests/HMC5883L.hs
@@ -19,85 +19,84 @@ import qualified Ivory.HXStream as HX
 
 import SMACCMPilot.Hardware.HMC5883L
 
+import qualified Ivory.BSP.STM32F405.Interrupt as F405
+import qualified BSP.Tests.Platforms as BSP
 import PX4.Tests.Platforms
 
-app :: forall p . (TestPlatform p) => Tower p ()
-app = do
-  boardInitializer
+app :: (e -> PX4Platform F405.Interrupt) -> Tower e ()
+app topx4 = do
+  px4platform <- fmap topx4 getEnv
   towerModule  hmc5883lTypesModule
   towerDepends hmc5883lTypesModule
-  (req, res) <- i2cTower (hmc5883periph platform)
-                         (hmc5883sda platform)
-                         (hmc5883scl platform)
 
-  samples <- hmc5883lctl req res (hmc5883addr platform)
-  (_uarti,uarto) <- uartTower (consoleUart platform) 115200 (Proxy :: Proxy 128)
+  let hmc = px4platform_hmc5883_device px4platform
+  (req, res) <- i2cTower tocc
+                         (hmc5883device_periph hmc)
+                         (hmc5883device_sda    hmc)
+                         (hmc5883device_scl    hmc)
 
-  task "hmc5883lsender" $ do
-    uartout <- withChannelEmitter uarto "uartout"
-    hmc5883lSender samples uartout
+  samples <- hmc5883lctl req res (hmc5883device_addr hmc)
+
+  let u = BSP.testUART . BSP.testplatform_uart . px4platform_testplatform
+  (_uarti,uarto) <- uartTower tocc (u px4platform) 115200 (Proxy :: Proxy 128)
+
+  monitor "hmc5883lsender" $ do
+    hmc5883lSender samples uarto
 
   towerDepends serializeModule
   towerModule  serializeModule
   where
-  platform = Proxy :: Proxy p
+  tocc = BSP.testplatform_clockconfig . px4platform_testplatform . topx4
 
-hmc5883lSender :: ChannelSink (Struct "hmc5883l_sample")
-               -> ChannelEmitter (Stored Uint8)
-               -> Task p ()
-hmc5883lSender samplesink out = do
-  samp <- withChannelEvent samplesink "sample"
-  (buf :: Ref Global (Array 22 (Stored Uint8))) <- taskLocal "hmc5883l_ser_buf"
-  handle samp "sample" $ \s -> noReturn $ do
-    ifail <- deref (s ~> initfail)
-    sfail <- deref (s ~> samplefail)
-    stime <- deref (s ~> time)
-    packInto_ buf 0 $ do
-      mpackV (ifail ? ((1 :: Uint8), 0))
-      mpackV (sfail ? ((1 :: Uint8), 0))
-      mpack ((s ~> sample) ! 0)
-      mpack ((s ~> sample) ! 1)
-      mpack ((s ~> sample) ! 2)
-      mpackV (toIMicroseconds stime)
-    HX.encode tag (constRef buf) (emitV_ out)
+hmc5883lSender :: ChanOutput (Struct "hmc5883l_sample")
+               -> ChanInput  (Stored Uint8)
+               -> Monitor e ()
+hmc5883lSender samples ostream = do
+  (buf :: Ref Global (Array 22 (Stored Uint8))) <- state "hmc5883l_ser_buf"
+  handler samples "sample" $ do
+    e <- emitter ostream 48
+    callback $ \s -> noReturn $ do
+      ifail <- deref (s ~> initfail)
+      sfail <- deref (s ~> samplefail)
+      stime <- deref (s ~> time)
+      packInto_ buf 0 $ do
+        mpackV (ifail ? ((1 :: Uint8), 0))
+        mpackV (sfail ? ((1 :: Uint8), 0))
+        mpack ((s ~> sample) ! 0)
+        mpack ((s ~> sample) ! 1)
+        mpack ((s ~> sample) ! 2)
+        mpackV (toIMicroseconds stime)
+      HX.encode tag (constRef buf) (emitV e)
   where
   tag = 99 -- 'c' for compass
 
-hmc5883lctl :: forall p
-        . ChannelSource (Struct "i2c_transaction_request")
-       -> ChannelSink   (Struct "i2c_transaction_result")
-       -> I2CDeviceAddr
-       -> Tower p (ChannelSink (Struct "hmc5883l_sample"))
+hmc5883lctl :: ChanInput  (Struct "i2c_transaction_request")
+            -> ChanOutput (Struct "i2c_transaction_result")
+            -> I2CDeviceAddr
+            -> Tower e (ChanOutput (Struct "hmc5883l_sample"))
 hmc5883lctl toDriver fromDriver addr = do
   samplechan <- channel
-  task "hmc5883lctl" $ do
-    i2cRequest <- withChannelEmitter toDriver "i2cRequest"
-    i2cResult <- withChannelEvent fromDriver "i2cResult"
-    sampleEmitter <- withChannelEmitter (src samplechan) "sample"
-
-    driver <- testDriverMachine addr i2cRequest i2cResult sampleEmitter
-
-    taskStackSize 3072
-
-    taskInit $ do
-      begin driver
-  return (snk samplechan)
+  monitor "hmc5883lctl" $ do
+    driver <- testDriverMachine addr toDriver fromDriver (fst samplechan)
+    stateMachine_onChan driver fromDriver
+  return (snd samplechan)
 
 testDriverMachine :: I2CDeviceAddr
-                  -> ChannelEmitter (Struct "i2c_transaction_request")
-                  -> Event          (Struct "i2c_transaction_result")
-                  -> ChannelEmitter (Struct "hmc5883l_sample")
-                  -> Task p Runnable
-testDriverMachine addr i2cRequest i2cResult sampleEmitter = do
-  s <- taskLocal "sample_buffer"
+                  -> ChanInput  (Struct "i2c_transaction_request")
+                  -> ChanOutput (Struct "i2c_transaction_result")
+                  -> ChanInput  (Struct "hmc5883l_sample")
+                  -> Monitor e (StateMachine e)
+testDriverMachine addr i2cRequest i2cResult sampleChan = do
   stateMachine "hmc5883lTestDriver" $ mdo
+    s     <- machineLocal "sample_buffer"
     setup <- sensorSetup addr (s ~> initfail) i2cRequest i2cResult read
     read  <- sensorRead  addr s               i2cRequest i2cResult waitRead
-    waitRead <- stateNamed "waitRead" $ do
-      entry $
-        liftIvory_ (emit_ sampleEmitter (constRef s))
+    waitRead <- machineStateNamed "waitRead" $ do
+      entry $ do
+        e <- machineEmitter sampleChan 1
+        machineCallback $ \_ -> (emit e (constRef s))
       timeout (Milliseconds 13) $ -- XXX 75hz?
-        goto read
+        machineControl $ \_ -> return (goto read)
 
     return setup
 
