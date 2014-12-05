215b1d3e1f7b7989297d739a903bc4705b5d4bc6
Pat Hickey <pat@galois.com>
2014-12-04 17:05:55 -0800

    ivory-px4-tests: mpu6000 typechecks

--- src/ivory-px4-tests/PX4/Tests/MPU6000.hs
+++ src/ivory-px4-tests/PX4/Tests/MPU6000.hs
@@ -5,7 +5,11 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE FlexibleContexts #-}
 
-module PX4.Tests.MPU6000 (mpu6000SensorManager, mpu6000Sender, testApp) where
+module PX4.Tests.MPU6000
+  ( mpu6000SensorManager
+  , mpu6000Sender
+  , testApp
+  ) where
 
 import Ivory.Language
 import Ivory.Stdlib
@@ -20,99 +24,98 @@ import Ivory.BSP.STM32.Driver.UART
 
 import SMACCMPilot.Hardware.MPU6000
 
+import qualified Ivory.BSP.STM32F405.Interrupt as F405
+import qualified BSP.Tests.Platforms as BSP
 import PX4.Tests.Platforms
 
-testApp :: forall p . (TestPlatform p) => Tower p ()
-testApp = do
-  boardInitializer
-
+testApp :: (e -> PX4Platform F405.Interrupt) -> Tower e ()
+testApp topx4 = do
   sample <- channel
-  (req, res) <- spiTower [mpu6000Device platform]
-  mpu6000SensorManager req res (src sample) (SPIDeviceHandle 0)
+  px4platform <- fmap topx4 getEnv
+  let mpu6000 = px4platform_mpu6000_device px4platform
+  (req, res) <- spiTower tocc [mpu6000]
+  mpu6000SensorManager req res (fst sample) (SPIDeviceHandle 0)
 
-  (_uarti, uarto) <- uartTower (consoleUart platform) 115200 (Proxy :: Proxy 256)
-  task "mpu6000sender" $ do
-    uartout <- withChannelEmitter uarto "uartout"
-    mpu6000Sender (snk sample) uartout
+  let u = BSP.testUART . BSP.testplatform_uart . px4platform_testplatform
+  (_uarti, uarto) <- uartTower tocc (u px4platform) 115200 (Proxy :: Proxy 256)
+  monitor "mpu6000sender" $ do
+    mpu6000Sender (snd sample) uarto
 
   towerDepends serializeModule
   towerModule  serializeModule
   towerModule  mpu6000TypesModule
   towerDepends mpu6000TypesModule
   where
-  platform = Proxy :: Proxy p
+  tocc = BSP.testplatform_clockconfig . px4platform_testplatform . topx4
 
-mpu6000Sender :: ChannelSink (Struct "mpu6000_sample")
-              -> ChannelEmitter (Stored Uint8)
-              -> Task p ()
-mpu6000Sender meassink out = do
-  meas <- withChannelEvent meassink "measurement"
-  (buf :: Ref Global (Array 38 (Stored Uint8))) <- taskLocal "mpu6000_ser_buf"
-  handle meas "measurement" $ \s -> noReturn $ do
-    ifail <- deref (s ~> initfail)
-    sfail <- deref (s ~> samplefail)
-    stime <- deref (s ~> time)
-    packInto_ buf 0 $ do
-      mpackV (ifail ? ((1 :: Uint8), 0))
-      mpackV (sfail ? ((1 :: Uint8), 0))
-      mpack  (s ~> gyro_x)
-      mpack  (s ~> gyro_y)
-      mpack  (s ~> gyro_z)
-      mpack  (s ~> accel_x)
-      mpack  (s ~> accel_y)
-      mpack  (s ~> accel_z)
-      mpack  (s ~> temp)
-      mpackV (toIMicroseconds stime)
-    HX.encode tag (constRef buf) (emitV_ out)
+mpu6000Sender :: ChanOutput (Struct "mpu6000_sample")
+              -> ChanInput (Stored Uint8)
+              -> Monitor e ()
+mpu6000Sender meas out = do
+  (buf :: Ref Global (Array 38 (Stored Uint8))) <- state "mpu6000_ser_buf"
+  handler meas "measurement" $ do
+    e <- emitter out (38*2 + 3)
+    callback $ \s -> noReturn $ do
+      ifail <- deref (s ~> initfail)
+      sfail <- deref (s ~> samplefail)
+      stime <- deref (s ~> time)
+      packInto_ buf 0 $ do
+        mpackV (ifail ? ((1 :: Uint8), 0))
+        mpackV (sfail ? ((1 :: Uint8), 0))
+        mpack  (s ~> gyro_x)
+        mpack  (s ~> gyro_y)
+        mpack  (s ~> gyro_z)
+        mpack  (s ~> accel_x)
+        mpack  (s ~> accel_y)
+        mpack  (s ~> accel_z)
+        mpack  (s ~> temp)
+        mpackV (toIMicroseconds stime)
+      HX.encode tag (constRef buf) (emitV e)
   where
   tag = 103 -- 'g' for gyro
 
-mpu6000SensorManager :: ChannelSource (Struct "spi_transaction_request")
-                     -> ChannelSink   (Struct "spi_transaction_result")
-                     -> ChannelSource (Struct "mpu6000_sample")
+mpu6000SensorManager :: ChanInput (Struct "spi_transaction_request")
+                     -> ChanOutput (Struct "spi_transaction_result")
+                     -> ChanInput (Struct "mpu6000_sample")
                      -> SPIDeviceHandle
-                     -> Tower p ()
-mpu6000SensorManager toDriver fromDriver sensorSource dh = task "mpu6kCtl" $ do
-  spiRequest <- withChannelEmitter toDriver "toDriver"
-  spiResult <- withChannelEvent fromDriver "fromDriver"
-  sensorEmitter <- withChannelEmitter sensorSource "sensorOutput"
+                     -> Tower e ()
+mpu6000SensorManager spiRequest spiResult sensorChan dh = do
+  p <- period (Milliseconds 5) -- 200hz
+  monitor "mpu6kCtl" $ do
+    (initMachine, initError) <- initializerMachine dh spiRequest spiResult
+    transactionPending <- stateInit "transactionPending" (ival false)
+    result             <- state     "mpu6000_result"
 
-  (initMachine, initError) <- initializerMachine dh spiRequest spiResult
+    handler p "period" $ do
+      sensorEmitter <- emitter sensorChan 1
+      spiEmitter    <- emitter spiRequest 1
+      callback $ \_ -> do
+        initializing <- stateMachine_active initMachine
+        running      <- deref transactionPending
+        initerror    <- deref initError
+        cond_
+          [ initializing .|| (iNot initializing .&& initerror) ==> do
+              store (result ~> initfail) true
+              invalidTransaction result
+              emit sensorEmitter (constRef result)
+          , running ==> do
+              invalidTransaction result
+              emit sensorEmitter (constRef result)
+          , true ==> do
+              store (result ~> initfail) false
+              store transactionPending true
+              req <- getSensorsReq dh
+              emit spiEmitter req
+          ]
 
-  taskStackSize 2048
-
-
-  transactionPending <- taskLocalInit "transactionPending" (ival false)
-  result             <- taskLocal     "mpu6000_result"
-  taskInit $ do
-    begin initMachine
-
-  p <- withPeriodicEvent (Milliseconds 5) -- 200hz
-  handle p "period" $ \_ -> do
-    initializing <- active initMachine
-    running      <- deref transactionPending
-    initerror    <- deref initError
-    cond_
-      [ initializing .|| (iNot initializing .&& initerror) ==> do
-          store (result ~> initfail) true
-          invalidTransaction result
-          emit_ sensorEmitter (constRef result)
-      , running ==> do
-          invalidTransaction result
-          emit_ sensorEmitter (constRef result)
-      , true ==> do
-          store (result ~> initfail) false
-          store transactionPending true
-          req <- getSensorsReq dh
-          emit_ spiRequest req
-      ]
-
-  handle spiResult "spiResult" $ \res -> do
-    initializing <- active initMachine
-    unless initializing $ do
-      store transactionPending false
-      rawSensorFromResponse res result
-      emit_ sensorEmitter (constRef result)
+    handler spiResult "spiResult" $ do
+      sensorEmitter <- emitter sensorChan 1
+      callback $ \res -> do
+        initializing <- stateMachine_active initMachine
+        unless initializing $ do
+          store transactionPending false
+          rawSensorFromResponse res result
+          emit sensorEmitter (constRef result)
 
   where
   invalidTransaction :: (GetAlloc eff ~ Scope s)
