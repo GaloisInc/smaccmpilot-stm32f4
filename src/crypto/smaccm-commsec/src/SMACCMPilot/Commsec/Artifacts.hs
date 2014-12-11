f4c309d2938629dd307d645801b0e58fd10236ca
Pat Hickey <pat@galois.com>
2014-12-10 20:47:40 -0500

    smaccm-commsec: export Artifacts

---
+++ src/crypto/smaccm-commsec/src/SMACCMPilot/Commsec/Artifacts.hs
@@ -0,0 +1,28 @@
+
+module SMACCMPilot.Commsec.Artifacts
+  ( commsecArtifacts
+  ) where
+
+import qualified Paths_smaccm_commsec
+import Ivory.Artifact
+
+commsecArtifacts :: [Artifact]
+commsecArtifacts = map
+  (\p -> artifactCabalFile Paths_smaccm_commsec.getDataDir ("support/" ++ p))
+  [ "aes.h"
+  , "aescrypt.c"
+  , "aeskey.c"
+  , "aestab.c"
+  , "aestab.h"
+  , "brg_endian.h"
+  , "brg_types.h"
+  , "commsec.c"
+  , "commsec.h"
+  , "gcm.c"
+  , "gcm.h"
+  , "gf128mul.c"
+  , "gf128mul.h"
+  , "gf_convert.c"
+  , "gf_mul_lo.h"
+  , "mode_hdr.h"
+  ]
