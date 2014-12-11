e58dd53b5a5b33355f24cbf91bc3598a35218366
Pat Hickey <pat@galois.com>
2014-12-11 15:26:02 -0500

    smaccm-commsec: implement config parser

--- src/crypto/smaccm-commsec/src/SMACCMPilot/Commsec/Config.hs
+++ src/crypto/smaccm-commsec/src/SMACCMPilot/Commsec/Config.hs
@@ -1,10 +1,11 @@
 
 module SMACCMPilot.Commsec.Config
   ( Config(..)
-  , configParser
+  , commsecConfigParser
   ) where
 
 import Data.Word
+import Tower.Config
 
 data Config = Config
   { sendID     :: Word32   -- 0 - 15 uint32
@@ -14,6 +15,26 @@ data Config = Config
   , recvSalt   :: Word32   -- word32
   } deriving (Show, Read, Eq)
 
-configParser :: a -> Config -- not really tho
-configParser = undefined
-
+commsecConfigParser :: ConfigParser Config
+commsecConfigParser = do
+  sid <- subsection "sendID"  (boundedInteger 0 15)
+  sk <- subsection "sendKey"  (arrayLen 16 (boundedInteger 0 255))
+  ss <- subsection "sendSalt" (boundedInteger 0 (2 ^ 32 - 1))
+  rk <- subsection "recvKey"  (arrayLen 16 (boundedInteger 0 255))
+  rs <- subsection "recvSalt" (boundedInteger 0 (2 ^ 32 - 1))
+  return Config
+    { sendID   = sid
+    , sendKey  = sk
+    , sendSalt = ss
+    , recvKey  = rk
+    , recvSalt = rs
+    }
+  where
+  arrayLen l e = do
+    a <- array e
+    if length a == l then return a
+      else fail ("not of length " ++ show l)
+  boundedInteger l h = do
+    i <- integer
+    if i >= l && i <= h then return (fromIntegral i)
+      else fail ("not within bounds [" ++  show l ++ ".." ++ show h ++ "]")
