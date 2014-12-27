ef18301b2541174cd0d8dc77f11dd989cc7da7b6
Pat Hickey <pat@galois.com>
2014-12-26 20:58:56 -0500

    smaccm-commsec: config naming conventions changed

--- src/crypto/smaccm-commsec/src/SMACCMPilot/Commsec/Config.hs
+++ src/crypto/smaccm-commsec/src/SMACCMPilot/Commsec/Config.hs
@@ -8,26 +8,26 @@ import Data.Word
 import Tower.Config
 
 data Config = Config
-  { sendID     :: Word32   -- 0 - 15 uint32
-  , sendKey    :: [Word8]  -- 16 uint8s
-  , sendSalt   :: Word32   -- word32
-  , recvKey    :: [Word8]  -- 16 uint8s
-  , recvSalt   :: Word32   -- word32
+  { encode_id     :: Word32   -- 0 - 15 uint32
+  , encode_key    :: [Word8]  -- 16 uint8s
+  , encode_salt   :: Word32   -- word32
+  , decode_key    :: [Word8]  -- 16 uint8s
+  , decode_salt   :: Word32   -- word32
   } deriving (Show, Read, Eq)
 
 commsecConfigParser :: ConfigParser Config
 commsecConfigParser = do
-  sid <- subsection "sendID"  (boundedInteger 0 15)
-  sk <- subsection "sendKey"  (arrayLen 16 (boundedInteger 0 255))
-  ss <- subsection "sendSalt" (boundedInteger 0 (2 ^ 32 - 1))
-  rk <- subsection "recvKey"  (arrayLen 16 (boundedInteger 0 255))
-  rs <- subsection "recvSalt" (boundedInteger 0 (2 ^ 32 - 1))
+  ei <- subsection "encode_id"  (boundedInteger 0 15)
+  ek <- subsection "encode_key"  (arrayLen 16 (boundedInteger 0 255))
+  es <- subsection "encode_salt" (boundedInteger 0 ((2 ^ (32::Integer)) - 1))
+  dk <- subsection "decode_key"  (arrayLen 16 (boundedInteger 0 255))
+  ds <- subsection "decode_salt" (boundedInteger 0 ((2 ^ (32::Integer)) - 1))
   return Config
-    { sendID   = sid
-    , sendKey  = sk
-    , sendSalt = ss
-    , recvKey  = rk
-    , recvSalt = rs
+    { encode_id   = ei
+    , encode_key  = ek
+    , encode_salt = es
+    , decode_key  = dk
+    , decode_salt = ds
     }
   where
   arrayLen l e = do
