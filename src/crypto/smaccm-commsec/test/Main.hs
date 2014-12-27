d29e764248a6d239fccc64e6c718ca93a6ac0476
Pat Hickey <pat@galois.com>
2014-12-26 22:54:56 -0500

    smaccm-commsec: test generates encode and decode modules, doesnt run them
    still need to port over the meaningful stuff from commsec tests to run these
    tests in a meaningful way. but just getting the c compiler to check the imports
    are done right is meaningful

--- src/crypto/smaccm-commsec/test/Main.hs
+++ src/crypto/smaccm-commsec/test/Main.hs
@@ -1,17 +1,60 @@
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds #-}
 
 module Main where
 
 import qualified Data.List as L
 
+import Ivory.Language
 import Ivory.Artifact
 import Ivory.Compile.C.CmdlineFrontend
 import SMACCMPilot.Commsec.Artifacts
+import SMACCMPilot.Commsec.Module
+import SMACCMPilot.Commsec.Config
 
 main :: IO ()
-main = compile [] as
+main = compile [ encmod, decmod ] as
   where
   as = commsecArtifacts
      ++ [ artifactString "Makefile" makefile ]
+
+config :: Config
+config = Config
+  { encode_id = 1
+  , encode_key = take 16 [1..]
+  , encode_salt = 0xdeadbeef
+  , decode_key  = take 16 [2..]
+  , decode_salt = 0xcafed00d
+  }
+
+encmod :: Module
+encmod = package "test_encode" $ do
+  incl test_encode_proc
+  commsec_encode_moddef ce
+  where
+  ce = commsecEncode config "etest"
+  test_encode_proc :: Def('[]:->())
+  test_encode_proc = proc "test_encode" $ body $ do
+    commsec_encode_init ce
+    ct <- local (iarray [])
+    pt <- local (iarray [])
+    _e <- commsec_encode_run ce (constRef pt) ct
+    return ()
+
+decmod :: Module
+decmod = package "test_decode" $ do
+  incl test_decode_proc
+  commsec_decode_moddef cd
+  where
+  cd = commsecDecode config "dtest"
+  test_decode_proc :: Def('[]:->())
+  test_decode_proc = proc "test_decode" $ body $ do
+    commsec_decode_init cd
+    ct <- local (iarray [])
+    pt <- local (iarray [])
+    _e <- commsec_decode_run cd (constRef ct) pt
+    return ()
+
 
 objects :: [FilePath]
 objects =
@@ -22,6 +65,8 @@ objects =
   , "gcm.o"
   , "gf128mul.o"
   , "gf_convert.o"
+  , "test_encode.o"
+  , "test_decode.o"
   ]
 
 makefile :: String
