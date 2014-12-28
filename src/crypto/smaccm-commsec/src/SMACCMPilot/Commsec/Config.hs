46cce401b074c216599215ea6e803514872d63d0
Pat Hickey <pat@galois.com>
2014-12-28 13:46:00 -0500

    smaccm-commsec: refactor Config datatypes

--- src/crypto/smaccm-commsec/src/SMACCMPilot/Commsec/Config.hs
+++ src/crypto/smaccm-commsec/src/SMACCMPilot/Commsec/Config.hs
@@ -1,6 +1,7 @@
 
 module SMACCMPilot.Commsec.Config
   ( Config(..)
+  , KeySalt(..)
   , commsecConfigParser
   ) where
 
@@ -8,28 +9,31 @@ import Data.Word
 import Tower.Config
 
 data Config = Config
-  { encode_id     :: Word32   -- 0 - 15 uint32
-  , encode_key    :: [Word8]  -- 16 uint8s
-  , encode_salt   :: Word32   -- word32
-  , decode_key    :: [Word8]  -- 16 uint8s
-  , decode_salt   :: Word32   -- word32
-  } deriving (Show, Read, Eq)
+  { encode_id :: Integer -- 0 - 15
+  , encode_ks :: KeySalt
+  , decode_ks :: KeySalt
+  } deriving (Eq, Show)
+
+data KeySalt = KeySalt
+  { ks_key  :: [Word8]  -- 16 uint8s
+  , ks_salt :: Word32   -- word32
+  } deriving (Eq, Show)
 
 commsecConfigParser :: ConfigParser Config
 commsecConfigParser = do
-  ei <- subsection "encode_id"  (boundedInteger 0 15)
-  ek <- subsection "encode_key"  (arrayLen 16 (boundedInteger 0 255))
-  es <- subsection "encode_salt" (boundedInteger 0 ((2 ^ (32::Integer)) - 1))
-  dk <- subsection "decode_key"  (arrayLen 16 (boundedInteger 0 255))
-  ds <- subsection "decode_salt" (boundedInteger 0 ((2 ^ (32::Integer)) - 1))
+  ei  <- subsection "encode" (subsection "id" (boundedInteger 0 15))
+  eks <- subsection "encode" ks
+  dks <- subsection "decode" ks
   return Config
-    { encode_id   = ei
-    , encode_key  = ek
-    , encode_salt = es
-    , decode_key  = dk
-    , decode_salt = ds
+    { encode_id = ei
+    , encode_ks = eks
+    , decode_ks = dks
     }
   where
+  ks = do
+    k <- subsection "key" (arrayLen 16 (boundedInteger 0 255))
+    s <- subsection "salt" (boundedInteger 0 ((2 ^ (32::Integer)) - 1))
+    return KeySalt { ks_key = k, ks_salt = s }
   arrayLen l e = do
     a <- array e
     if length a == l then return a
