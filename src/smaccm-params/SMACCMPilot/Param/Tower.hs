aba29127f37ce7664b193b73a33ac7ad174e8e9b
Pat Hickey <pat@galois.com>
2015-03-19 13:53:54 -0700

    smaccm-params: add cabal file, makefile, update for tower9

--- src/smaccm-params/SMACCMPilot/Param/Tower.hs
+++ src/smaccm-params/SMACCMPilot/Param/Tower.hs
@@ -6,6 +6,7 @@
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ExistentialQuantification #-}
 
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
@@ -36,15 +37,16 @@ import qualified SMACCMPilot.Mavlink.Messages.ParamValue as PV
 -- | Wrapper type for a data source of parameter values.
 data ParamSource =
   ParamSource
-    { psrc_chan :: ChannelSource (Stored IFloat)
+    { psrc_chan :: ChanInput (Stored IFloat)
     , psrc_name :: String
     }
 
 -- | Wrapper type for a data sink of parameter values.
 data ParamSink =
   ParamSink
-    { psnk_chan :: ChannelSink (Stored IFloat)
+    { psnk_chan :: ChanOutput (Stored IFloat)
     , psnk_name :: String
+    , psnk_ival :: Float
     }
 
 -- | Wrapper for a source/sink dataport pair.
@@ -60,50 +62,48 @@ initTowerParams x = paramInit go x
   where
     go :: String -> Float -> Tower p PortPair
     go n v = do
-      (psrc, psink) <- channel' (Proxy :: Proxy 2) (Just (ival (ifloat v)))
-      return (PortPair (ParamSource psrc n) (ParamSink psink n))
+      (psrc, psink) <- channel
+      return (PortPair (ParamSource psrc n) (ParamSink psink n v))
 
 -- | Shorthand type for a reader for parameter values.
-type ParamReader = ChannelReader (Stored IFloat)
+data ParamState = forall s . ParamState (Ref s (Stored IFloat))
+-- | Open data readers for a parameter tree containing sinks.
+paramState :: (Traversable a)
+            => a ParamSink
+            -> Monitor p (a ParamState)
+paramState = traverse mkreader
+  where
+  mkreader psnk = do
+    s <- stateInit ("param_reader_" ++ psnk_name psnk)
+                   (ival (ifloat (psnk_ival psnk)))
+    handler (psnk_chan psnk) ("param_update_" ++ psnk_name psnk) $ do
+      callbackV $ \v -> store s v
+    return (ParamState s)
+
+-- | Read the float values of a parameter tree.
+paramRead :: (GetAlloc eff ~ Scope s, Traversable a)
+          => a ParamState
+          -> Ivory eff (a IFloat)
+paramRead = traverse (\(ParamState s) -> deref s)
+
+-- | Shorthand for reading a single unwrapped Param value
+paramGetState :: (GetAlloc eff ~ Scope s)
+              => Param ParamState -> Ivory eff IFloat
+paramGetState r = do
+  p <- paramRead r
+  return (paramData p)
 
 -- | Shorthand type for a writer for parameter values.
-type ParamWriter = ChannelEmitter (Stored IFloat)
-
--- | Open data readers for a parameter tree containing sinks.
-paramReader :: (Traversable a)
-            => a ParamSink
-            -> Task p (a ParamReader)
-paramReader = traverse mkreader
-  where
-  mkreader psnk = withChannelReader (psnk_chan psnk)
-                                 ("param_reader_" ++ psnk_name psnk)
+type ParamWriter = Emitter (Stored IFloat)
 
 -- | Open data writers for a parameter tree containing sources.
 paramWriter :: (Traversable a)
             => a ParamSource
-            -> Task p (a ParamWriter)
+            -> Handler e b (a ParamWriter)
 paramWriter = traverse mkwriter
   where
-  mkwriter psrc = withChannelEmitter (psrc_chan psrc)
-                                 ("param_writer_" ++ psrc_name psrc)
+  mkwriter psrc = emitter (psrc_chan psrc) 1
 
--- | Read the float values of a parameter tree.
-paramRead :: (GetAlloc eff ~ Scope s, Traversable a)
-          => a ParamReader
-          -> Ivory eff (a IFloat)
-paramRead =
-  traverse $ \d -> do
-    x <- local (ival 0.0)
-    -- Safe to ignore validity, because we always initialize channel
-    _ <- chanRead d x
-    deref x
-
--- | Shorthand for reading a single unwrapped Param value
-paramGet :: (GetAlloc eff ~ Scope s)
-         => Param ParamReader -> Ivory eff IFloat
-paramGet r = do
-  p <- paramRead r
-  return (paramData p)
 
 ----------------------------------------------------------------------
 -- Parameter Lookup
@@ -129,12 +129,12 @@ newtype ParamIndexGetter =
 
 -- | Create a function to look up a parameter index by name.
 makeGetParamIndex :: [Param f]
-                  -> Task p ParamIndexGetter
+                  -> Monitor p ParamIndexGetter
 makeGetParamIndex params = do
   fr <- fresh
   let p = ParamIndexGetter $ proc ("getParamIndex" ++ (show fr)) $
             \name ref -> body $ go params name ref 0
-  taskModuleDef $ incl (paramIndexGetter p)
+  monitorModuleDef $ incl (paramIndexGetter p)
   return p
   where
     go [] _ _ _ = ret false
@@ -156,14 +156,15 @@ newtype ParamInfoGetter =
               ] :-> IBool)
     }
 
+
 -- | Create a function to get parameter info by index.
-makeGetParamInfo :: [Param ParamReader]
-                 -> Task p ParamInfoGetter
+makeGetParamInfo :: [Param ParamState]
+                 -> Monitor p ParamInfoGetter
 makeGetParamInfo params = do
   fr <- fresh
   let p = ParamInfoGetter $ proc ("getParamInfo" ++ (show fr)) $
             \ix ref -> body $ go params ix ref 0
-  taskModuleDef $ incl (paramInfoGetter p)
+  monitorModuleDef $ incl (paramInfoGetter p)
   return p
   where
     count :: Uint16
@@ -172,7 +173,8 @@ makeGetParamInfo params = do
     go (x:xs) ix ref n = do
       when (ix ==? n) $ do
         name <- local (stringInit (paramName x) :: Init ParamString)
-        _ <- chanRead (paramData x) (ref ~> PV.param_value)
+        s <- paramGetState x
+        store (ref ~> PV.param_value) s
         store (ref ~> PV.param_count) count
         store (ref ~> PV.param_index) (signCast ix)
         store (ref ~> PV.param_type)  0
@@ -180,25 +182,24 @@ makeGetParamInfo params = do
         ret true
       go xs ix ref (n + 1)
 
+
 -- | Create a function to set a parameter value by index.
-makeSetParamValue :: [Param ParamWriter]
-                  -> Task p (Def ('[ Sint16
-                                   , IFloat
-                                   ] :-> IBool))
-makeSetParamValue params = do
-  fr <- fresh
-  let p = proc ("setParamValue" ++ (show fr)) $
-            \ix value -> body $ go params ix value 0
-  taskModuleDef $ incl p
-  return p
+makeSetParamValue :: (GetAlloc eff ~ Scope s)
+                  => [Param ParamSource]
+                  -> Handler e b (Sint16 -> IFloat -> Ivory eff IBool)
+makeSetParamValue param_src = do
+  pws <- mapM paramWriter param_src
+  return $ \ix value -> do
+    rv <- local (ival false)
+    go pws ix value 0 rv
   where
-    go [] _ _ _ = ret false
-    go (x:xs) ix v n = do
+    go [] _ _ _ rv = deref rv
+    go (x:xs) ix v n rv = do
       when (ix ==? n) $ do
         val <- local (ival v)
-        emit_ (paramData x) (constRef val)
-        ret true
-      go xs ix v (n + 1)
+        emit (paramData x) (constRef val)
+        store rv true
+      go xs ix v (n + 1) rv
 
 -- | Ivory module for structures defined in this module.
 paramModule :: Module
