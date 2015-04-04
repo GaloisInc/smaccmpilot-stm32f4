84f67b89ec1de1f03bd43f68b15f17687a4e3544
Pat Hickey <pat@galois.com>
2015-04-03 17:49:40 -0700

    smaccm-comm-server: wip, isn't working and gdb is acting up for me
    if gdb is crashing anytime you hit a breakpoint in the relevant part of
    your program and its 6pm on a friday, time to just go home

--- src/smaccm-comm-server/src/SMACCMPilot/Comm/Ivory/Server.hs
+++ src/smaccm-comm-server/src/SMACCMPilot/Comm/Ivory/Server.hs
@@ -1,15 +1,81 @@
+{-# LANGUAGE DataKinds #-}
 
 module SMACCMPilot.Comm.Ivory.Server where
 
 import Ivory.Language
+import Ivory.Stdlib
 import Ivory.Tower
 import Ivory.Serialize
-import SMACCMPilot.Commsec.Sizes
 import SMACCMPilot.Comm.Ivory.Types
 import SMACCMPilot.Comm.Ivory.Interface.ControllableVehicle
 
-commTower :: Tower e ()
-commTower = do
+controllableVehicleProducerInput :: (ANat n)
+                                 => ChanOutput (Array n (Stored Uint8))
+                                 -> Tower e ( ChanOutput (Struct "waypoint")
+                                            , ChanOutput (Struct "waypoint")
+                                            , ChanOutput (Struct "heartbeat"))
+controllableVehicleProducerInput frame_ch = do
+  curr_waypt <- channel
+  next_waypt <- channel
+  heartbeat  <- channel
+
+  monitor "controllableVehicleProducerInput" $ do
+    handler frame_ch "parse_frame" $ do
+      cw_e <- emitter (fst curr_waypt) 1
+      nw_e <- emitter (fst next_waypt) 1
+      hb_e <- emitter (fst heartbeat)  1
+      callback $ \f -> do
+        offs <- local izero
+        controllableVehicleProducerParser
+          [ CurrentWaypointValProducerHandler (emit cw_e)
+          , NextWaypointValProducerHandler    (emit nw_e)
+          , HeartbeatProducerHandler          (emit hb_e)]
+          f offs
+
+  return (snd curr_waypt, snd next_waypt, snd heartbeat)
+
+
+
+controllableVehicleProducerOutput :: (ANat n)
+                                  => ChanOutput (Struct "waypoint")
+                                  -> ChanOutput (Struct "waypoint")
+                                  -> ChanOutput (Struct "heartbeat")
+                                  -> Tower e (ChanOutput (Array n (Stored Uint8)))
+controllableVehicleProducerOutput curr_waypt next_waypt heartbeat = do
+  frame_ch <- channel
+
+  monitor "controllableVehicleProducerOutput" $ do
+    handler curr_waypt "curr_waypt" $ do
+      e <- emitter (fst frame_ch) 1
+      callback $ \w -> do
+        f    <- local izero
+        offs <- local izero
+        let sender = controllableVehicleProducerSender f offs
+        ok <- currentWaypointValProducerSender sender w
+        when ok $ emit e (constRef f)
+
+    handler next_waypt "next_waypt" $ do
+      e <- emitter (fst frame_ch) 1
+      callback $ \w -> do
+        f    <- local izero
+        offs <- local izero
+        let sender = controllableVehicleProducerSender f offs
+        ok <- nextWaypointValProducerSender sender w
+        when ok $ emit e (constRef f)
+
+    handler heartbeat "heartbeat" $ do
+      e <- emitter (fst frame_ch) 1
+      callback $ \w -> do
+        f    <- local izero
+        offs <- local izero
+        let sender = controllableVehicleProducerSender f offs
+        ok <- heartbeatProducerSender sender w
+        when ok $ emit e (constRef f)
+
+  return (snd frame_ch)
+
+commTowerDependencies :: Tower e ()
+commTowerDependencies = do
   towerModule serializeModule
   mapM_ towerArtifact serializeArtifacts
   mapM_ towerModule typeModules
--- src/smaccm-comm-server/src/SMACCMPilot/Comm/Ivory/Server/TestApp.hs
+++ src/smaccm-comm-server/src/SMACCMPilot/Comm/Ivory/Server/TestApp.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module SMACCMPilot.Comm.Ivory.Server.TestApp
   ( app
@@ -5,10 +7,44 @@ module SMACCMPilot.Comm.Ivory.Server.TestApp
 
 import Ivory.Language
 import Ivory.Tower
+import Ivory.Stdlib
 import SMACCMPilot.Hardware.Tests.Platforms
 
+import SMACCMPilot.Commsec.Sizes
 import SMACCMPilot.Comm.Ivory.Server
+import SMACCMPilot.Datalink.HXStream.Tower
+import Ivory.BSP.STM32.Driver.RingBuffer
 
 app :: (e -> PX4Platform) -> Tower e ()
-app _topx4 = do
-  commTower
+app topx4 = do
+
+  (uarti, uarto) <- px4ConsoleTower topx4
+
+
+  input_frames_unbuffered <- channel
+  hxstreamDecodeTower "frame" uarti (fst input_frames_unbuffered)
+
+  input_frames <- channel
+
+  p <- period (Milliseconds 5)
+  monitor "inputbuffer" $ do
+    (rb :: RingBuffer 4 CyphertextArray) <- monitorRingBuffer "inputbuffer"
+    handler (snd input_frames_unbuffered) "input_frames" $ do
+      callback $ \v -> do
+        _ <- ringbuffer_push rb v
+        return ()
+
+    handler p "periodic_input_pop" $ do
+      e <- emitter (fst input_frames) 1
+      callback $ const $ do
+        v <- local izero
+        got <- ringbuffer_pop rb v
+        when got $ emit e (constRef v)
+
+  (curr_waypt, next_waypt, heartbeat) <- controllableVehicleProducerInput (snd input_frames)
+  (output_frames :: ChanOutput CyphertextArray) <-
+        controllableVehicleProducerOutput curr_waypt next_waypt heartbeat
+
+  hxstreamEncodeTower "frame" output_frames uarto
+
+  commTowerDependencies
