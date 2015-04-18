af636fea8bf6d27e988f85387d610f74149d1b0b
Pat Hickey <pat@galois.com>
2015-04-17 17:33:31 -0700

    smaccm-commsec: haskell level abstraction for symmetric key
    we mostly need it in smaccm-datalink.

--- src/smaccm-commsec/src/SMACCMPilot/Commsec/Ivory/Types/SymmetricKey.hs
+++ src/smaccm-commsec/src/SMACCMPilot/Commsec/Ivory/Types/SymmetricKey.hs
@@ -8,8 +8,9 @@
 module SMACCMPilot.Commsec.Ivory.Types.SymmetricKey where
 
 import Ivory.Language
-import Ivory.Tower.Config
 import SMACCMPilot.Commsec.Sizes
+import SMACCMPilot.Commsec.SymmetricKey
+import Data.Word
 
 [ivory|
 struct symmetric_key
@@ -37,19 +38,9 @@ symmetricKeyTypesModule = package "gec_symmetric_key_types" $ do
     incl deriveSymmetricKey
     defStruct (Proxy :: Proxy "symmetric_key")
 
-symmetricKeyParser :: ConfigParser (Init (Struct "symmetric_key"))
-symmetricKeyParser = subsection "symmetric_key" $ do
-  s2c_ks_ <- subsection "server_to_client" ks
-  c2s_ks_ <- subsection "client_to_server" ks
-  return $ istruct [ s2c_ks .= toiarray s2c_ks_, c2s_ks .= toiarray c2s_ks_ ]
-  where
-  toiarray as = iarray (map (ival . fromInteger) as)
-  ks = subsection "keysalt" (arrayOfLen 24 (boundedInteger 0 255))
-  arrayOfLen l e = do
-    a <- array e
-    if length a == l then return a
-      else fail ("not of length " ++ show l)
-  boundedInteger l h = do
-    i <- integer
-    if i >= l && i <= h then return (fromIntegral i)
-      else fail ("not within bounds [" ++  show l ++ ".." ++ show h ++ "]")
+symKeySaltArrayIval :: [Word8] -> Init SymKeySaltArray
+symKeySaltArrayIval w8s = iarray (map (ival . fromIntegral) w8s)
+
+symmetricKeyIval :: SymmetricKey -> Init (Struct "symmetric_key")
+symmetricKeyIval sk = istruct [ s2c_ks .= symKeySaltArrayIval (sk_s2c sk)
+                              , c2s_ks .= symKeySaltArrayIval (sk_c2s sk) ]
---
+++ src/smaccm-commsec/src/SMACCMPilot/Commsec/SymmetricKey.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE RecordWildCards #-}
+
+module SMACCMPilot.Commsec.SymmetricKey where
+
+import Data.Word
+import Ivory.Tower.Config
+
+data SymmetricKey =
+  SymmetricKey
+    { sk_c2s :: [Word8] -- Vector of 24 word8s
+    , sk_s2c :: [Word8] -- Vector of 24 word8s
+    }
+
+symmetricKeyParser :: ConfigParser SymmetricKey
+symmetricKeyParser = subsection "symmetric_key" $ do
+  sk_s2c <- subsection "server_to_client" ks
+  sk_c2s <- subsection "client_to_server" ks
+  return SymmetricKey{..}
+  where
+  ks = subsection "keysalt" (arrayOfLen 24 (boundedInteger 0 255))
+  arrayOfLen l e = do
+    a <- array e
+    if length a == l then return a
+      else fail ("not of length " ++ show l)
+  boundedInteger l h = do
+    i <- integer
+    if i >= l && i <= h then return (fromIntegral i)
+      else fail ("not within bounds [" ++  show l ++ ".." ++ show h ++ "]")
