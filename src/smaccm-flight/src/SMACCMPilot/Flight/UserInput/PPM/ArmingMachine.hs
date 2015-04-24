199971ce48182c2785fbce23e05b0763d1ed246e
Pat Hickey <pat@galois.com>
2015-04-24 15:34:25 -0700

    smaccm-flight: start porting in old flight code

--- src/smaccm-flight/src/SMACCMPilot/Flight/UserInput/PPM/ArmingMachine.hs
+++ src/smaccm-flight/src/SMACCMPilot/Flight/UserInput/PPM/ArmingMachine.hs
@@ -7,22 +7,21 @@
 
 module SMACCMPilot.Flight.UserInput.PPM.ArmingMachine
   ( ArmingMachine(..)
-  , taskArmingMachine
+  , monitorArmingMachine
   ) where
 
 import Ivory.Language
 import Ivory.Tower
 import Ivory.Stdlib
 
-import qualified SMACCMPilot.Flight.Types.UserInput         as I
-import qualified SMACCMPilot.Flight.Types.ControlLawRequest as CL
+import qualified SMACCMPilot.Comm.Ivory.Types.ArmingMode  as A
 
 data ArmingMachine =
   ArmingMachine
     { am_init       :: forall eff   . Ivory eff ()
-    , am_new_sample :: forall eff s . Ref s I.PPMs -> ITime -> Ivory eff ()
+    , am_new_sample :: forall eff s . Ref s (Array 8 (Stored Uint16)) -> ITime -> Ivory eff ()
     , am_no_sample  :: forall eff   . Ivory eff ()
-    , am_get_cl_req :: forall eff s . Ref s (Struct "control_law_request")
+    , am_get_cl_req :: forall eff s . Ref s (Stored A.ArmingMode)
                                    -> Ivory eff ()
     }
 
@@ -46,12 +45,12 @@ armingActive = ArmingState 1
 armingComplete :: ArmingState
 armingComplete = ArmingState 2
 
-taskArmingMachine :: Task p ArmingMachine
-taskArmingMachine = do
+monitorArmingMachine :: Monitor p ArmingMachine
+monitorArmingMachine = do
   fr <- fresh
-  arming_state      <- taskLocal "arming_state"
-  arming_state_time <- taskLocal "arming_state_time"
-  dead_last_pos     <- taskLocal "dead_last_pos"
+  arming_state      <- state "arming_state"
+  arming_state_time <- state "arming_state_time"
+  dead_last_pos     <- state "dead_last_pos"
   let named n = "ppmdecoder_arming_" ++ n ++ "_" ++ show fr
 
       init_proc :: Def('[]:->())
@@ -60,7 +59,7 @@ taskArmingMachine = do
         store arming_state_time 0
         store dead_last_pos deadSafe
 
-      new_sample_proc :: Def('[Ref s I.PPMs, ITime]:->())
+      new_sample_proc :: Def('[Ref s (Array 8 (Stored Uint16)), ITime]:->())
       new_sample_proc = proc (named "new_sample") $ \ppms time -> body $ do
         throttle_chan   <- deref (ppms ! (2 :: Ix 8))
         rudder_chan     <- deref (ppms ! (3 :: Ix 8))
@@ -77,17 +76,17 @@ taskArmingMachine = do
         store arming_state armingIdle
         store arming_state_time time
 
-      arming_sm :: I.PPM -> I.PPM -> ITime -> Ivory eff ()
+      arming_sm :: Uint16 -> Uint16 -> ITime -> Ivory eff ()
       arming_sm thr rud time = do
         prevt <- deref arming_state_time
-        state <- deref arming_state
+        s <- deref arming_state
         sticks_corner <- assign $ thr <? 1050 .&& rud >? 1900
         donewaiting   <- assign $ (time - prevt) >? hystresis
         cond_
-          [ state ==? armingIdle .&& sticks_corner ==> do
+          [ s ==? armingIdle .&& sticks_corner ==> do
               store arming_state armingActive
               store arming_state_time time
-          , state ==? armingActive .&& sticks_corner ==> do
+          , s ==? armingActive .&& sticks_corner ==> do
               when donewaiting $ store arming_state armingComplete
           , true ==> do
               store arming_state armingIdle
@@ -99,24 +98,25 @@ taskArmingMachine = do
       no_sample_proc = proc (named "no_sample") $ body $
         store dead_last_pos deadSafe
 
-      get_cl_req_proc :: Def('[Ref s (Struct "control_law_request")]:->())
-      get_cl_req_proc = proc (named "cl_req_proc") $ \cl_req -> body $ do
+      get_arming_mode_proc :: Def('[Ref s (Stored A.ArmingMode)]:->())
+      get_arming_mode_proc = proc (named "get_arming_mode") $ \a -> body $ do
         d <- deref dead_last_pos
-        store (cl_req ~> CL.set_safe) (d ==? deadSafe)
         s <- deref arming_state
-        when (s ==? armingComplete) $ do
-          store (cl_req ~> CL.set_armed) true
+        cond_
+          [(d ==? deadSafe) ==> store a A.safe
+          ,(s ==? armingComplete) ==> store a A.armed
+          ]
 
 
-  taskModuleDef $ do
+  monitorModuleDef $ do
     incl init_proc
     incl new_sample_proc
     incl no_sample_proc
-    incl get_cl_req_proc
+    incl get_arming_mode_proc
 
   return ArmingMachine
     { am_init       = call_ init_proc
     , am_new_sample = call_ new_sample_proc
     , am_no_sample  = call_ no_sample_proc
-    , am_get_cl_req = call_ get_cl_req_proc
+    , am_get_cl_req = call_ get_arming_mode_proc
     }
---
+++ src/smaccm-flight/src/SMACCMPilot/Flight/UserInput/PPM/ModeSwitch.hs
@@ -0,0 +1,118 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
+module SMACCMPilot.Flight.UserInput.PPM.ModeSwitch
+  ( ModeSwitch(..)
+  , monitorModeSwitch
+  ) where
+
+import Ivory.Language
+import Ivory.Tower
+import Ivory.Stdlib
+
+import qualified SMACCMPilot.Comm.Ivory.Types.ControlLaw as CL
+import qualified SMACCMPilot.Comm.Ivory.Types.ControlSource as CS
+import qualified SMACCMPilot.Comm.Ivory.Types.YawMode as Y
+import qualified SMACCMPilot.Comm.Ivory.Types.ThrottleMode as T
+
+data ModeSwitch =
+  ModeSwitch
+    { ms_init       :: forall eff   . Ivory eff ()
+    , ms_new_sample :: forall eff s . Ref s (Array 8 (Stored Uint16))
+                                   -> ITime -> Ivory eff ()
+    , ms_no_sample  :: forall eff   . Ivory eff ()
+    , ms_get_cl_req :: forall eff s . Ref s (Struct "control_law")
+                                   -> Ivory eff ()
+    }
+
+newtype ThreePositionSwitch = ThreePositionSwitch Uint8
+  deriving ( IvoryType, IvoryVar, IvoryExpr, IvoryEq
+           , IvoryStore, IvoryInit, IvoryZeroVal)
+
+posUp :: ThreePositionSwitch
+posUp = ThreePositionSwitch 0
+posCenter :: ThreePositionSwitch
+posCenter = ThreePositionSwitch 1
+posDown :: ThreePositionSwitch
+posDown = ThreePositionSwitch 2
+
+monitorModeSwitch :: Monitor p ModeSwitch
+monitorModeSwitch = do
+  fr <- fresh
+  md_last_position      <- state "md_last_position"
+  md_last_position_time <- state "md_last_position_time"
+  let named n = "ppmdecoder_modeswitch_" ++ n ++ "_" ++ show fr
+
+      init_proc :: Def('[]:->())
+      init_proc = proc (named "init") $ body $ do
+        store md_last_position posDown
+        store md_last_position_time 0
+
+      new_sample_proc :: Def('[Ref s (Array 8 (Stored Uint16)), ITime]:->())
+      new_sample_proc = proc (named "new_sample") $ \ppms time -> body $ do
+        switch <- deref (ppms ! (4 :: Ix 8))
+        position <- assign $ modeswitchpos_from_ppm switch
+        -- XXX debouncing
+        store md_last_position position
+        store md_last_position_time time
+
+      -- No failure logic--handled in arming state machine.
+      no_sample_proc :: Def('[] :-> ())
+      no_sample_proc = proc (named "no_sample") $ body $ return ()
+
+      get_cl_req_proc :: Def('[Ref s (Struct "control_law")]:->())
+      get_cl_req_proc = proc (named "cl_req_proc") $ \cl -> body $ do
+        p <- deref md_last_position
+        cond_
+          [ p ==? posUp ==> do
+              -- UI source may be gcs
+              store (cl ~> CL.ui_mode) CS.gcs
+              -- Yaw law must be heading
+              store (cl ~> CL.yaw_mode) Y.heading
+              -- Throttle law must be autothrottle
+              store (cl ~> CL.thr_mode) T.auto
+          , p ==? posCenter ==> do
+              -- UI source must be ppm
+              store (cl ~> CL.ui_mode) CS.ppm
+              -- Yaw law must be rate
+              store (cl ~> CL.yaw_mode) Y.rate
+              -- Throttle law must be autothrottle
+              store (cl ~> CL.thr_mode) T.auto
+          , p ==? posDown ==> do
+              -- UI source must be ppm
+              store (cl ~> CL.ui_mode) CS.ppm
+              -- Yaw law must be rate
+              store (cl ~> CL.yaw_mode) Y.rate
+              -- Throttle law must be direct
+              store (cl ~> CL.thr_mode) T.direct
+          ]
+
+  monitorModuleDef $ do
+    incl init_proc
+    incl new_sample_proc
+    incl no_sample_proc
+    incl get_cl_req_proc
+
+  return ModeSwitch
+    { ms_init       = call_ init_proc
+    , ms_new_sample = call_ new_sample_proc
+    , ms_no_sample  = call_ no_sample_proc
+    , ms_get_cl_req = call_ get_cl_req_proc
+    }
+  where
+
+  modeswitchpos_from_ppm :: Uint16 -> ThreePositionSwitch
+  modeswitchpos_from_ppm ppm = foldr (match_mode_map ppm) posDown mode_ppm_map
+  match_mode_map ppm (mode, (min_ppm, max_ppm)) def =
+    ((ppm >=? min_ppm) .&& (ppm <=? max_ppm)) ? (mode, def)
+
+  mode_ppm_map :: [(ThreePositionSwitch, (Uint16, Uint16))]
+  mode_ppm_map =  [(posUp,    (minBound, 1300))
+                  ,(posCenter,(1301, 1700))
+                  ,(posDown,  (1701, maxBound))
+                  ]
+
