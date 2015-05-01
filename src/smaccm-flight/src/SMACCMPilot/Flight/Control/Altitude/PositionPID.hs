3b8c6b82c27b828093a39934a403e7a5e8981590
Pat Hickey <pat@galois.com>
2015-05-01 14:40:27 -0700

    smaccm-flight: port position pid

--- src/smaccm-flight/src/SMACCMPilot/Flight/Control/Altitude/PositionPID.hs
+++ src/smaccm-flight/src/SMACCMPilot/Flight/Control/Altitude/PositionPID.hs
@@ -8,11 +8,12 @@ import Ivory.Language
 import Ivory.Tower
 import Ivory.Stdlib
 
-import qualified SMACCMPilot.Flight.Types.AltControlDebug as A
+import qualified SMACCMPilot.Comm.Ivory.Types.PidState        as P
+import qualified SMACCMPilot.Comm.Ivory.Types.PidConfig       as C
+import qualified SMACCMPilot.Comm.Ivory.Types.AltControlDebug as A
+import           SMACCMPilot.Comm.Tower.Attr
 import           SMACCMPilot.Flight.Control.PID
 import           SMACCMPilot.Flight.Control.Altitude.Estimator
-import           SMACCMPilot.Flight.Param
-import           SMACCMPilot.Param
 
 data PositionPid =
   PositionPid
@@ -23,19 +24,22 @@ data PositionPid =
                                      -> IFloat -- dt, seconds
                                      -> Ivory eff IFloat
     , pos_pid_write_debug :: forall eff s .
-          Ref s (Struct "alt_control_dbg") -> Ivory eff ()
+          Ref s (Struct "alt_control_debug") -> Ivory eff ()
     }
 
-taskPositionPid :: PIDParams ParamReader -> AltEstimator -> Task p PositionPid
-taskPositionPid params alt_estimator = do
+monitorPositionPid :: AttrReadable a
+                   => a (Struct "pid_config")
+                   -> AltEstimator
+                   -> Monitor e PositionPid
+monitorPositionPid pid_config alt_estimator = do
   uniq <- fresh
-  ppid_state  <- taskLocal "posPidState"
+  ppid_state  <- state "posPidState"
+  ppid_config <- attrState pid_config
   let proc_pid_calculate :: Def('[IFloat, IFloat, IFloat] :-> IFloat)
       proc_pid_calculate = proc ("pos_pid_calculate" ++ show uniq) $
         \pos_sp vel_sp dt -> body $ do
-          ppid_params <- allocPIDParams params
-          p_gain <-             (ppid_params~>*pid_pGain)
-          d_gain <- fmap (/ dt) (ppid_params~>*pid_dGain)
+          p_gain <-             (ppid_config~>*C.p_gain)
+          d_gain <- fmap (/ dt) (ppid_config~>*C.d_gain)
 
           (pos_est, vel_est) <- ae_state alt_estimator
           pos_err            <- assign (pos_sp - pos_est)
@@ -44,21 +48,16 @@ taskPositionPid params alt_estimator = do
           pid_result <- assign ((p_gain * pos_err) - (d_gain * vel_err))
           ret (pid_result + vel_sp)
 
-  taskModuleDef $ do
+  monitorModuleDef $ do
     incl proc_pid_calculate
     depend controlPIDModule
   return PositionPid
     { pos_pid_init = do
         call_ pid_reset ppid_state
     , pos_pid_set_integral = \i ->
-        store (ppid_state ~> pid_iState) i
+        store (ppid_state ~> P.i_state) i
     , pos_pid_calculate = call proc_pid_calculate
     , pos_pid_write_debug = \r -> do
-        p <- deref (ppid_state ~> pid_pLast)
-        i <- deref (ppid_state ~> pid_iState)
-        d <- deref (ppid_state ~> pid_dLast)
-        store (r ~> A.pos_p) p
-        store (r ~> A.pos_i) i
-        store (r ~> A.pos_d) d
+        refCopy (r ~> A.pos) ppid_state
     }
 
