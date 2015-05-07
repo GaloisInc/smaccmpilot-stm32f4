08fa665a9d3d9532873407fc31f960c5d78efdd7
Pat Hickey <pat@galois.com>
2015-05-07 13:45:04 -0700

    smaccm-flight: hook up sensorManager to controllable vehicle attributes

---
+++ src/smaccm-flight/src/SMACCMPilot/Flight/Sensors.hs
@@ -0,0 +1,40 @@
+
+{-# LANGUAGE DataKinds #-}
+
+module SMACCMPilot.Flight.Sensors
+  ( sensorTower
+  ) where
+
+import Ivory.Language
+import Ivory.Tower
+
+import           SMACCMPilot.Comm.Tower.Attr
+import           SMACCMPilot.Comm.Tower.Interface.ControllableVehicle
+import           SMACCMPilot.Flight.Platform
+import           SMACCMPilot.Hardware.SensorManager
+
+sensorTower :: (e -> FlightPlatform)
+            -> ControllableVehicleAttrs Attr
+            -> Tower e ()
+sensorTower tofp attrs = do
+
+  (a,g,m,b) <- sensorManager (fp_sensors . tofp) (fp_clockconfig . tofp)
+
+  monitor "sensorAttrs" $ do
+
+    handler a "accel_attr_proxy" $ do
+      e <- attrEmitter (accelOutput attrs)
+      callback $ \v -> emit e v
+
+    handler g "gyro_attr_proxy" $ do
+      e <- attrEmitter (gyroOutput attrs)
+      callback $ \v -> emit e v
+
+    handler m "mag_attr_proxy" $ do
+      e <- attrEmitter (magOutput attrs)
+      callback $ \v -> emit e v
+
+    handler b "baro_attr_proxy" $ do
+      e <- attrEmitter (baroOutput attrs)
+      callback $ \v -> emit e v
+
