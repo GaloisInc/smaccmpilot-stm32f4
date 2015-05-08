64831178ad007118a182ff1c1c5f58edb6427c60
Pat Hickey <pat@galois.com>
2015-05-07 19:08:49 -0700

    smaccm-flight: uart datalink buffers incoming frames

--- src/smaccm-flight/src/SMACCMPilot/Flight/Datalink/UART.hs
+++ src/smaccm-flight/src/SMACCMPilot/Flight/Datalink/UART.hs
@@ -1,10 +1,13 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE DataKinds #-}
 
 module SMACCMPilot.Flight.Datalink.UART
   ( uartDatalink
   ) where
 
+import Ivory.Language
 import Ivory.Tower
+import Ivory.Tower.HAL.RingBuffer
 
 import Ivory.BSP.STM32.Driver.UART
 import SMACCMPilot.Commsec.Sizes
@@ -25,10 +28,43 @@ uartDatalink tocc uart baud k = do
   input_frames <- channel
 
   hxstreamDecodeTower "frame" uarti (fst input_frames)
-  -- TODO XXX FIXME: buffer input frames.
 
-  (a, output_frames) <- k (snd input_frames)
+  buffered_input_frames <- frameBuffer (snd input_frames)
+  -- Buffering timing analysis:
+  -- Worst case: 115200 baud
+  -- 10 bits per byte (UART framing) = 11520 bytes per second
+  -- 96+3 bytes per hx frame = 117 frames per second
+  -- 117 frames per second = 8.6ms per frame
+                                       (Milliseconds 5)
+  -- buffer depth of 4: we will never use more than 2, and it will fit 3.
+                                       (Proxy :: Proxy 4)
+
+  (a, output_frames) <- k buffered_input_frames
 
   hxstreamEncodeTower "frame" output_frames uarto
 
   return a
+
+
+frameBuffer :: forall a t n e
+             . (IvoryArea a, IvoryZero a, Time t, ANat n)
+            => ChanOutput a
+            -> t
+            -> Proxy n
+            -> Tower e (ChanOutput a)
+frameBuffer input pop_period _buf_size = do
+  out <- channel
+  p <- period pop_period
+  monitor "frameBuffer" $ do
+    (rb :: RingBuffer n a) <- monitorRingBuffer "frameBuffer"
+    handler input "push" $ do
+      callback $ \v -> do
+        _ <- ringbuffer_push rb v
+        return ()
+    handler p "periodic_pop" $ do
+      e <- emitter (fst out) 1
+      callback $ const $ do
+        v <- local izero
+        got <- ringbuffer_pop rb v
+        ifte_ got (emit e (constRef v)) (return ())
+  return (snd out)
