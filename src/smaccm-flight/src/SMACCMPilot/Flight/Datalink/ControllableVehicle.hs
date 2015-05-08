e3ff873d16c635437d85c476d6dc98006c48ddec
Pat Hickey <pat@galois.com>
2015-05-08 16:53:51 -0700

    smaccm-flight: remove unnecessary second buffering of datalink

--- src/smaccm-flight/src/SMACCMPilot/Flight/Datalink/ControllableVehicle.hs
+++ src/smaccm-flight/src/SMACCMPilot/Flight/Datalink/ControllableVehicle.hs
@@ -8,8 +8,6 @@ module SMACCMPilot.Flight.Datalink.ControllableVehicle
 import Ivory.Language
 import Ivory.Tower
 
-import Ivory.Tower.HAL.RingBuffer
-
 import SMACCMPilot.Comm.Tower.Attr
 import SMACCMPilot.Comm.Tower.Interface.ControllableVehicle
 
@@ -23,27 +21,11 @@ controllableVehicle :: forall e n
                                , ChanOutput (Array n (Stored Uint8))
                                )
 controllableVehicle input_frames = do
-  buffered_input <- channel
-
-  p <- period (Milliseconds 5)
-
-  monitor "frame_input_buffer" $ do
-    (rb :: RingBuffer 4 (Array n (Stored Uint8))) <- monitorRingBuffer "inputbuffer"
-    handler input_frames "input_frames" $ do
-      callback $ \v -> do
-        _ <- ringbuffer_push rb v
-        return ()
-    handler p "periodic_input_frame_pop" $ do
-      e <- emitter (fst buffered_input) 1
-      callback $ const $ do
-        v <- local izero
-        got <- ringbuffer_pop rb v
-        ifte_ got (emit e (constRef v)) (return ())
 
   attrs <- towerControllableVehicleAttrs initControllableVehicleAttrs
   streams <- towerControllableVehicleStreams
 
-  cvc <- controllableVehicleConsumerInput (snd buffered_input)
+  cvc <- controllableVehicleConsumerInput input_frames
   cvp <- towerControllableVehicleServer cvc attrs (snd streams)
   output_frames <- controllableVehicleProducerOutput cvp
 
