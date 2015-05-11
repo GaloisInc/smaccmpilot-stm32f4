3f7fe772095b6346a401e7c5c36910c01ae1fefe
Pat Hickey <pat@galois.com>
2015-05-11 14:43:27 -0700

    smaccm-flight: can server runs sensors.

--- src/smaccm-flight/src/SMACCMPilot/Flight/Datalink/CAN/TestServer.hs
+++ src/smaccm-flight/src/SMACCMPilot/Flight/Datalink/CAN/TestServer.hs
@@ -9,6 +9,7 @@ import Ivory.Tower
 import SMACCMPilot.Flight.Platform
 import SMACCMPilot.Flight.Datalink.ControllableVehicle
 import SMACCMPilot.Flight.Datalink.CAN
+import SMACCMPilot.Flight.Sensors
 import SMACCMPilot.Hardware.CAN
 
 
@@ -25,7 +26,9 @@ app tofp = do
       let emptyID = CANFilterID32 (fromRep 0) (fromRep 0) False False
       canFilterInit (can_filters can) [CANFilterBank CANFIFO0 CANFilterMask $ CANFilter32 emptyID emptyID] []
 
-  (_attrs, _streams) <- canDatalink canTx canRx controllableVehicle
+  (attrs, _streams) <- canDatalink canTx canRx controllableVehicle
+
+  sensorTower tofp attrs
 
   return ()
   where
