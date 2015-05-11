202c1ccf3ed5d748ccf3cb165a36d89811ca4dea
Pat Hickey <pat@galois.com>
2015-05-11 15:15:49 -0700

    smaccm-commsec: factor out chaninput/output variants on tower components

--- src/smaccm-commsec/src/SMACCMPilot/Commsec/Tower.hs
+++ src/smaccm-commsec/src/SMACCMPilot/Commsec/Tower.hs
@@ -1,9 +1,13 @@
 
 module SMACCMPilot.Commsec.Tower
   ( commsecEncodeTower
+  , commsecEncodeTower'
   , commsecDecodeTower
+  , commsecDecodeTower'
   , keyableCommsecEncodeTower
+  , keyableCommsecEncodeTower'
   , keyableCommsecDecodeTower
+  , keyableCommsecDecodeTower'
   , commsecTowerDeps
   ) where
 
@@ -20,7 +24,17 @@ commsecEncodeTower :: String
                    -> Init SymKeySaltArray
                    -> ChanOutput PlaintextArray
                    -> Tower p (ChanOutput CyphertextArray)
-commsecEncodeTower n ks_ival ct_chan = do
+commsecEncodeTower n ks_ival pt_chan = do
+  ct_chan <- channel
+  commsecEncodeTower' n ks_ival pt_chan (fst ct_chan)
+  return (snd ct_chan)
+
+commsecEncodeTower' :: String
+                   -> Init SymKeySaltArray
+                   -> ChanOutput PlaintextArray
+                   -> ChanInput CyphertextArray
+                   -> Tower p ()
+commsecEncodeTower' n ks_ival pt_chan ct_chan = do
   ks_chan <- channel
   monitor "commsecEncodeStaticKey" $ do
     handler systemInit "configureStaticKey" $ do
@@ -28,18 +42,27 @@ commsecEncodeTower n ks_ival ct_chan = do
       callback $ const $ do
         ks <- local ks_ival
         emit e (constRef ks)
-  keyableCommsecEncodeTower n (snd ks_chan) ct_chan
+  keyableCommsecEncodeTower' n (snd ks_chan) pt_chan ct_chan
 
 keyableCommsecEncodeTower :: String
                           -> ChanOutput SymKeySaltArray
                           -> ChanOutput PlaintextArray
                           -> Tower p (ChanOutput CyphertextArray)
 keyableCommsecEncodeTower n ks_chan pt_chan = do
+  ct_chan <- channel
+  keyableCommsecEncodeTower' n ks_chan pt_chan (fst ct_chan)
+  return (snd ct_chan)
+
+keyableCommsecEncodeTower' :: String
+                          -> ChanOutput SymKeySaltArray
+                          -> ChanOutput PlaintextArray
+                          -> ChanInput CyphertextArray
+                          -> Tower p ()
+keyableCommsecEncodeTower' n ks_chan pt_chan ct_chan = do
   commsecTowerDeps
   f <- freshname n
   let named t = t ++ "_" ++ showUnique f
       ce = gecEncode (named "ctx")
-  ct_chan <- channel
   monitor "commsecEncodeState" $ do
     validkey <- stateInit "validkey" (ival false)
     monitorModuleDef $ gec_encode_moddef ce
@@ -48,7 +71,7 @@ keyableCommsecEncodeTower n ks_chan pt_chan = do
         gec_encode_init ce sk
         store validkey true
     handler pt_chan "plaintext_encode" $ do
-      e <- emitter (fst ct_chan) 1
+      e <- emitter ct_chan 1
       callback $ \ pt -> do
         vk <- deref validkey
         when vk $ do
@@ -56,13 +79,22 @@ keyableCommsecEncodeTower n ks_chan pt_chan = do
           res <- gec_encode_run ce pt ct
           when (res ==? success) $
             emit e (constRef ct)
-  return (snd ct_chan)
 
 commsecDecodeTower :: String
                    -> Init SymKeySaltArray
                    -> ChanOutput CyphertextArray
                    -> Tower p (ChanOutput PlaintextArray)
 commsecDecodeTower n ks_ival ct_chan = do
+  pt_chan <- channel
+  commsecDecodeTower' n ks_ival ct_chan (fst pt_chan)
+  return (snd pt_chan)
+
+commsecDecodeTower' :: String
+                   -> Init SymKeySaltArray
+                   -> ChanOutput CyphertextArray
+                   -> ChanInput PlaintextArray
+                   -> Tower p ()
+commsecDecodeTower' n ks_ival ct_chan pt_chan = do
   ks_chan <- channel
   monitor "commsecDecodeStaticKey" $ do
     handler systemInit "configureStaticKey" $ do
@@ -70,7 +102,7 @@ commsecDecodeTower n ks_ival ct_chan = do
       callback $ const $ do
         ks <- local ks_ival
         emit e (constRef ks)
-  keyableCommsecDecodeTower n (snd ks_chan) ct_chan
+  keyableCommsecDecodeTower' n (snd ks_chan) ct_chan pt_chan
 
 
 keyableCommsecDecodeTower :: String
@@ -78,11 +110,20 @@ keyableCommsecDecodeTower :: String
                           -> ChanOutput CyphertextArray
                           -> Tower p (ChanOutput PlaintextArray)
 keyableCommsecDecodeTower n ks_chan ct_chan = do
+  pt_chan <- channel
+  keyableCommsecDecodeTower' n ks_chan ct_chan (fst pt_chan)
+  return (snd pt_chan)
+
+keyableCommsecDecodeTower' :: String
+                          -> ChanOutput SymKeySaltArray
+                          -> ChanOutput CyphertextArray
+                          -> ChanInput PlaintextArray
+                          -> Tower p ()
+keyableCommsecDecodeTower' n ks_chan ct_chan pt_chan = do
   commsecTowerDeps
   f <- freshname n
   let named t = t ++ "_" ++ showUnique f
       cd = gecDecode (named "ctx")
-  pt_chan <- channel
   monitor "commsecDecodeState" $ do
     validkey <- stateInit "validkey" (ival false)
     monitorModuleDef $ gec_decode_moddef cd
@@ -91,7 +132,7 @@ keyableCommsecDecodeTower n ks_chan ct_chan = do
         gec_decode_init cd sk
         store validkey true
     handler ct_chan "cyphertext_decode" $ do
-      e <- emitter (fst pt_chan) 1
+      e <- emitter pt_chan 1
       callback $ \ ct -> do
         vk <- deref validkey
         when vk $ do
@@ -99,7 +140,6 @@ keyableCommsecDecodeTower n ks_chan ct_chan = do
           res <- gec_decode_run cd ct pt
           when (res ==? success) $
             emit e (constRef pt)
-  return (snd pt_chan)
 
 commsecTowerDeps :: Tower p ()
 commsecTowerDeps = mapM_ towerArtifact commsecArtifacts
