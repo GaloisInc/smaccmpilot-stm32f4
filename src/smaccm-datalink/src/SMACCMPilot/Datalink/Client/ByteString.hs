fea028801e4027dbbc672ca2ead1d600311f8a0f
Jamey Sharp <jamey@galois.com>
2015-05-13 14:51:16 -0700

    Simpler comma-separated string construction.

--- src/smaccm-datalink/src/SMACCMPilot/Datalink/Client/ByteString.hs
+++ src/smaccm-datalink/src/SMACCMPilot/Datalink/Client/ByteString.hs
@@ -2,6 +2,7 @@ module SMACCMPilot.Datalink.Client.ByteString where
 
 import           Data.ByteString               (ByteString)
 import qualified Data.ByteString            as B
+import           Data.List
 import           Text.Printf
 
 bytestringPad :: Integer -> ByteString -> Either String ByteString
@@ -16,10 +17,8 @@ bytestringPad l bs
 bytestringShowHex :: ByteString -> String
 bytestringShowHex bs = printf "[%s](%d)" body (B.length bs)
   where
-  body = fixup (unwords (map hexdig (B.unpack bs)))
-  hexdig = printf "0x%0.2x,"
-  -- Drop last char because the above map/unwords is bad hack
-  fixup = reverse . drop 1 . reverse
+  body = intercalate ", " (map hexdig (B.unpack bs))
+  hexdig = printf "0x%0.2x"
 
 bytestringDebug :: String -> ByteString -> String
 bytestringDebug tag bs = printf "%s ByteString %s" tag (bytestringShowHex bs)
