ecc3176776a64d17d9e2d51c20e67648250053a2
Jamey Sharp <jamey@galois.com>
2015-05-13 13:45:20 -0700

    gcs.sh: bash magic!

--- src/smaccm-comm-client/gcs.sh
+++ src/smaccm-comm-client/gcs.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
+
+if [ $# -lt 2 ]; then
+	echo "Usage: $0 <serial> <baud> [options]..." >&2
+	exit 1
+fi
+
+serial=$1
+baud=$2
+shift 2
+
 cabal run smaccm-comm-client -- \
-	--serial=$1 \
-	--baud=$2 \
+	--serial=$serial \
+	--baud=$baud \
 	--conf-file=../smaccm-flight/fmu24.conf \
-	--conf-path=../smaccm-flight
+	--conf-path=../smaccm-flight \
+	"$@"
