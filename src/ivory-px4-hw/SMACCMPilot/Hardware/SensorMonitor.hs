c0d80882c1054b15c333484da15fcd2745b317f8
Pat Hickey <pat@galois.com>
2015-05-19 14:56:57 -0700

    ivory-px4-hw: factor SensorsMonitor into a library and test

---
+++ src/ivory-px4-hw/SMACCMPilot/Hardware/SensorMonitor.hs
@@ -0,0 +1,114 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE RecordWildCards #-}
+
+module SMACCMPilot.Hardware.SensorMonitor
+  ( SensorHandlers(..)
+  , decoder
+  ) where
+
+import Data.Char (ord)
+import SMACCMPilot.Datalink.HXStream.Ivory
+import Ivory.Language
+import Ivory.Serialize
+import Ivory.Stdlib
+import SMACCMPilot.Hardware.GPS.Types
+import SMACCMPilot.Comm.Ivory.Types.AccelerometerSample
+import SMACCMPilot.Comm.Ivory.Types.GyroscopeSample
+import SMACCMPilot.Comm.Ivory.Types.BarometerSample
+import SMACCMPilot.Comm.Ivory.Types.MagnetometerSample
+
+data SensorHandlers =
+  SensorHandlers
+    { sh_baro  :: forall s eff . ConstRef s (Struct "barometer_sample") -> Ivory eff ()
+    , sh_mag   :: forall s eff . ConstRef s (Struct "magnetometer_sample") -> Ivory eff ()
+    , sh_gyro  :: forall s eff . ConstRef s (Struct "gyroscope_sample") -> Ivory eff ()
+    , sh_accel :: forall s eff . ConstRef s (Struct "accelerometer_sample") -> Ivory eff ()
+    , sh_gps   :: forall s eff . ConstRef s (Struct "position") -> Ivory eff ()
+    , sh_moddef :: ModuleDef
+    }
+
+decoder :: SensorHandlers -> [Module]
+decoder SensorHandlers{..} = decoderModule : dependencies
+  where
+  decoder_proc :: Def ('[] :-> Sint32)
+  decoder_proc = proc "main" $ body $ do
+    hxstate <- local initStreamState
+    buf <- local (izero :: Init (Array 256 (Stored Uint8)))
+    forever $ do
+      nextchar <- call getchar
+      when (nextchar <? 0) breakOut
+      noReturn $ noBreak $ decodes
+        [ handler buf 'b' baro_proc
+        , handler buf 'm' mag_proc
+        , handler buf 'g' gyro_proc
+        , handler buf 'a' accel_proc
+        , handler buf 'p' gps_proc
+        ]
+        hxstate
+        (castDefault nextchar)
+    ret 0
+
+  decoderModule :: Module
+  decoderModule = package "decoder" $ do
+    mapM_ depend dependencies
+    incl baro_proc
+    incl mag_proc
+    incl gyro_proc
+    incl accel_proc
+    incl gps_proc
+    incl getchar
+    incl decoder_proc
+    sh_moddef
+
+  dependencies =
+    [ gpsTypesModule
+    , serializeModule
+    , hxstreamModule
+    , accelerometerSampleTypesModule
+    , gyroscopeSampleTypesModule
+    , magnetometerSampleTypesModule
+    , barometerSampleTypesModule
+    ]
+
+  baro_proc :: Def ('[ConstRef s (Struct "barometer_sample")] :-> ())
+  baro_proc = proc "baro" $ \ v -> body $ do
+    sh_baro v
+
+  mag_proc :: Def ('[ConstRef s (Struct "magnetometer_sample")] :-> ())
+  mag_proc = proc "mag" $ \ v -> body $ do
+    sh_mag v
+
+  gyro_proc :: Def ('[ConstRef s (Struct "gyroscope_sample")] :-> ())
+  gyro_proc = proc "gyro" $ \ v -> body $ do
+    sh_gyro v
+
+  accel_proc :: Def ('[ConstRef s (Struct "accelerometer_sample")] :-> ())
+  accel_proc = proc "accel" $ \ v -> body $ do
+    sh_accel v
+
+  gps_proc :: Def ('[ConstRef s (Struct "position")] :-> ())
+  gps_proc = proc "gps" $ \ v -> body $ do
+    sh_gps v
+
+handler :: (ANat len, IvoryArea a, IvoryZero a, Packable a)
+        => Ref s1 (Array len (Stored Uint8))
+        -> Char
+        -> (forall s2. Def ('[ConstRef s2 a] :-> ()))
+        -> FrameHandler
+handler buf tag consumer = mkFrameHandler ScopedFrameHandler
+  { fhTag = fromIntegral $ ord tag
+  , fhBegin = return ()
+  , fhData = \ b off -> do
+      assert $ off <? arrayLen buf
+      store (buf ! toIx off) b
+  , fhEnd = do
+      result <- local izero
+      unpackFrom (constRef buf) 0 result
+      call_ consumer $ constRef result
+  }
+
+
+getchar :: Def ('[] :-> Sint32)
+getchar = importProc "getchar" "stdio.h"
--- src/ivory-px4-hw/tests/SensorsMonitor.hs
+++ src/ivory-px4-hw/tests/SensorsMonitor.hs
@@ -4,111 +4,37 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Data.List
-import Data.Char (ord)
 import Ivory.Artifact
 import qualified Ivory.Compile.C.CmdlineFrontend as C (compile)
-import SMACCMPilot.Datalink.HXStream.Ivory
 import Ivory.Language
 import Ivory.Serialize
-import Ivory.Stdlib
-import SMACCMPilot.Hardware.GPS.Types
-import SMACCMPilot.Comm.Ivory.Types (typeModules)
-import SMACCMPilot.Comm.Ivory.Types.AccelerometerSample
-import SMACCMPilot.Comm.Ivory.Types.GyroscopeSample
-import SMACCMPilot.Comm.Ivory.Types.BarometerSample
-import SMACCMPilot.Comm.Ivory.Types.MagnetometerSample
 
-handler :: (ANat len, IvoryArea a, IvoryZero a, Packable a)
-        => Ref s1 (Array len (Stored Uint8))
-        -> Char
-        -> (forall s2. Def ('[ConstRef s2 a] :-> ()))
-        -> FrameHandler
-handler buf tag consumer = mkFrameHandler ScopedFrameHandler
-  { fhTag = fromIntegral $ ord tag
-  , fhBegin = return ()
-  , fhData = \ b off -> do
-      assert $ off <? arrayLen buf
-      store (buf ! toIx off) b
-  , fhEnd = do
-      result <- local izero
-      unpackFrom (constRef buf) 0 result
-      call_ consumer $ constRef result
-  }
-
-baro :: Def ('[ConstRef s (Struct "barometer_sample")] :-> ())
-baro = proc "baro" $ \ _ -> body $ do
-  call_ puts "baro"
-
-mag :: Def ('[ConstRef s (Struct "magnetometer_sample")] :-> ())
-mag = proc "mag" $ \ _ -> body $ do
-  call_ puts "mag"
-
-gyro :: Def ('[ConstRef s (Struct "gyroscope_sample")] :-> ())
-gyro = proc "gyro" $ \ _ -> body $ do
-  call_ puts "gyro"
-
-accel :: Def ('[ConstRef s (Struct "accelerometer_sample")] :-> ())
-accel = proc "accel" $ \ _ -> body $ do
-  call_ puts "accel"
-
-gps :: Def ('[ConstRef s (Struct "position")] :-> ())
-gps = proc "gps" $ \ _ -> body $ do
-  call_ puts "gps"
-
-getchar :: Def ('[] :-> Sint32)
-getchar = importProc "getchar" "stdio.h"
+import SMACCMPilot.Hardware.SensorMonitor
 
 puts :: Def('[IString]:-> Sint32)
 puts = importProc "puts" "stdio.h"
 
-
-decoder :: Def ('[] :-> Sint32)
-decoder = proc "main" $ body $ do
-  hxstate <- local initStreamState
-  buf <- local (izero :: Init (Array 256 (Stored Uint8)))
-  forever $ do
-    nextchar <- call getchar
-    when (nextchar <? 0) breakOut
-    noReturn $ noBreak $ decodes
-      [ handler buf 'b' baro
-      , handler buf 'm' mag
-      , handler buf 'g' gyro
-      , handler buf 'a' accel
-      , handler buf 'p' gps
-      ]
-      hxstate
-      (castDefault nextchar)
-  ret 0
-
-decoderModule :: Module
-decoderModule = package "decoder" $ do
-  depend gpsTypesModule
-  depend accelerometerSampleTypesModule
-  depend gyroscopeSampleTypesModule
-  depend magnetometerSampleTypesModule
-  depend barometerSampleTypesModule
-  depend serializeModule
-  depend hxstreamModule
-  incl baro
-  incl mag
-  incl gyro
-  incl accel
-  incl gps
-  incl getchar
-  incl puts
-  incl decoder
-
 main :: IO ()
 main = C.compile modules artifacts
   where
-  modules = [decoderModule, gpsTypesModule, serializeModule, hxstreamModule]
-         ++ typeModules
+  modules = decoder $ SensorHandlers
+    { sh_baro   = const $ call_ puts "baro"
+    , sh_mag    = const $ call_ puts "mag"
+    , sh_gyro   = const $ call_ puts "gyro"
+    , sh_accel  = const $ call_ puts "accel"
+    , sh_gps    = const $ call_ puts "gps"
+    , sh_moddef = incl puts
+    }
+
   artifacts = makefile : runscript : serializeArtifacts
+
   makefile = Root $ artifactString "Makefile" $ unlines [
       "CC = gcc",
       "CFLAGS = -Wall -O0 -g -I. -DIVORY_TEST",
       "LDLIBS = -lm",
       "OBJS = " ++ intercalate " " [ moduleName m ++ ".o" | m <- modules ],
+      "default: decoder",
+      "\tchmod +x run.sh",
       "decoder: $(OBJS)",
       "clean:",
       "\t-rm -f $(OBJS)",
