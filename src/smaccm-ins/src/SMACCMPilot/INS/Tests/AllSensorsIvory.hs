662d14aeddf56d5214b9f2bb84ba1604734e9219
Pat Hickey <pat@galois.com>
2015-05-20 13:56:45 -0700

    smaccm-ins: refactor all sensors ivory client so we can print inputs and outputs

--- src/smaccm-ins/src/SMACCMPilot/INS/Tests/AllSensorsIvory.hs
+++ src/smaccm-ins/src/SMACCMPilot/INS/Tests/AllSensorsIvory.hs
@@ -10,7 +10,6 @@ import Data.List (intercalate)
 
 import Ivory.Language
 import Ivory.Stdlib
-import Ivory.Serialize
 import Ivory.Artifact
 import qualified Ivory.Compile.C.CmdlineFrontend as C (compile)
 
@@ -26,77 +25,6 @@ import qualified SMACCMPilot.Comm.Ivory.Types.BarometerSample     as B
 import qualified SMACCMPilot.Comm.Ivory.Types.Xyz                 as XYZ
 import qualified SMACCMPilot.Comm.Ivory.Types.TimeMicros          as T
 
-kalman_state :: MemArea (Struct "kalman_state")
-kalman_state = area "kalman_state" Nothing
-
-kalman_covariance :: MemArea (Struct "kalman_covariance")
-kalman_covariance = area "kalman_covariance" Nothing
-
-kalman_init :: Def ('[IFloat, IFloat, IFloat, IFloat, IFloat, IFloat, IFloat] :-> ())
-kalman_init = proc "kalman_init" $ \ accX accY accZ magX magY magZ pressure -> body $ do
-  let acc = xyz accX accY accZ
-  let mag = xyz magX magY magZ
-  kalmanInit (addrOf kalman_state) (addrOf kalman_covariance) acc mag pressure
-
-kalman_predict :: Def ('[IFloat, IFloat, IFloat, IFloat, IFloat, IFloat, IFloat] :-> ())
-kalman_predict = proc "kalman_predict" $ \ dt dax day daz dvx dvy dvz -> body $ do
-  let distVector = DisturbanceVector { disturbanceGyro = xyz dax day daz -- delta angle
-                                     , disturbanceAccel = xyz dvx dvy dvz } -- delta velocity
-  kalmanPredict (addrOf kalman_state) (addrOf kalman_covariance) dt distVector
-
-kalman_output :: Def ('[]:->())
-kalman_output = proc "kalman_output" $ body $ do
-  print_array (addrOf kalman_state ~> orient)
-  print_array (addrOf kalman_state ~> vel)
-  print_array (addrOf kalman_state ~> pos)
-  endl
-  where
-  print_array a = arrayMap $ \ix ->
-    deref (a ! ix) >>= print_float
-
-  print_float :: IFloat -> Ivory eff ()
-  print_float v = call_ printf_float "%f " v
-
-  endl :: Ivory eff ()
-  endl = call_ puts ""
-
-vel_measure :: Def ('[IFloat, IFloat, IFloat] :-> ())
-vel_measure = proc "vel_measure" $ \ velN velE velD -> body $ do
-  velMeasure (addrOf kalman_state) (addrOf kalman_covariance) $ ned velN velE velD
-
-pos_measure :: Def ('[IFloat, IFloat, IFloat] :-> ())
-pos_measure = proc "pos_measure" $ \ posN posE posD -> body $ do
-  posMeasure (addrOf kalman_state) (addrOf kalman_covariance) $ ned posN posE posD
-
-pressure_measure :: Def ('[IFloat] :-> ())
-pressure_measure = proc "pressure_measure" $ \ pressure -> body $ do
-  pressureMeasure (addrOf kalman_state) (addrOf kalman_covariance) pressure
-
-tas_measure :: Def ('[IFloat] :-> ())
-tas_measure = proc "tas_measure" $ \ tas -> body $ do
-  tasMeasure (addrOf kalman_state) (addrOf kalman_covariance) tas
-
-mag_measure :: Def ('[IFloat, IFloat, IFloat] :-> ())
-mag_measure = proc "mag_measure" $ \ magX magY magZ -> body $ do
-  magMeasure (addrOf kalman_state) (addrOf kalman_covariance) $ xyz magX magY magZ
-
-
-ins_module :: Module
-ins_module = package "smaccm_ins" $ do
-  defStruct (Proxy :: Proxy "kalman_state")
-  defStruct (Proxy :: Proxy "kalman_covariance")
-  defMemArea kalman_state
-  defMemArea kalman_covariance
-  incl kalman_init
-  incl kalman_predict
-  incl kalman_output
-  incl vel_measure
-  incl pos_measure
-  incl pressure_measure
-  incl tas_measure
-  incl mag_measure
-  incl printf_float
-  incl puts
 
 printf_float :: Def('[IString, IFloat] :-> ())
 printf_float = importProc "printf" "stdio.h"
@@ -104,7 +32,7 @@ printf_float = importProc "printf" "stdio.h"
 puts :: Def('[IString] :-> ())
 puts = importProc "puts" "stdio.h"
 
-sensorMonitor :: [Module]
+sensorMonitor :: ([Module], [Located Artifact])
 sensorMonitor = decoder $ SensorHandlers
   { sh_baro = \baro_sample -> do
       refCopy baro_buf baro_sample
@@ -147,7 +75,7 @@ sensorMonitor = decoder $ SensorHandlers
       defMemArea baro_area
       defMemArea init_area
       defMemArea timestamp_area
-      depend ins_module
+      ins_moddef
   }
 
   where
@@ -210,27 +138,109 @@ sensorMonitor = decoder $ SensorHandlers
   baro_area = area "baro_latest" Nothing
   baro_buf = addrOf baro_area
 
+
+  kalman_state :: MemArea (Struct "kalman_state")
+  kalman_state = area "kalman_state" Nothing
+
+  kalman_covariance :: MemArea (Struct "kalman_covariance")
+  kalman_covariance = area "kalman_covariance" Nothing
+
+  kalman_init :: Def ('[IFloat, IFloat, IFloat, IFloat, IFloat, IFloat, IFloat] :-> ())
+  kalman_init = proc "kalman_init" $ \ accX accY accZ magX magY magZ pressure -> body $ do
+    let acc = xyz accX accY accZ
+    let mag = xyz magX magY magZ
+    kalmanInit (addrOf kalman_state) (addrOf kalman_covariance) acc mag pressure
+
+  kalman_predict :: Def ('[IFloat, IFloat, IFloat, IFloat, IFloat, IFloat, IFloat] :-> ())
+  kalman_predict = proc "kalman_predict" $ \ dt dax day daz dvx dvy dvz -> body $ do
+    let distVector = DisturbanceVector { disturbanceGyro = xyz dax day daz -- delta angle
+                                       , disturbanceAccel = xyz dvx dvy dvz } -- delta velocity
+    kalmanPredict (addrOf kalman_state) (addrOf kalman_covariance) dt distVector
+
+  kalman_output :: Def ('[]:->())
+  kalman_output = proc "kalman_output" $ body $ do
+    accel_get_sample accel_buf >>= print_three_floats
+    gyro_get_sample  gyro_buf  >>= print_three_floats
+    mag_get_sample   mag_buf   >>= print_three_floats
+    baro_get_sample  baro_buf  >>= print_float
+
+    print_array (addrOf kalman_state ~> orient)
+    print_array (addrOf kalman_state ~> vel)
+    print_array (addrOf kalman_state ~> pos)
+    endl
+    where
+    print_array a = arrayMap $ \ix ->
+      deref (a ! ix) >>= print_float
+    print_three_floats (x,y,z) = do
+      print_float x
+      print_float y
+      print_float z
+
+
+    print_float :: IFloat -> Ivory eff ()
+    print_float v = call_ printf_float "%f " v
+
+    endl :: Ivory eff ()
+    endl = call_ puts ""
+
+  vel_measure :: Def ('[IFloat, IFloat, IFloat] :-> ())
+  vel_measure = proc "vel_measure" $ \ velN velE velD -> body $ do
+    velMeasure (addrOf kalman_state) (addrOf kalman_covariance) $ ned velN velE velD
+
+  pos_measure :: Def ('[IFloat, IFloat, IFloat] :-> ())
+  pos_measure = proc "pos_measure" $ \ posN posE posD -> body $ do
+    posMeasure (addrOf kalman_state) (addrOf kalman_covariance) $ ned posN posE posD
+
+  pressure_measure :: Def ('[IFloat] :-> ())
+  pressure_measure = proc "pressure_measure" $ \ pressure -> body $ do
+    pressureMeasure (addrOf kalman_state) (addrOf kalman_covariance) pressure
+
+  tas_measure :: Def ('[IFloat] :-> ())
+  tas_measure = proc "tas_measure" $ \ tas -> body $ do
+    tasMeasure (addrOf kalman_state) (addrOf kalman_covariance) tas
+
+  mag_measure :: Def ('[IFloat, IFloat, IFloat] :-> ())
+  mag_measure = proc "mag_measure" $ \ magX magY magZ -> body $ do
+    magMeasure (addrOf kalman_state) (addrOf kalman_covariance) $ xyz magX magY magZ
+
+
+  ins_moddef :: ModuleDef
+  ins_moddef = do
+    defStruct (Proxy :: Proxy "kalman_state")
+    defStruct (Proxy :: Proxy "kalman_covariance")
+    defMemArea kalman_state
+    defMemArea kalman_covariance
+    incl kalman_init
+    incl kalman_predict
+    incl kalman_output
+    incl vel_measure
+    incl pos_measure
+    incl pressure_measure
+    incl tas_measure
+    incl mag_measure
+    incl printf_float
+    incl puts
+
+
+
+
 app :: IO ()
 app = C.compile modules artifacts
   where
-  modules = [ins_module] ++ sensorMonitor
-  artifacts = [makefile, runscript] ++ serializeArtifacts
+  (sens_modules, sens_artifacts) = sensorMonitor
+  modules = sens_modules
+  artifacts = makefile : sens_artifacts
 
-  exename = moduleName ins_module
-  makefile = Root $ artifactString "Makefile" $ unlines [
-      "CC = gcc",
-      "CFLAGS = -Wall -Os -std=c99 -Wno-unused-variable -I.",
-      "LDLIBS = -lm",
-      "OBJS = " ++ intercalate " " [ moduleName m ++ ".o" | m <- modules ],
-      "default: " ++ exename,
-      "\tchmod +x run.sh",
-      exename ++ ": $(OBJS)",
-      "clean:",
-      "\t-rm -f $(OBJS)",
-      ".PHONY: clean"
-    ]
-  runscript = Root $ artifactString "run.sh" $ unlines
-    [ "#!/bin/sh"
-    , "stty raw 115200 < $1"
-    , "$(dirname $0)/ " ++ exename ++ " < $1"
+  exename = "decoder"
+
+  makefile = Root $ artifactString "Makefile" $ unlines
+    [ "CC = gcc"
+    , "CFLAGS = -Wall -Os -std=c99 -Wno-unused-variable -I."
+    , "LDLIBS = -lm"
+    , "OBJS = " ++ intercalate " " [ moduleName m ++ ".o" | m <- modules ]
+      ++ " termios_helpers.o"
+    , exename ++ ": $(OBJS)"
+    , "clean:"
+    , "\t-rm -f $(OBJS)"
+    , ".PHONY: clean"
     ]
