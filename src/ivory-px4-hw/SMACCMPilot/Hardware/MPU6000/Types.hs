115458fa41a110871c4bb964a52d736243de40b3
Pat Hickey <pat@galois.com>
2015-05-20 17:01:18 -0700

    ivory-px4-hw: start adding struct so we can deserialize mpu6000 response

---
+++ src/ivory-px4-hw/SMACCMPilot/Hardware/MPU6000/Types.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TypeOperators #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+module SMACCMPilot.Hardware.MPU6000.Types where
+
+import Ivory.Language
+import Ivory.Serialize
+
+[ivory|
+struct mpu6000_response
+  { ax   :: Stored Sint16
+  ; ay   :: Stored Sint16
+  ; az   :: Stored Sint16
+  ; temp :: Stored Uint16
+  ; gx   :: Stored Sint16
+  ; gy   :: Stored Sint16
+  ; gz   :: Stored Sint16
+  }
+|]
+
+mpu6000ResponseWrapper :: WrappedPackRep (Struct "mpu6000_response")
+mpu6000ResponseWrapper = wrapPackRep "mpu6000_response" $ packStruct
+  [ packLabel ax
+  , packLabel ay
+  , packLabel az
+  , packLabel temp
+  , packLabel gx
+  , packLabel gy
+  , packLabel gz
+  ]
+
+mpu6000TypesModule :: Module
+mpu6000TypesModule = package "mpu6000_types" $ do
+  defStruct (Proxy :: Proxy "mpu6000_response")
+  depend serializeModule
+  wrappedPackMod mpu6000ResponseWrapper
+
+instance Packable (Struct "mpu6000_response") where
+  packRep = wrappedPackRep mpu6000ResponseWrapper
+
