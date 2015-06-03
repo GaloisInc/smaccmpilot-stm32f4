0703176080f31d1e2454d943f18db945d36015ac
Jamey Sharp <jamey@galois.com>
2015-06-02 17:54:08 -0700

    Split plotter.py to a column-slicing slice.py and a simple plotter.

--- src/smaccm-ins/plotter.py
+++ src/smaccm-ins/plotter.py
@@ -1,88 +1,27 @@
 #!/usr/bin/env python
 
+import matplotlib.pyplot as plt
+import numpy
 import sys
-import pylab
-import math
-
-columns =   [ "time"
-            , "ax"
-            , "ay"
-            , "az"
-            , "gx"
-            , "gy"
-            , "gz"
-            , "mx"
-            , "my"
-            , "mz"
-            , "q0" # 10
-            , "q1" # 11
-            , "q2" # 12
-            , "q3" # 13
-            , "gbiasx"
-            , "gbiasy"
-            , "gbiasz"
-            , "magn"
-            , "mage"
-            , "magd"
-            , "magx"
-            , "magy"
-            , "magz"
-            ]
-
-def euler_angle(q):
-    q0 = q[0]
-    q1 = q[1]
-    q2 = q[2]
-    q3 = q[3]
-    return [ math.atan2( 2 * (q0 * q1 + q2 * q3), 1 - 2 * (q1 * q1 + q2 * q2))
-           , math.asin(2 * (q0 * q2 - q3 * q1))
-           , math.atan2(2 * (q0 * q3 + q1 * q2), 1 - 2 * (q2 * q2 + q3 * q3))
-           ]
-
-def raw_gyro(rad):
-    return rad / math.pi * 180 * 16.4
-
-calculated_columns = dict(
-    { "roll": lambda (row) : euler_angle(row[10:14])[0]
-    , "pitch": lambda (row) : euler_angle(row[10:14])[1]
-    , "yaw": lambda (row) : euler_angle(row[10:14])[2]
-    , "rgx": lambda (row) : raw_gyro(row[4])
-    , "rgy": lambda (row) : raw_gyro(row[5])
-    , "rgz": lambda (row) : raw_gyro(row[6])
-    })
-
-def parse(f, cs):
-    rows = dict({})
-    for c in cs:
-        rows[c] = []
-    for c in calculated_columns:
-        rows[c] = []
-    for line in f:
-        elems = line.split(' ')
-        row = []
-        for e in elems:
-            try:
-                row.append(float(e))
-            except ValueError:
-                pass
-        if len(row) == len(cs):
-            for i in range(0,len(row)):
-                rows[cs[i]].append(row[i])
-            for c in calculated_columns:
-                rows[c].append(calculated_columns[c](row))
-    return rows
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        rows = parse(sys.stdin, columns);
-        for arg in sys.argv[1:]:
-            if arg in rows:
-                pylab.plot(rows["time"], rows[arg], label=arg)
-            else:
-                print "no column named " + arg
-        pylab.legend()
-        pylab.show()
+    columnnames = sys.stdin.readline().strip().split()
+    columns = list(numpy.loadtxt(sys.stdin, unpack=True))
+
+    times = None
+    try:
+        timecol = columnnames.index("time")
+    except ValueError:
+        pass
     else:
-        print "must provide columns as arguments"
-        print "valid columns:"
-        print (columns + calculated_columns.keys())
+        times = columns.pop(timecol)
+        del columnnames[timecol]
+
+    for name, column in zip(columnnames, columns):
+        if times is not None:
+            plt.plot(times, column, label=name)
+        else:
+            plt.plot(column, label=name)
+
+    plt.legend()
+    plt.show()
---
+++ src/smaccm-ins/slice.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+import math
+import sys
+
+columns = [ "time"
+          , "ax"
+          , "ay"
+          , "az"
+          , "gx"
+          , "gy"
+          , "gz"
+          , "mx"
+          , "my"
+          , "mz"
+          , "q0"
+          , "q1"
+          , "q2"
+          , "q3"
+          , "gbiasx"
+          , "gbiasy"
+          , "gbiasz"
+          , "magn"
+          , "mage"
+          , "magd"
+          , "magx"
+          , "magy"
+          , "magz"
+          ]
+
+def raw_gyro(rad):
+    return rad / math.pi * 180 * 16.4
+
+calculated_columns = dict(
+    { "roll":    (("q0", "q1", "q2", "q3"), lambda q0, q1, q2, q3: math.atan2(2 * (q0 * q1 + q2 * q3), 1 - 2 * (q1 * q1 + q2 * q2)))
+    , "pitch":   (("q0", "q1", "q2", "q3"), lambda q0, q1, q2, q3: math.asin(2 * (q0 * q2 - q3 * q1)))
+    , "yaw":     (("q0", "q1", "q2", "q3"), lambda q0, q1, q2, q3: math.atan2(2 * (q0 * q3 + q1 * q2), 1 - 2 * (q2 * q2 + q3 * q3)))
+    , "rgx":     (("gx",), raw_gyro)
+    , "rgy":     (("gy",), raw_gyro)
+    , "rgz":     (("gz",), raw_gyro)
+    , "rgbiasx": (("gbiasx",), raw_gyro)
+    , "rgbiasy": (("gbiasy",), raw_gyro)
+    , "rgbiasz": (("gbiasz",), raw_gyro)
+    })
+
+def columngetter(name):
+    calculator = calculated_columns.get(name)
+    if calculator is not None:
+        getters = columnsgetter(calculator[0])
+        return lambda row: calculator[1](*getters(row))
+
+    idx = columns.index(name)
+    return lambda row: float(row[idx])
+
+def columnsgetter(names):
+    if not names:
+        raise ValueError("must specify at least one column")
+    getters = map(columngetter, names)
+    return lambda row: tuple(getter(row) for getter in getters)
+
+if __name__ == "__main__":
+    selected_columns = sys.argv[1:]
+    try:
+        get_columns = columnsgetter(selected_columns)
+    except ValueError, e:
+        print >>sys.stderr, e.message
+        print >>sys.stderr, "usage: {0} columns...".format(sys.argv[0])
+        print >>sys.stderr, "columns:", ' '.join(columns + calculated_columns.keys())
+        sys.exit(1)
+
+    print '\t'.join(selected_columns)
+    for line in sys.stdin:
+        row = get_columns(line.strip().split())
+        print '\t'.join(map(str, row))
