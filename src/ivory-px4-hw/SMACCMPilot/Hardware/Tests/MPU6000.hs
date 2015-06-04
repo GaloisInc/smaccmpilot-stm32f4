a05aff9383159f82a3012f8f5df8d4a91d1c1bde
Pat Hickey <pat@galois.com>
2015-06-04 15:48:37 -0700

    ivory-px4-hw: support l3gd20 init in mpu6000 test
    need to add it to the other tests that run on the Pixhawk...

--- src/ivory-px4-hw/SMACCMPilot/Hardware/Tests/MPU6000.hs
+++ src/ivory-px4-hw/SMACCMPilot/Hardware/Tests/MPU6000.hs
@@ -15,6 +15,7 @@ import Ivory.Tower.HAL.Bus.Sched
 import Ivory.BSP.STM32.Driver.SPI
 
 import SMACCMPilot.Hardware.MPU6000
+import SMACCMPilot.Hardware.L3GD20
 
 import SMACCMPilot.Hardware.Tests.Platforms
 import SMACCMPilot.Hardware.Tests.Serialize
@@ -25,12 +26,29 @@ app topx4 = do
   a_sample <- channel
   px4platform <- fmap topx4 getEnv
   let mpu6000  = px4platform_mpu6000 px4platform
-  (req, ready) <- spiTower (px4platform_clockconfig . topx4)
-                                [mpu6000_spi_device mpu6000]
-                                (mpu6000_spi_pins mpu6000)
+  (req, ready) <- case px4platform_l3gd20 px4platform of
+    Nothing -> do
+      (spi_req, spi_rdy) <- spiTower (px4platform_clockconfig . topx4)
+                                     [mpu6000_spi_device mpu6000]
+                                     (mpu6000_spi_pins mpu6000)
+      sensors_ready <- px4platform_sensorenable_tower topx4 spi_rdy
+      return (spi_req, sensors_ready)
+    Just l3gd20 -> do
+      (spi_req, spi_rdy) <- spiTower (px4platform_clockconfig . topx4)
+                                     [mpu6000_spi_device mpu6000, l3gd20]
+                                     (mpu6000_spi_pins mpu6000)
+      sensors_ready <- px4platform_sensorenable_tower topx4 spi_rdy
 
-  sensors_ready <- px4platform_sensorenable_tower topx4 ready
-  mpu6000SensorManager req sensors_ready (fst g_sample) (fst a_sample) (SPIDeviceHandle 0)
+      l3gd20_rdy <- channel
+      l3gd20_panic <- channel
+      l3gd20Disabler spi_req
+                     sensors_ready
+                     (fst l3gd20_rdy)
+                     (fst l3gd20_panic)
+                     (SPIDeviceHandle 1)
+      return (spi_req, snd l3gd20_rdy)
+
+  mpu6000SensorManager req ready (fst g_sample) (fst a_sample) (SPIDeviceHandle 0)
 
   (uarto, _uarti) <- px4ConsoleTower topx4
   half_g <- rateDivider 2 (snd g_sample)
