944ae6ab671b57a2b80241d75ece742e433be1fc
Jamey Sharp <jamey@galois.com>
2015-06-09 14:01:51 -0700

    smaccm-ins: Use datasheet-derived noise parameters.

--- src/smaccm-ins/src/SMACCMPilot/INS/Parameters.hs
+++ src/smaccm-ins/src/SMACCMPilot/INS/Parameters.hs
@@ -9,19 +9,19 @@ import SMACCMPilot.INS.SensorFusion
 processNoise :: Fractional a => a -> StateVector a
 processNoise dt = fmap (^ (2 :: Int)) $ fmap (dt *) $ StateVector
     { stateOrient = pure 1.0e-9
-    , stateGyroBias = pure 5.0e-7
+    , stateGyroBias = pure 0
     , stateMagNED = pure 3.0e-4
     , stateMagXYZ = pure 3.0e-4
     }
 
 distCovariance :: Fractional a => DisturbanceVector a
 distCovariance = fmap (^ (2 :: Int)) $ DisturbanceVector
-    { disturbanceGyro = pure 7.762875447020379e-3
-    , disturbanceAccel = pure 0.05
+    { disturbanceGyro = pure 1.394590636121633e-3
+    , disturbanceAccel = pure 3.8031599738985476e-2
     }
 
 magNoise :: Fractional a => XYZ a
 magNoise = pure 1.4826
 
 accelNoise :: Fractional a => XYZ a
-accelNoise = pure 0.02
+accelNoise = disturbanceAccel distCovariance
