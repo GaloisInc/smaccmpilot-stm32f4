a3046f6012d3fde183ad60bc5d2cb54d545bae9f
Pat Hickey <pat@galois.com>
2015-07-16 15:44:18 -0700

    smaccm-flight: add standalone flight application

---
+++ src/smaccm-flight/src/SMACCMPilot/Flight/Standalone.hs
@@ -0,0 +1,41 @@
+
+
+module SMACCMPilot.Flight.Standalone
+  ( app
+  ) where
+
+import           Ivory.Tower
+
+import           SMACCMPilot.Flight.Platform
+import           SMACCMPilot.Flight.Datalink
+import           SMACCMPilot.Flight.IO
+import           SMACCMPilot.Flight.UserInput
+import           SMACCMPilot.Flight.Sensors
+import           SMACCMPilot.Flight.Control
+import           SMACCMPilot.Flight.Motors
+
+import           SMACCMPilot.Comm.Tower.Attr
+import           SMACCMPilot.Comm.Tower.Interface.ControllableVehicle
+
+app :: (e -> FlightPlatform)
+    -> Tower e ()
+app tofp = do
+  (attrs, _streams) <- datalinkTower tofp
+
+  rcin_ui <- channel
+  rcin_cl <- channel
+
+  flightIOTower tofp attrs
+                (fst rcin_ui) (fst rcin_cl)
+                (attrReaderChan (controlLaw attrs))
+                (attrReaderChan (motorOutput attrs))
+
+  userInputTower (snd rcin_ui) (snd rcin_cl) attrs
+
+  sensorTower tofp attrs
+
+  controlTower attrs
+
+  motorMixer (controlOutput attrs)
+             (controlLaw attrs)
+             (motorOutput attrs)
---
+++ src/smaccm-flight/tests/Standalone.hs
@@ -0,0 +1,12 @@
+
+module Main where
+
+import Ivory.Tower.Config
+import Ivory.OS.FreeRTOS.Tower.STM32
+
+import SMACCMPilot.Flight.Platform
+import SMACCMPilot.Flight.Standalone (app)
+
+main :: IO ()
+main = compileTowerSTM32FreeRTOS fp_stm32config p (app id)
+  where p topts = getConfig topts flightPlatformParser
