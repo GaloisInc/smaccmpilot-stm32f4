189e60527c31bbec8dda2799df7dd477831cca35
Pat Hickey <pat@galois.com>
2015-07-25 16:21:18 -0700

    smaccm-flight: add rgbled driver to standalone flight
    all ive used it to do so far is debug. i guess you can set the color via telem
    for fun. maybe one day it'll do something useful

--- src/smaccm-flight/src/SMACCMPilot/Flight/Standalone.hs
+++ src/smaccm-flight/src/SMACCMPilot/Flight/Standalone.hs
@@ -14,6 +14,7 @@ import           SMACCMPilot.Flight.Control
 import           SMACCMPilot.Flight.Motors
 import           SMACCMPilot.Flight.Tuning
 import           SMACCMPilot.Flight.Law
+import           SMACCMPilot.Flight.Light
 
 import           SMACCMPilot.Comm.Tower.Attr
 import           SMACCMPilot.Comm.Tower.Interface.ControllableVehicle
@@ -22,6 +23,8 @@ app :: (e -> FlightPlatform)
     -> Tower e ()
 app tofp = do
   (attrs, _streams) <- datalinkTower tofp 57600
+
+  lightTower tofp attrs
 
   flightTuningTower (fp_tuning . tofp) attrs
 
