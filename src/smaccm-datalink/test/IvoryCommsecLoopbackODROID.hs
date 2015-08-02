616de616680d6b21e8c9bc944ef1acfdf2e30881
leepike <leepike@gmail.com>
2015-08-02 14:53:40 -0700

    odroid: user new UART interface.

--- src/smaccm-datalink/test/IvoryCommsecLoopbackODROID.hs
+++ src/smaccm-datalink/test/IvoryCommsecLoopbackODROID.hs
@@ -2,6 +2,7 @@
 module Main where
 
 import Ivory.Language
+import Ivory.Language.Proxy
 import Ivory.Stdlib
 import Ivory.Tower
 import Tower.AADL
@@ -28,9 +29,13 @@ main = compileTowerAADL fst p (app snd)
 app :: (e -> SymmetricKey)
     -> Tower e ()
 app tosk = do
+  towerModule  stdlibStringModule
+  towerDepends stdlibStringModule
+  _ <- mapM towerArtifact stdlibStringArtifacts
 
   (o, i)    <- uartTower
   valueChan <- channel
+  bytesChan <- channel
 
   monitor "uartTyMon" $ do
     packet <- stateInit "uartPacket" (izero :: Init UartPacket)
@@ -45,6 +50,16 @@ app tosk = do
 
   let o' = o { I.backpressureTransmit = fst valueChan }
 
+  monitor "to_hx" $ do
+    handler i "unpack" $ do
+      let n = fromTypeNat (aNat :: Proxy (Capacity UartPacket))
+      e <- emitter (fst bytesChan) n
+      callback $ \msg -> do
+        len <- msg ~>* stringLengthL
+        let d = msg ~> stringDataL
+        arrayMap $ \ix -> do
+          when (fromIx ix <? len) $ emit e (d!ix)
+
   sk <- fmap tosk getEnv
-  frame_loopback sk o' i
+  frame_loopback sk o' (snd bytesChan)
 
