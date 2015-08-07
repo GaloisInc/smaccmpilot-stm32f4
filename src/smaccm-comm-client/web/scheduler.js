25a614bfbfd860e7bcf33789474367f1be25f328
Jamey Sharp <jamey@galois.com>
2015-08-06 17:52:03 -0700

    smaccm-comm-client: Refactor uitest.js for reuse.

--- src/smaccm-comm-client/web/scheduler.js
+++ src/smaccm-comm-client/web/scheduler.js
@@ -1,71 +1,67 @@
-
-$(function() {
-
-  window.Scheduler = Backbone.Model.extend({
-    defaults: {
-      period: false
-    },
-    initialize: function (attrs, models) {
-      this.models = [].concat(models); // creates list from single object or list of objects
+window.Scheduler = Backbone.Model.extend({
+  defaults: {
+    period: false
+  },
+  initialize: function (attrs, models) {
+    this.models = [].concat(models); // creates list from single object or list of objects
+    this.interval = null;
+    this.on('change', this.update, this);
+    this.trigger('change', this);
+  },
+  update: function () {
+    var attrs = this.toJSON();
+    var self = this;
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+    if (attrs.period > 0) {
+      this.interval = setInterval(function () {
+        // Build up a chain of callbacks to fetch each model after the
+        // previous one completes, because we don't seem to reliably
+        // get replies when we have multiple requests pending.
+        var chain = self.models.reduceRight(function (k, m) {
+          return function () {
+            m.fetch().done(k);
+          };
+        }, z);
+        // The end of the chain does nothing, but we could invoke
+        // setTimeout here if we didn't want the setInterval policy.
+        function z() {}
+        // Start the callback chain running.
+        chain();
+      }, attrs.period);
+    } else {
       this.interval = null;
-      this.on('change', this.update, this);
-      this.trigger('change', this);
-    },
-    update: function () {
-      var attrs = this.toJSON();
-      var self = this;
-      if (this.interval) {
-        clearInterval(this.interval);
-      }
-      if (attrs.period > 0) {
-        this.interval = setInterval(function () {
-          // Build up a chain of callbacks to fetch each model after the
-          // previous one completes, because we don't seem to reliably
-          // get replies when we have multiple requests pending.
-          var chain = self.models.reduceRight(function (k, m) {
-            return function () {
-              m.fetch().done(k);
-            };
-          }, z);
-          // The end of the chain does nothing, but we could invoke
-          // setTimeout here if we didn't want the setInterval policy.
-          function z() {}
-          // Start the callback chain running.
-          chain();
-        }, attrs.period);
-      } else {
-        this.interval = null;
-      }
     }
-  });
+  }
+});
 
-  window.SchedulerButtonView = Backbone.View.extend({
-    events: {
-      'click' : 'click'
-    },
-    initialize: function () {
-      this.model.on('change', this.render, this);
-      this.render();
-    },
-    render: function () {
-      var p = this.model.get('period');
-      if (p > 0) {
-        this.$el.removeClass('btn-warning btn-success')
-                .addClass('btn-success')
-                .html('Polling');
-      } else {
-        this.$el.removeClass('btn-warning btn-success')
-                .addClass('btn-warning')
-                .html('Begin Polling');
-      }
-    },
-    click: function () {
-      var p = this.model.get('period');
-      if (p > 0) {
-        this.model.set('period', false);
-      } else {
-        this.model.set('period', 200);
-      }
+window.SchedulerButtonView = Backbone.View.extend({
+  events: {
+    'click' : 'click'
+  },
+  initialize: function () {
+    this.model.on('change', this.render, this);
+    this.render();
+  },
+  render: function () {
+    var p = this.model.get('period');
+    if (p > 0) {
+      this.$el.removeClass('btn-warning btn-success')
+              .addClass('btn-success')
+              .html('Polling');
+    } else {
+      this.$el.removeClass('btn-warning btn-success')
+              .addClass('btn-warning')
+              .html('Begin Polling');
     }
-  });
+  },
+  click: function () {
+    var p = this.model.get('period');
+    if (p > 0) {
+      this.model.set('period', false);
+    } else {
+      this.model.set('period', 200);
+    }
+  }
 });
--- src/smaccm-comm-client/web/uitest.js
+++ src/smaccm-comm-client/web/uitest.js
@@ -1,183 +1,143 @@
-
-
-$(function() {
-
-  var ControlLaw = Backbone.Model.extend({
-    urlRoot: '/controllable_vehicle_i/control_law',
-    defaults: {
-      thr_mode: 'Direct',
-      arming_mode: 'Safe',
-      ui_mode: 'Ppm',
-      yaw_mode: 'Rate'
-    },
-    valid : {
-      thr_mode: [ 'Direct', 'Auto' ],
-      arming_mode: [ 'Safe', 'Disarmed', 'Armed' ],
-      ui_mode: [ 'Ppm', 'Gcs', 'Nav' ],
-      yaw_mode: [ 'Rate', 'Heading' ]
-    }
-  });
-  var ControlLawRequest = ControlLaw.extend({
-    urlRoot: '/controllable_vehicle_i/control_law_request',
-  });
-
-  var ControlLawView = Backbone.View.extend({
-    initialize: function () {
-      this.$el.html(''
-        + '<p>Arming Mode: '
-        + this.button_group('arming_mode', this.model.valid.arming_mode)
-        + '</p>'
-        + '<p>Throttle Mode: '
-        + this.button_group('thr_mode', this.model.valid.thr_mode)
-        + '</p>'
-        + '<p>UI Mode: '
-        + this.button_group('ui_mode', this.model.valid.ui_mode)
-        + '</p>'
-        + '<p>Yaw Mode: '
-        + this.button_group('yaw_mode', this.model.valid.yaw_mode)
-        + '</p>'
-        );
-      this.model.on('change', this.render, this);
-      this.render();
-      if (this.post_init) {
-        this.post_init();
-      }
-    },
-    render: function () {
-      var m = this.model.toJSON();
-      var valid = this.model.valid;
-      var self = this;
-      _.map(valid, function (vs,k) {
-        _.map(vs, function (v) {
-          if (m[k] == v) {
-            self.$('#btn-' + k + '-' + v)
-                .removeClass('btn-default')
-                .addClass('btn-success');
-          } else {
-            self.$('#btn-' + k + '-' + v)
-                .removeClass('btn-success')
-                .addClass('btn-default');
-          }
-        });
-      });
-    },
-    button_group: function (group, labels) {
-      var contents = _.map(labels, function(lbl) {
-        return ('<button type="button" class="btn btn-default" id="btn-'
-              + group + '-' + lbl +'">'
-              + lbl + '</button>');
-      });
-      return ('<div class="btn-group btn-group-sm" role="group">' 
-        + _.foldl(contents, function (a, b) { return a+b })
-        + "</div>");
-    }
-  });
-
-
-  var ControlLawRequestView = ControlLawView.extend({
-    post_init: function () {
-      var m = this.model.toJSON();
-      var valid = this.model.valid;
-      var self = this;
-      _.map(valid, function (vs,k) {
-        _.map(vs, function (v) {
-            self.$('#btn-' + k + '-' + v)
-                .click(function () {
-                  self.model.set(k,v);
-                  self.model.save();
-                });
-        });
-      });
-    }
-  });
-
-  var UserInput = Backbone.Model.extend({
-    urlRoot: '/controllable_vehicle_i/user_input',
-    defaults: {
-      ui: { throttle: 0, roll: 0, pitch: 0, yaw: 0 },
-      source: 'Ppm',
-      time: { unTimeMicros: 0 }
-    }
-  });
-
-  var TextView = Backbone.View.extend({
-    initialize: function (options) {
-      this.accessor = options.accessor;
-      this.model.on('change', this.render, this);
-      this.render();
-    },
-    render: function () {
-      var val = this.accessor(this.model.toJSON())
-      this.$el.html(val);
-    }
-  });
-
-  var UserInputSliderView = Backbone.View.extend({
-    initialize: function (options) {
-      this.axis = options.axis;
-      this.model.on('change', this.render, this);
-      this.$progbar = $('#ui-' + this.axis + '-progbar');
-      this.$label   = $('#ui-' + this.axis + '-progbar-lbl');
-      this.render();
-    },
-    render: function () {
-      var val = this.model.toJSON()['ui'][this.axis];
-      this.$label.html(val.toFixed(2));
-      var percent = (val + 1) * 50;
-      this.$progbar.css('width', percent.toString() + '%');
-    }
-  });
-
-  var UserInputView = function (opts) {
-    this.throttle = new UserInputSliderView({ model: opts.model, axis: 'throttle' });
-    this.roll     = new UserInputSliderView({ model: opts.model, axis: 'roll' });
-    this.pitch    = new UserInputSliderView({ model: opts.model, axis: 'pitch' });
-    this.yaw      = new UserInputSliderView({ model: opts.model, axis: 'yaw'  });
-    this.source   = new TextView({
-      el: '#ui-source',
-      model: opts.model,
-      accessor: function (json) { return json['source'].toString() }
-    });
-    this.time     = new TextView({
-      el: '#ui-time',
-      model: opts.model,
-      accessor: function (json) {
-        return (json['time']['unTimeMicros'] / 1000000).toFixed(1) + ' sec';
-      }
-    });
-  };
-
-  window.userInput =
-    new UserInput({});
-  window.userInputView =
-    new UserInputView({ model: userInput });
-
-  window.userInputScheduler =
-    new Scheduler({ period: false}, userInput);
-  window.userInputSchedulerView =
-    new SchedulerButtonView({ model: userInputScheduler, el: '#ui-sch-btn' });
-
-  window.controlLaw = new ControlLaw({});
-  window.controlLawView = new ControlLawView({
-    model: controlLaw,
-    el: '#control-law-view'
-  });
-  window.controlLawScheduler =
-    new Scheduler({ period: false}, controlLaw);
-  window.controlLawSchedulerView =
-    new SchedulerButtonView({ model: controlLawScheduler, el: '#cl-sch-btn' });
-
-  window.controlLawRequest = new ControlLawRequest({});
-  window.controlLawRequestView = new ControlLawRequestView({
-    model: controlLawRequest,
-    el: '#control-law-request-view'
-  });
-  window.controlLawRequestScheduler =
-    new Scheduler({ period: false}, controlLawRequest);
-  window.controlLawRequestSchedulerView =
-    new SchedulerButtonView({ model: controlLawRequestScheduler, el: '#clr-sch-btn' });
+window.ControlLaw = Backbone.Model.extend({
+  urlRoot: '/controllable_vehicle_i/control_law',
+  defaults: {
+    thr_mode: 'Direct',
+    arming_mode: 'Safe',
+    ui_mode: 'Ppm',
+    yaw_mode: 'Rate'
+  },
+  valid : {
+    thr_mode: [ 'Direct', 'Auto' ],
+    arming_mode: [ 'Safe', 'Disarmed', 'Armed' ],
+    ui_mode: [ 'Ppm', 'Gcs', 'Nav' ],
+    yaw_mode: [ 'Rate', 'Heading' ]
+  }
+});
+window.ControlLawRequest = ControlLaw.extend({
+  urlRoot: '/controllable_vehicle_i/control_law_request',
 });
 
+window.ControlLawView = Backbone.View.extend({
+  initialize: function () {
+    this.$el.html(''
+      + '<p>Arming Mode: '
+      + this.button_group('arming_mode', this.model.valid.arming_mode)
+      + '</p>'
+      + '<p>Throttle Mode: '
+      + this.button_group('thr_mode', this.model.valid.thr_mode)
+      + '</p>'
+      + '<p>UI Mode: '
+      + this.button_group('ui_mode', this.model.valid.ui_mode)
+      + '</p>'
+      + '<p>Yaw Mode: '
+      + this.button_group('yaw_mode', this.model.valid.yaw_mode)
+      + '</p>'
+      );
+    this.model.on('change', this.render, this);
+    this.render();
+    if (this.post_init) {
+      this.post_init();
+    }
+  },
+  render: function () {
+    var m = this.model.toJSON();
+    var valid = this.model.valid;
+    var self = this;
+    _.map(valid, function (vs,k) {
+      _.map(vs, function (v) {
+        if (m[k] == v) {
+          self.$('#btn-' + k + '-' + v)
+              .removeClass('btn-default')
+              .addClass('btn-success');
+        } else {
+          self.$('#btn-' + k + '-' + v)
+              .removeClass('btn-success')
+              .addClass('btn-default');
+        }
+      });
+    });
+  },
+  button_group: function (group, labels) {
+    var contents = _.map(labels, function(lbl) {
+      return ('<button type="button" class="btn btn-default" id="btn-'
+            + group + '-' + lbl +'">'
+            + lbl + '</button>');
+    });
+    return ('<div class="btn-group btn-group-sm" role="group">' 
+      + _.foldl(contents, function (a, b) { return a+b })
+      + "</div>");
+  }
+});
 
+window.ControlLawRequestView = ControlLawView.extend({
+  post_init: function () {
+    var m = this.model.toJSON();
+    var valid = this.model.valid;
+    var self = this;
+    _.map(valid, function (vs,k) {
+      _.map(vs, function (v) {
+          self.$('#btn-' + k + '-' + v)
+              .click(function () {
+                self.model.set(k,v);
+                self.model.save();
+              });
+      });
+    });
+  }
+});
 
+window.UserInput = Backbone.Model.extend({
+  urlRoot: '/controllable_vehicle_i/user_input',
+  defaults: {
+    ui: { throttle: 0, roll: 0, pitch: 0, yaw: 0 },
+    source: 'Ppm',
+    time: { unTimeMicros: 0 }
+  }
+});
 
+window.TextView = Backbone.View.extend({
+  initialize: function (options) {
+    this.accessor = options.accessor;
+    this.model.on('change', this.render, this);
+    this.render();
+  },
+  render: function () {
+    var val = this.accessor(this.model.toJSON())
+    this.$el.html(val);
+  }
+});
+
+window.UserInputSliderView = Backbone.View.extend({
+  initialize: function (options) {
+    this.axis = options.axis;
+    this.model.on('change', this.render, this);
+    this.$progbar = $('#ui-' + this.axis + '-progbar');
+    this.$label   = $('#ui-' + this.axis + '-progbar-lbl');
+    this.render();
+  },
+  render: function () {
+    var val = this.model.toJSON()['ui'][this.axis];
+    this.$label.html(val.toFixed(2));
+    var percent = (val + 1) * 50;
+    this.$progbar.css('width', percent.toString() + '%');
+  }
+});
+
+window.UserInputView = function (opts) {
+  this.throttle = new UserInputSliderView({ model: opts.model, axis: 'throttle' });
+  this.roll     = new UserInputSliderView({ model: opts.model, axis: 'roll' });
+  this.pitch    = new UserInputSliderView({ model: opts.model, axis: 'pitch' });
+  this.yaw      = new UserInputSliderView({ model: opts.model, axis: 'yaw'  });
+  this.source   = new TextView({
+    el: '#ui-source',
+    model: opts.model,
+    accessor: function (json) { return json['source'].toString() }
+  });
+  this.time     = new TextView({
+    el: '#ui-time',
+    model: opts.model,
+    accessor: function (json) {
+      return (json['time']['unTimeMicros'] / 1000000).toFixed(1) + ' sec';
+    }
+  });
+};
