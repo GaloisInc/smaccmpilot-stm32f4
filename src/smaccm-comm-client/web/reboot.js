d14ca1c8507194727f37f9510a9197f19d20cac1
Jamey Sharp <jamey@galois.com>
2015-09-02 19:18:41 -0700

    Add a GIDL message and HUD button to reboot the Linux VM.
    This isn't wired up in the Odroid yet, and the GCS HTTP server is
    rejecting the button clicks with "400 Bad Request"--but from everything
    I can check, this code should be correct. I'm committing it so Lee can
    work on the Odroid side while I continue debugging the GCS side.

---
+++ src/smaccm-comm-client/web/reboot.js
@@ -0,0 +1,15 @@
+window.RebootButtonView = Backbone.View.extend({
+  tagName: 'button',
+  attributes: { type: 'button' },
+  className: 'btn btn-default',
+  events: {
+    'click' : 'click'
+  },
+  initialize: function (options) {
+    this.$el.text('Reboot');
+  },
+  click: function () {
+    this.model.set('magic', 'LinuxRebootMagic1');
+    this.model.save();
+  }
+});
