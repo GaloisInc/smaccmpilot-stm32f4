e3c974030b4195d80b6ca8b402f71e331e464ff7
Jason Dagit <dagitj@gmail.com>
2016-02-17 10:40:19 -0800

    smaccm-flight: add echronos/aadl tests

---
+++ src/smaccm-flight/tests/CANServerAADL.hs
@@ -0,0 +1,24 @@
+module Main where
+
+import Ivory.Tower.Config
+import Ivory.Tower.Options
+import Ivory.OS.FreeRTOS.Tower.STM32
+
+import Tower.AADL
+import Tower.AADL.Build.Common
+import Tower.AADL.Build.EChronos
+
+import SMACCMPilot.Flight.Platform
+import SMACCMPilot.Flight.Datalink.CAN.TestServer (app)
+
+main :: IO ()
+main = compileTowerAADLForPlatform f p (app id)
+  where
+  f :: FlightPlatform -> (AADLConfig, OSSpecific STM32Config e)
+  f config = ( defaultAADLConfig { configSystemOS = EChronos
+                                 , configSystemHW = PIXHAWK
+                                 }
+             , defaultEChronosOS (fp_stm32config config)
+             )
+  p :: TOpts -> IO FlightPlatform
+  p topts = fmap fst (getConfig' topts flightPlatformParser)
