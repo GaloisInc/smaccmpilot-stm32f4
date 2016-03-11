8fd5f6915db7c84885418917a4fd38a518a9564b
Jason Dagit <dagitj@gmail.com>
2016-03-10 17:01:43 -0800

    smaccm-flight: add RGB LED test for aadl and sensors aadl test

---
+++ src/smaccm-flight/tests/SensorsAADL.hs
@@ -0,0 +1,25 @@
+
+module Main where
+
+import Ivory.Tower.Config
+import Ivory.Tower.Options
+import Ivory.OS.FreeRTOS.Tower.STM32
+
+import SMACCMPilot.Flight.Platform
+import SMACCMPilot.Flight.Sensors.TestApp (app)
+
+import Tower.AADL
+import Tower.AADL.Build.Common
+import Tower.AADL.Build.EChronos
+
+main :: IO ()
+main = compileTowerAADLForPlatform f p (app id)
+  where
+  f :: FlightPlatform -> (AADLConfig, OSSpecific STM32Config e)
+  f config = ( defaultAADLConfig { configSystemOS = EChronos
+                                 , configSystemHW = PIXHAWK
+                                 }
+             , defaultEChronosOS (fp_stm32config config)
+             )
+  p :: TOpts -> IO FlightPlatform
+  p topts = fmap fst (getConfig' topts flightPlatformParser)
