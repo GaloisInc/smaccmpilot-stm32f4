9d29c56c6e8c99404a643fae8e403267683b0b3d
Adam C. Foltzer <acfoltzer@galois.com>
2016-10-10 19:09:28 -0700

    origin in center for artificial horizon

--- src/smaccm-comm-client/elm/PFD.elm
+++ src/smaccm-comm-client/elm/PFD.elm
@@ -14,26 +14,26 @@ pfd pitchRads rollRads =
 artificialHorizon pitchRads rollRads =
   let skyColor = "#72cde4"
       groundColor = "#c0a020"
-      ground = Svg.path [ d "M-100,50 L-100,200 L200,200 L200,50 Z", fill groundColor ] []
-      sky = Svg.path [ d "M-100,50 L-100,-100 L200,-100 L200,50 Z", fill skyColor ] []
-      horizon = line [ x1 "-100", y1 "50", x2 "200", y2 "50", strokeWidth "2.5", stroke "#343434" ] []
+      ground = Svg.path [ d "M-150,0 L-150,150 L150,150 L150,0 Z", fill groundColor ] []
+      sky = Svg.path [ d "M-150,0 L-150,-150 L150,-150 L150,0 Z", fill skyColor ] []
+      horizon = line [ x1 "-150", y1 "0", x2 "150", y2 "0", strokeWidth "2.5", stroke "#343434" ] []
       ladder h w =
-        let translate = "translate(" ++ toString (50 - (w / 2)) ++ " " ++ toString (50 - h) ++ ")"
+        let translate = "translate(" ++ toString -(w / 2) ++ " " ++ toString -h ++ ")"
         in line [ x1 "0", y1 "0", x2 (toString w), y2 "0"
                 , transform translate, stroke "#202020" ] []
-      rungs = [ ladder 7 12, ladder 15 17, ladder 22 12, ladder 30 17
-              , ladder -7 12, ladder -15 17, ladder -22 12, ladder -30 17
+      rungs = [ ladder 7 12, ladder 14 17, ladder 21 12, ladder 28 17
+              , ladder -7 12, ladder -14 17, ladder -21 12, ladder -28 17
               ]
       movingGroup =
         let translate = "translate(0 " ++ toString (pitchRads * (180 / pi) / 2) ++ ")"
-            rotate    = "rotate(" ++ toString (rollRads * (180 / pi)) ++ " 50 50)"
+            rotate    = "rotate(" ++ toString (rollRads * (180 / pi)) ++ ")"
         in g [ transform (translate ++ " " ++ rotate) ]
              (List.append [ ground, sky, horizon ] rungs)
       indicator_left = Svg.path [
-          d "M25,50 L37,50 L43,55 M25,50 Z"
+          d "M-25,0 L-13,0 L-7,5 M-25,0 Z"
         , strokeWidth "2.5", stroke "black", fill "none" ] []
       indicator_right = Svg.path [
-          d "M75,50 L63,50 L57,55 M75,50 Z"
+          d "M25,0 L13,0 L7,5 M25,0 Z"
         , strokeWidth "2.5", stroke "black", fill "none" ] []
         
-  in g [] [ movingGroup, indicator_left, indicator_right ]
+  in g [ transform "translate(50 50)" ] [ movingGroup, indicator_left, indicator_right ]
