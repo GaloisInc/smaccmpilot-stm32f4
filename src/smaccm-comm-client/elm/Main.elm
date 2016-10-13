0bd3ca460759edf4144f19ab4546911360824404
Adam C. Foltzer <acfoltzer@galois.com>
2016-10-12 17:09:58 -0700

    switch to using gidl-generated Elm

--- src/smaccm-comm-client/elm/Main.elm
+++ src/smaccm-comm-client/elm/Main.elm
@@ -7,11 +7,17 @@ import Html.App as App
 import Html.Shorthand exposing (..)
 import Html.Attributes exposing (..)
 import Http
-import Json.Decode as Json
-import Json.Decode exposing ((:=))
 import Task
 import Time exposing (Time, millisecond)
 import PFD exposing (..)
+
+import SMACCMPilot.Comm.Types.ArmingMode as ArmingMode
+import SMACCMPilot.Comm.Types.ControlSource as ControlSource
+import SMACCMPilot.Comm.Types.GpsFix as GpsFix
+import SMACCMPilot.Comm.Types.PackedStatus as PackedStatus
+import SMACCMPilot.Comm.Types.ThrottleMode as ThrottleMode
+import SMACCMPilot.Comm.Types.Tristate as Tristate
+import SMACCMPilot.Comm.Types.YawMode as YawMode
 
 main =
   App.program
@@ -21,87 +27,42 @@ main =
     , subscriptions = subscriptions
     }
 
-
-
 -- MODEL
 
 
 type alias Model =
-  { packedStatus : PackedStatus
+  { packedStatus : PackedStatus.PackedStatus
   , lastUpdate : Time
   , lastUpdateDt : Float -- milliseconds
   , httpError : Maybe Http.Error
   }
 
-type alias PackedStatus =
-  { valid : Bool
-  , roll : Float
-  , pitch : Float
-  , yaw : Float
-  , baro_alt : Float
-  , fix : GpsFix
-  , num_sv : Int
-  , lat : Int
-  , lon : Int
-  , alt : Int
-  , vground : Int
-  , heading : Float
-  , rcinput : Tristate
-  , telem : Tristate
-  , px4io : Tristate
-  , sens_cal : Tristate
-  , gyro_progress : Float
-  , mag_progress : Float
-  , accel_progress : Float
-  , arming_mode : ArmingMode
-  , control_modes : ControlModes
-  , battery_voltage : Float
-  }
-
-type GpsFix = FixNone | Fix2d | Fix3d
-
-type Tristate = Negative | Neutral | Positive
-
-type alias ControlModes =
-  { ui_mode : ControlSource
-  , yaw_mode : YawMode
-  , thr_mode : ThrottleMode
-  }
-
-type ArmingMode = Safe | Armed
-
-type ControlSource = Ppm | Gcs | Nav
-
-type YawMode = Rate | Heading
-
-type ThrottleMode = DirectUi | AltUi | AltSetpt
-
-initPackedStatus : PackedStatus
+initPackedStatus : PackedStatus.PackedStatus
 initPackedStatus = {
     valid = False
   , roll = 0
   , pitch = 0
   , yaw = 0
   , baro_alt = 0
-  , fix = FixNone
+  , fix = GpsFix.FixNone
   , num_sv = 0
   , lat = 0
   , lon = 0
   , alt = 0
   , vground = 0
   , heading = 0
-  , rcinput = Negative
-  , telem = Negative
-  , px4io = Negative
-  , sens_cal = Negative
+  , rcinput = Tristate.Negative
+  , telem = Tristate.Negative
+  , px4io = Tristate.Negative
+  , sens_cal = Tristate.Negative
   , gyro_progress = 0
   , mag_progress = 0
   , accel_progress = 0
-  , arming_mode = Safe
+  , arming_mode = ArmingMode.Safe
   , control_modes = {
-        ui_mode = Ppm
-      , yaw_mode = Rate
-      , thr_mode = DirectUi
+        ui_mode = ControlSource.Ppm
+      , yaw_mode = YawMode.Rate
+      , thr_mode = ThrottleMode.DirectUi
       }
   , battery_voltage = 0
   }
@@ -112,15 +73,13 @@ init =
   , fetchPackedStatus
   )
 
-
-
 -- UPDATE
 
 
 type Msg
   = Poll
   | UpdateTime Time
-  | FetchPackedStatusSucceed PackedStatus
+  | FetchPackedStatusSucceed PackedStatus.PackedStatus
   | FetchFail Http.Error
 
 update : Msg -> Model -> (Model, Cmd Msg)
@@ -231,91 +190,4 @@ fetchPackedStatus =
   in Task.perform
        FetchFail
        FetchPackedStatusSucceed
-       (Http.get decodePackedStatus url)
-
-decodePackedStatus : Json.Decoder PackedStatus
-decodePackedStatus = PackedStatus
-  `Json.map` ("valid" := Json.bool)
-  `thenMap`  ("roll"  := Json.float)
-  `thenMap`  ("pitch" := Json.float)
-  `thenMap`  ("yaw"   := Json.float)
-  `thenMap`  ("baro_alt" := Json.float)
-  `thenMap`  ("fix" := decodeGpsFix)
-  `thenMap`  ("num_sv" := Json.int)
-  `thenMap`  ("lat" := Json.int)
-  `thenMap`  ("lon" := Json.int)
-  `thenMap`  ("alt" := Json.int)
-  `thenMap`  ("vground" := Json.int)
-  `thenMap`  ("heading" := Json.float)
-  `thenMap`  ("rcinput" := decodeTristate)
-  `thenMap`  ("telem" := decodeTristate)
-  `thenMap`  ("px4io" := decodeTristate)
-  `thenMap`  ("sens_cal" := decodeTristate)
-  `thenMap`  ("gyro_progress" := Json.float)
-  `thenMap`  ("mag_progress" := Json.float)
-  `thenMap`  ("accel_progress" := Json.float)
-  `thenMap`  ("arming_mode" := decodeArmingMode)
-  `thenMap`  ("control_modes" := decodeControlModes)
-  `thenMap`  ("battery_voltage" := Json.float)
-
-decodeGpsFix : Json.Decoder GpsFix
-decodeGpsFix =
-  Json.customDecoder Json.string (\tag ->
-    case tag of
-      "FixNone" -> Ok FixNone
-      "Fix2d" -> Ok Fix2d
-      "Fix3d" -> Ok Fix3d
-      str -> Err ("unrecognized GpsFix tag: " ++ str))
-
-decodeTristate : Json.Decoder Tristate
-decodeTristate =
-  Json.customDecoder Json.string (\tag ->
-    case tag of
-      "Negative" -> Ok Negative
-      "Neutral" -> Ok Neutral
-      "Positive" -> Ok Positive
-      str -> Err ("unrecognized Tristate tag: " ++ str))
-
-decodeArmingMode : Json.Decoder ArmingMode
-decodeArmingMode =
-  Json.customDecoder Json.string (\tag ->
-    case tag of
-      "Safe" -> Ok Safe
-      "Armed" -> Ok Armed
-      str -> Err ("unrecognized ArmingMode tag: " ++ str))
-
-decodeControlModes : Json.Decoder ControlModes
-decodeControlModes = ControlModes
-  `Json.map` ("ui_mode" := decodeControlSource)
-  `thenMap` ("yaw_mode" := decodeYawMode)
-  `thenMap` ("thr_mode" := decodeThrottleMode)
-
-decodeControlSource : Json.Decoder ControlSource
-decodeControlSource =
-  Json.customDecoder Json.string (\tag ->
-    case tag of
-      "Ppm" -> Ok Ppm
-      "Gcs" -> Ok Gcs
-      "Nav" -> Ok Nav
-      str -> Err ("unrecognized ControlSource tag: " ++ str))
-
-decodeYawMode : Json.Decoder YawMode
-decodeYawMode =
-  Json.customDecoder Json.string (\tag ->
-    case tag of
-      "Rate" -> Ok Rate
-      "Heading" -> Ok Heading
-      str -> Err ("unrecognized YawMode tag: " ++ str))
-
-decodeThrottleMode : Json.Decoder ThrottleMode
-decodeThrottleMode =
-  Json.customDecoder Json.string (\tag ->
-    case tag of
-      "DirectUi" -> Ok DirectUi
-      "AltUi" -> Ok AltUi
-      "AltSetpt" -> Ok AltSetpt
-      str -> Err ("unrecognized ThrottleMode tag: " ++ str))
-
-thenMap : Json.Decoder (a -> b) -> Json.Decoder a -> Json.Decoder b
-thenMap f aDecoder =
-  f `Json.andThen` (\f' -> f' `Json.map` aDecoder)
+       (Http.get PackedStatus.decode url)
--- stack.mk
+++ stack.mk
@@ -5,11 +5,12 @@ export PATH := $(shell stack path --stack-yaml=$(ROOT_DIR)/gidl-bootstrap.yaml -
 COMM_SCHEMA        := $(ROOT_DIR)/src/smaccm-comm-schema
 COMM_SCHEMA_NATIVE := $(COMM_SCHEMA)/smaccm-comm-schema-native
 COMM_SCHEMA_TOWER  := $(COMM_SCHEMA)/smaccm-comm-schema-tower
+COMM_SCHEMA_ELM    := $(COMM_SCHEMA)/smaccm-comm-schema-elm
 
-default: $(COMM_SCHEMA_NATIVE) $(COMM_SCHEMA_TOWER)
+default: $(COMM_SCHEMA_NATIVE) $(COMM_SCHEMA_TOWER) $(COMM_SCHEMA_ELM)
 	stack build
 
-$(COMM_SCHEMA_NATIVE) $(COMM_SCHEMA_TOWER):
+$(COMM_SCHEMA_NATIVE) $(COMM_SCHEMA_TOWER) $(COMM_SCHEMA_ELM): $(COMM_SCHEMA)/smaccm-comm-schema.gidl
 	make -C $(ROOT_DIR) gidl-bootstrap
 	make -C $(COMM_SCHEMA) smaccm-comm-schema-native smaccm-comm-schema-tower
 
