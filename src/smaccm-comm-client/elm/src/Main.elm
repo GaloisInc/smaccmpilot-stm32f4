6633a8f6e8fc58b9b9985214403274732f319921
Adam C. Foltzer <acfoltzer@galois.com>
2016-11-11 17:39:34 -0800

    add smoothing and UI for controlling polling

--- src/smaccm-comm-client/elm/src/Main.elm
+++ src/smaccm-comm-client/elm/src/Main.elm
@@ -9,7 +9,7 @@ import Html.Shorthand exposing (..)
 import Html.Attributes exposing (..)
 import Http
 import Task
-import Time exposing (Time, millisecond)
+import Time exposing (Time, millisecond, second)
 import PFD exposing (..)
 
 import SMACCMPilot.Comm.Interface.ControllableVehicle as CV
@@ -35,8 +35,11 @@ main =
 
 type alias Model =
   { packedStatus : PackedStatus
+  , refreshRate : Float -- milliseconds
+  , baroSmoother : Smoother
+  , latencySmoother : Smoother
   , lastUpdate : Time
-  , lastUpdateDt : Float -- milliseconds
+  , lastUpdateDts : List Float -- milliseconds
   , httpError : Maybe Http.Error
   }
 
@@ -45,14 +48,46 @@ cvc = CV.client FetchFail CVResponse "http://localhost:8080"
 
 init : (Model, Cmd Msg)
 init =
-  ( Model PackedStatus.init 0 0 Nothing
-  , cvc.getPackedStatus
-  )
+  let baroWeights = [
+          -0.004079714666025863
+        , -0.011002950421607123
+        , -0.004853702172814258
+        , 0.03994619789045572
+        , 0.12899291808975988
+        , 0.22464354904548067
+        , 0.26647104342536615
+        , 0.22464354904548067
+        , 0.12899291808975988
+        , 0.03994619789045572
+        , -0.004853702172814258
+        , -0.011002950421607123
+        , -0.004079714666025863
+        ]
+      latencyWeights = [
+          0.11538864317459548
+        , 0.36510250931275834
+        , 0.5000373407321809
+        , 0.36510250931275834
+        , 0.11538864317459548
+        , 0.0006633631777757225
+        ]
+  in ( { packedStatus = PackedStatus.init
+       , refreshRate = 66
+       , baroSmoother = mkSmoother baroWeights
+       , latencySmoother = mkSmoother latencyWeights
+       , lastUpdate = 0
+       , lastUpdateDts = []
+       , httpError = Nothing
+       }
+     , cvc.getPackedStatus
+     )
 
 -- UPDATE
 
 type Msg
   = Poll
+  | SetRefreshRate Float -- hz
+  | UpdateLatency Time
   | UpdateTime Time
   | CVResponse CV.Response
   | SendReboot
@@ -72,32 +107,48 @@ update msg model =
   case msg of
     Poll ->
       (model, cvc.getPackedStatus)
+    SetRefreshRate hz ->
+      { model | refreshRate = 1*second / (hz + 0.0001) } ! []
+    UpdateLatency time ->
+      let -- use the current dt if we haven't seen a sample since the last update
+          dt = Time.inMilliseconds time - Time.inMilliseconds model.lastUpdate
+          window = model.latencySmoother.firState.size
+          s' =
+            case model.lastUpdateDts of
+              [] -> putSome model.latencySmoother (List.repeat window dt)
+              ls -> putSome model.latencySmoother ls
+      in { model | latencySmoother = s', lastUpdateDts = [] } ! []
     UpdateTime time ->
       let dt = Time.inMilliseconds time - Time.inMilliseconds model.lastUpdate
-      in {model | lastUpdate = time, lastUpdateDt = dt} ! []
+      in { model | lastUpdate = time, lastUpdateDts = dt :: model.lastUpdateDts } ! []
     CVResponse resp ->
       let (model', cmd) = CV.handle cvh resp model
-      in {model' | httpError = Nothing} ! [cmd, Task.perform never UpdateTime Time.now]
+          model'' = updateSmoothers model'
+      in { model'' | httpError = Nothing } ! [ cmd, Task.perform never UpdateTime Time.now ]
     SendReboot -> (model, cvc.setRebootReq (RebootReq.RebootReq RebootMagic.LinuxRebootMagic1))
     FetchFail err ->
-      {model | httpError = Just err} ! []
+      { model | httpError = Just err } ! []
+
+updateSmoothers : Model -> Model
+updateSmoothers model =
+  { model | baroSmoother = putNext model.baroSmoother model.packedStatus.baro_alt }
 
 -- VIEW
 
 view : Model -> Html Msg
 view model =
-  container_ [
+  container_ [ panelDefault_ [ panelHeading_ [ panelTitle_ "SMACCMPilot" ], panelBody_ [
       row_ [
         colXs_ 4 [ pfd
                      model.packedStatus.pitch
                      model.packedStatus.roll
-                     model.packedStatus.baro_alt
+                     model.baroSmoother.value
                      model.packedStatus.yaw ]
       , colXs_ 8 [
           panelDefault_ [
             ul [ class "nav nav-tabs", attribute "role" "tablist" ] [
                 li' { class = "active" } [ a [ href "#calibration", attribute "data-toggle" "tab" ] [ strong_ "Calibration" ] ]
-              , li_ [ a [href "#status", attribute "data-toggle" "tab" ] [ strong_ "Status"] ]
+              , li_ [ a [href "#status", attribute "data-toggle" "tab" ] [ strong_ "Control Law Status"] ]
               , li_ [ a [href "#tuning", attribute "data-toggle" "tab" ] [ strong_ "Tuning" ] ]
               ]
           , panelBody_ [
@@ -109,14 +160,49 @@ view model =
             ]
           ]
         ]
-    , row_ [ colXs_ 4 [ node "button" [ class "btn btn-primary btn-lg btn-block", onClick SendReboot ] [ text "Reboot" ] ] ]
-    , row_ [ colXs_ 12 [
-        case model.httpError of
-          Just err -> div [ class "alert alert-warning" ] [ text (toString err) ]
-          Nothing -> div [] []
-      ] ]
     ]
-  ]
+    , hr_
+    , row_ [ colXs_ 12 [ table' { class = "table" } [ tbody_ [
+           tr
+             [ let v = model.latencySmoother.value
+               in class (if v < 1000
+                         then "success"
+                         else if v >= 1000 && v < 5000
+                         then "warning"
+                         else "danger") ]
+             [ thLabel 80 "Time between updates", td_ [ text (toString (round model.latencySmoother.value) ++ "ms") ] ]
+         , tr_ [ thLabel 80 "Refresh rate"
+               , td_ [ div' { class = "input-group" } [
+                   inputFloat' {
+                     class = "form-control"
+                   , name = "refresh-rate"
+                   , placeholder = Nothing
+                   , value = 15
+                   , min = Just 0
+                   , max = Just 15
+                   , step = Nothing
+                   , update = {
+                         onEnter = Nothing
+                       , onKeyboardLost = Nothing
+                       , onInput = Just (\res -> Maybe.map SetRefreshRate (Result.toMaybe res))
+                       }
+                   }
+                 , span' { class = "input-group-addon" } [ text "hz" ] ] ] ]
+         , tr_ [ thLabel 80 "Linux VM"
+               , td_ [ node "button"
+                   [ class "btn btn-primary btn-lg btn-block", onClick SendReboot ]
+                   [ text "Reboot" ] ] ]
+         ] ] ] ]
+    , row_ [ colXs_ 12 [
+          case model.httpError of
+            Just err -> div [ class "alert alert-warning" ] [ text (toString err) ]
+            Nothing -> div [] []
+        ] ]
+    ] ] ]
+    
+    
+
+thLabel w str = th [ style [ ("width", toString w ++ "%"), ("vertical-align", "middle") ] ] [ text str ]
 
 renderPidTabs : Html Msg
 renderPidTabs = panelDefault_ [
@@ -197,5 +283,57 @@ renderCalProgress p =
 -- SUBSCRIPTIONS
 
 subscriptions : Model -> Sub Msg
-subscriptions model =
-  Time.every (66 * millisecond) (\_ -> Poll)
+subscriptions model = Sub.batch [
+    Time.every (model.refreshRate * millisecond) (\_ -> Poll)
+  , Time.every second UpdateLatency
+  ]
+
+-- MOVE TO ANOTHER MODULE
+
+type alias FIR =
+  { weights : List Float
+  , inputs : List Float
+  , size : Int
+  }
+
+{-| Initialize a FIR filter with impulse response coefficients listed from most to least recent -}
+mkFir : List Float -> FIR
+mkFir ws =
+  let s = List.length ws
+  in { weights = ws, inputs = List.repeat s 0.0, size = s }
+
+{-| Run a FIR filter with a list of inputs from most to least recent. Returns the intermediate outputs and the updated filter state -}
+fir : FIR -> List Float -> (List Float, FIR)
+fir f is =
+  let go i (os, f') =
+        let (o, f'') = fir1 f' i
+        in (o :: os, f'')
+  in List.foldr go ([], f) is
+
+{-| Run a FIR filter with a single new input. Returns the output and the updated filter state -}
+fir1 : FIR -> Float -> (Float, FIR)
+fir1 f i =
+  let output = List.foldr go 0 (List.map2 (,) f.weights inputs')
+      inputs' = List.take f.size (i :: f.inputs)
+      f' = { f | inputs = inputs' }
+      go (w, i) s = s + (w * i)
+  in (output, f')
+
+type alias Smoother =
+  { firState : FIR
+  , value : Float
+  , mag : Float
+  }
+
+mkSmoother ws =
+  { firState = mkFir ws, mag = List.sum ws, value = 0 }
+
+putNext : Smoother -> Float -> Smoother
+putNext s i =
+  let (v, fs') = fir1 s.firState i
+  in { s | firState = fs', value = v / s.mag }
+
+putSome : Smoother -> List Float -> Smoother
+putSome s is =
+  let (vs, fs') = fir s.firState is
+  in { s | firState = fs', value = Maybe.withDefault s.value (Maybe.map (\x -> x / s.mag) (List.head vs)) }
