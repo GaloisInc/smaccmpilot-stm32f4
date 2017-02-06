a46ed6b30bc33239a33c9e521f2986da9e7e6dfd
Adam C. Foltzer <acfoltzer@galois.com>
2017-02-06 09:56:31 -0800

    restore argument passing for gcs script
    fixes #32

--- src/smaccm-comm-client/gcs.sh
+++ src/smaccm-comm-client/gcs.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 if [ $# -lt 2 ]; then
-	echo "Usage: $0 <serial> <baud> [options]..." >&2
-	exit 1
+        echo "Usage: $0 <serial> <baud> [options]..." >&2
+        exit 1
 fi
 
 serial=$1
@@ -10,9 +10,9 @@ baud=$2
 shift 2
 
 exec stack exec -- smaccm-comm-client \
-	--serial=$serial \
-	--baud=$baud \
-	--conf-file=../smaccm-flight/fmu24.conf \
-	--conf-path=../smaccm-flight \
-  --logsuffix=log.csv
-	"$@"
+        --serial=$serial \
+        --baud=$baud \
+        --conf-file=../smaccm-flight/fmu24.conf \
+        --conf-path=../smaccm-flight \
+        --logsuffix=log.csv \
+        "$@"
